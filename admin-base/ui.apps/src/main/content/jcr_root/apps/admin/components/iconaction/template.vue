f72fad2d61a489537e5eac9f6dfc397331644136
Byron Ponce <byron@polished-web.com>
2017-04-27 12:05:58 -0700

    update bg to blue-grey

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/iconaction/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/iconaction/template.vue
@@ -1,13 +1,17 @@
 <template>
-<div class="col m4">
-    <div class="card indigo darken-1">
+<div class="col s12 m4">
+    <div class="card blue-grey darken-3">
         <div class="card-content white-text">
             <span class="card-title">{{model.title}}</span>
-            <p>
-                {{model.description}}
-            </p>
-            <div class="card-action">
-    <admin-components-action v-bind:model="{ target: model.action, command: 'selectPath', title: 'explore' }"></admin-components-action>
+            <p>{{model.description}}</p>
+                <div class="card-action">
+                    <admin-components-action 
+                        v-bind:model="{ 
+                            target: model.action, 
+                            command: 'selectPath', 
+                            title: 'explore' 
+                        }">
+                    </admin-components-action>
             </div>
         </div>
     </div>
