f91a4659427b06822ebdff6c9d845124b11f27e3
Byron Ponce <byron@polished-web.com>
2017-04-27 11:15:49 -0700

    update mayterialize styles

--- admin-base/materialize/index.js
+++ admin-base/materialize/index.js
@@ -3,5 +3,7 @@ var fs = require('fs-extra');
 
 fs.mkdirs('./target/classes/etc/felibs/materialize')
 sass.render({
-  file: 'materialize-src/sass/materialize.scss',
-}, function(err, result) { fs.writeFileSync('./target/classes/etc/felibs/materialize/materialize.css',result.css) });
+  file: 'peregrine.scss',
+}, function(err, result) { 
+	fs.writeFileSync('./target/classes/etc/felibs/materialize/materialize.css', result.css) 
+});
