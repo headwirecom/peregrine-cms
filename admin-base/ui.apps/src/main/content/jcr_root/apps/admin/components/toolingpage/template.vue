24eb36c46608579da35c7501d78af5526d8df0c8
Byron Ponce <byron@polished-web.com>
2017-04-27 18:51:48 -0700

    extract component styles to scss

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/toolingpage/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/toolingpage/template.vue
@@ -1,9 +1,9 @@
 <template>
-<div style="position: fixed; width: 100%; height: 100%; top: 0; bottom: 0; left: 0; right: 0; overflow: scroll;">
-    <template v-for="child in model.children">
-        <component v-bind:is="child.component" v-bind:model="child"></component>
-    </template>
-</div>
+    <div class="tooling-page">
+        <template v-for="child in model.children">
+            <component v-bind:is="child.component" v-bind:model="child"></component>
+        </template>
+    </div>
 </template>
 
 <script>
