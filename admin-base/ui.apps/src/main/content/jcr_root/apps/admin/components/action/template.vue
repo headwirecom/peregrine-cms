a9574f8c08619a9c7605ab1d441ca4b04231cc53
Byron Ponce <byron@polished-web.com>
2017-04-27 17:55:57 -0700

    beautify formatting

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/action/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/action/template.vue
@@ -1,9 +1,22 @@
 <template>
     <span>
-    <a v-if="!model.type" v-bind:href="model.target" v-on:click.stop.prevent="action" v-bind:class="model.classes">{{model.title}}<slot></slot></a>
-    <a v-if="model.type === 'icon'" v-bind:title="model.title" v-bind:href="model.target" v-on:click.stop.prevent="action" class="btn-floating waves-effect waves-light" v-bind:class="model.classes">
-        <i class="material-icons">{{model.icon ? model.icon : model.title}}<slot></slot></i>
-    </a>
+        <a 
+            v-if                    = "!model.type" 
+            v-bind:href             = "model.target" 
+            v-on:click.stop.prevent = "action" 
+            v-bind:class            = "model.classes">
+            {{model.title}}
+            <slot></slot>
+        </a>
+        <a 
+            v-if                    = "model.type === 'icon'" 
+            v-bind:title            = "model.title" 
+            v-bind:href             = "model.target" 
+            v-on:click.stop.prevent = "action" 
+            class                   = "btn-floating waves-effect waves-light" 
+            v-bind:class            = "model.classes">
+            <i class="material-icons">{{model.icon ? model.icon : model.title}}<slot></slot></i>
+        </a>
     </span>
 </template>
 
