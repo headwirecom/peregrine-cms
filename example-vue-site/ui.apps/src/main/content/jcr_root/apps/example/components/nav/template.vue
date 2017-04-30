b78fb39a88375b363f8b63f7c4ccc1017836893c
Ruben Reusser <rr@headwire.com>
2017-04-30 08:31:26 -0700

    added a couple more pages to the example project and support for nav items and hilight of current page

--- example-vue-site/ui.apps/src/main/content/jcr_root/apps/example/components/nav/template.vue
+++ example-vue-site/ui.apps/src/main/content/jcr_root/apps/example/components/nav/template.vue
@@ -3,12 +3,12 @@
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <a class="navbar-brand" href="#">{{model.brand}}</a>
+        <a class="navbar-brand" href="/content/sites/example.html">{{model.brand}}</a>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">intro</a>
+                <li class="nav-item" v-for="navItem in model.navigation">
+                    <a class="nav-link" v-bind:class="isActive(navItem)" v-bind:href="navItem.path +'.html'">{{navItem.title}}</a>
                 </li>
             </ul>
         </div>
@@ -17,6 +17,16 @@
 
 <script>
     export default {
-        props: ['model']
+        props: ['model'],
+        computed: {
+            currentPagePath: function() {
+                return this.$root.$data.page.pagePath
+            }
+        },
+        methods: {
+            isActive: function(item) {
+                return item.path === this.$root.$data.page.pagePath ? 'active': ''
+            }
+        }
     }
 </script>
