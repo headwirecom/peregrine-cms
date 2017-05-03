24a0349ebba937ae0d227cdd2ba181e9fc192a27
Byron Ponce <byron@polished-web.com>
2017-05-03 15:12:38 -0700

    add drag and drop upload

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/fileupload/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/fileupload/template.vue
@@ -1,41 +1,80 @@
 <template>
-    <span>
-        <input id="fileupload" type="file" placeholder="upload file" class="form-control">
-        <button v-on:click="uploadFile" class="btn">upload</button>
-    </span>
+    <form 
+      id="file_upload" 
+      method="post" 
+      action="" 
+      enctype="multipart/form-data"
+      v-bind:class="dragStateClass"
+      v-on:submit.prevent="uploadFile"
+      v-on:drag.prevent="stopPropagation"
+      v-on:dragstart.prevent="stopPropagation"
+      v-on:dragover.prevent="addDragOverClass"
+      v-on:dragenter.prevent="addDragOverClass"
+      v-on:dragleave.prevent="removeDragOverClass"
+      v-on:dragend.prevent="removeDragOverClass"
+      v-on:drop.prevent="onDropFile">
+        <div class="file-upload-inner">
+          <i class="material-icons">file_download</i>
+          <span class="file-upload-text">Drag files here</span>
+        </div>
+    </form>
 </template>
 
 <script>
 export default {
-    props: ['model'],
-    data: function() {
-
-            return {
-            formModel: { file: '' },
-            schema: { fields: [{
-                       type: "input",
-                       inputType: "file",
-                       label: "File",
-                       model: 'file',
-                       required: false,
-                       placeholder: "upload file"
-                       },{
-                        type: "submit",
-                        buttonText: "upload",
-                        onSubmit: this.uploadFile,
-                        model: "upload"
-                        }]},
-            formOptions: {
-                          validateAfterLoad: true,
-                          validateAfterChanged: true
-                        }
-                   }
-
-    }, methods: {
-        uploadFile: function() {
-            $perAdminApp.stateAction('uploadFiles', { path: $perAdminApp.getView().state.tools.assets, files: this.$el.children[0].files })
-        }
+  props: ['model'],
+  data() {
+    return {
+      dragStateClass: '',
+      formModel: { file: '' },
+      schema: { 
+        fields: [
+          {
+            type: "input",
+            inputType: "file",
+            label: "File",
+            model: 'file',
+            required: false,
+            placeholder: "upload file"
+          },
+          {
+            type: "submit",
+            buttonText: "upload",
+            onSubmit: this.uploadFile,
+            model: "upload"
+          }
+        ]
+      },
+      formOptions: {
+        validateAfterLoad: true,
+        validateAfterChanged: true
+      }
     }
+  }, 
+  methods: {
+    addDragOverClass(ev){
+      ev.stopPropagation()
+      this.dragStateClass = 'is-dragover'
+    },
+    removeDragOverClass(ev){
+      ev.stopPropagation()
+      this.dragStateClass = ''
+    },
+    onDropFile(ev){
+      ev.stopPropagation()
+      this.dragStateClass = ''
+      this.uploadFile(ev.dataTransfer.files)
+    },
+    stopPropagation(ev){
+      ev.stopPropagation()
+    },
+    uploadFile(files) {
+      $perAdminApp.stateAction('uploadFiles', { 
+        path: $perAdminApp.getView().state.tools.assets, 
+        files: files
+      })
+    }
+  }
 }
 
 </script>
