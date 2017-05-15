b69b4006d6842701c62840fd0fcb2d4d8188ac12
rr <ruben@perfectsensedigital.com>
2017-05-15 08:56:11 -0700

    added initial subnav to page editing

---
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/subnav/template.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="nav-content">
+        <ul class="tabs tabs-transparent">
+            <li class="tab"><a href="#">mobile</a></li>
+            <li class="tab"><a href="#">tablet</a></li>
+            <li class="tab"><a href="#">desktop</a></li>
+            <li class="tab"><a href="#">preview</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['model']
+}
+</script>
