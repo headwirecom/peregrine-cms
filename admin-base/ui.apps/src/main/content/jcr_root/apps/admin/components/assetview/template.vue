718423148b17bba6393c48e5d935cbdae6a43f19
Byron Ponce <byron@polished-web.com>
2017-05-26 12:27:57 -0700

    add asset info

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/assetview/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/assetview/template.vue
@@ -1,7 +1,21 @@
 <template>
-    <div v-if="currentObject">
-        <img v-if="isImage(currentObject.show)" v-bind:src="currentObject.show" width="400px"/>
-        <iframe v-else v-bind:src="currentObject.show" width="400px"></iframe>
+    <div v-if="currentObject" class="asset-preview">
+        <ul class="asset-info">
+            <li>
+                <span class="asset-name">created:</span>
+                <span class="asset-value">April 1st, 2017</span>
+            </li>
+            <li>
+                <span class="asset-name">modified:</span>
+                <span class="asset-value">April 1st, 2017</span>
+            </li>
+            <li>
+                <span class="asset-name">source:</span>
+                <span class="asset-value">{{ currentObject.show }}</span>
+            </li>
+        </ul>
+        <img v-if="isImage(currentObject.show)" v-bind:src="currentObject.show"/>
+        <iframe v-else v-bind:src="currentObject.show"></iframe>
     </div>
 </template>
 
