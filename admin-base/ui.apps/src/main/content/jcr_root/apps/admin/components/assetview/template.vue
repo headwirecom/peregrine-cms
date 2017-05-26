bd5c493880aa524f5203724f8a867dc9de6aedfb
Byron Ponce <byron@polished-web.com>
2017-05-26 12:58:29 -0700

    show icon in preview when no asset is selected

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/assetview/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/assetview/template.vue
@@ -1,22 +1,31 @@
 <template>
-    <div v-if="currentObject" class="asset-preview">
-        <ul class="asset-info">
-            <li>
-                <span class="asset-name">created:</span>
-                <span class="asset-value">April 1st, 2017</span>
-            </li>
-            <li>
-                <span class="asset-name">modified:</span>
-                <span class="asset-value">April 1st, 2017</span>
-            </li>
-            <li>
-                <span class="asset-name">source:</span>
-                <span class="asset-value">{{ currentObject.show }}</span>
-            </li>
-        </ul>
-        <img v-if="isImage(currentObject.show)" v-bind:src="currentObject.show"/>
-        <iframe v-else v-bind:src="currentObject.show"></iframe>
+    <div class="asset-preview">
+        <template v-if="currentObject">
+            <ul class="asset-info">
+                <li>
+                    <span class="asset-name">created:</span>
+                    <span class="asset-value">April 1st, 2017</span>
+                </li>
+                <li>
+                    <span class="asset-name">modified:</span>
+                    <span class="asset-value">April 1st, 2017</span>
+                </li>
+                <li>
+                    <span class="asset-name">source:</span>
+                    <span class="asset-value">{{ currentObject.show }}</span>
+                </li>
+            </ul>
+            <img v-if="isImage(currentObject.show)" v-bind:src="currentObject.show"/>
+            <iframe v-else v-bind:src="currentObject.show"></iframe>
+        </template>
+        <template v-else>
+            <div class="no-asset-selected">
+                <span>no asset selected</span>
+                <i class="material-icons">info</i>
+            </div>
+        </template>
     </div>
+
 </template>
 
 <script>
