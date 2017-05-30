9bd73f232fd290fbb8cc90465078cb1290755530
Byron Ponce <byron@polished-web.com>
2017-05-30 10:31:02 -0700

    add subnav styles

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/nav/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/nav/template.vue
@@ -1,26 +1,29 @@
 <template>
 <nav v-bind:data-per-path="model.path" v-bind:class="isExtended ? 'nav-extended' : ''">
     <div class="nav-wrapper blue-grey darken-3">
-      <div class="brand-logo">
-        <admin-components-action 
-          v-bind:model="{ 
-            command: 'selectPath', 
-            title: 'home', 
-            target: '/content/admin' 
-          }">
-        </admin-components-action>
-          <template v-for="item in vueRoot.adminPage.breadcrumbs">&nbsp;&raquo;&nbsp;<admin-components-action
+      <div class="col s12">
+        <div class="brand-logo">
+          <admin-components-action 
+            v-bind:model="{ 
+              command: 'selectPath', 
+              title: 'home', 
+              target: '/content/admin',
+              classes: 'peregrine-logo'
+            }">
+          </admin-components-action>
+          <template v-for="item in vueRoot.adminPage.breadcrumbs">
+            <admin-components-action
               v-bind:model="{
-              command: 'selectPath',
-              title: item.title,
-              target: item.path
-              }">
-              </admin-components-action></template>
-
+                command: 'selectPath',
+                title: item.title,
+                target: item.path
+              }"></admin-components-action>
+          </template>
+        </div>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="/system/sling/logout?resource=/index.html">{{this.$root.$data.state.user}}</a></li>
+        </ul>
       </div>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="/system/sling/logout?resource=/index.html">{{this.$root.$data.state.user}}</a></li>
-      </ul>
     </div>
     <template v-for="child in model.children">
         <component v-bind:is="child.component" v-bind:model="child"></component>
