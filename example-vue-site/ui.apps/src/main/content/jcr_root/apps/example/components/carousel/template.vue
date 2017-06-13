089cc012e6fc244a75ac72a7affdd4bc4073b241
Byron Ponce <byron@polished-web.com>
2017-06-13 14:26:14 -0700

    update carousel template

--- example-vue-site/ui.apps/src/main/content/jcr_root/apps/example/components/carousel/template.vue
+++ example-vue-site/ui.apps/src/main/content/jcr_root/apps/example/components/carousel/template.vue
@@ -1,91 +1,49 @@
 <template>
-    <div v-bind:id="name" class="carousel slide" data-ride="carousel" v-bind:data-per-path="model.path" style="background-color: silver;">
-        <ol class="carousel-indicators">
-            <li v-for="(item, key) in model.children" v-bind:data-target="'#'+item.name" v-bind:data-slide-to="key" v-bind:class="key === 0 ? 'active': ''"></li>
-        </ol>
-        <div class="carousel-inner" role="listbox">
-            <div v-for="(item, key) in model.children" class="carousel-item" v-bind:class="key === 0 ? 'active': ''">
-                <img class="d-block img-fluid" v-bind:src="item.imagePath" v-bind:alt="item.alt">
-                <div v-if="item.heading || item.text" class="carousel-caption d-none d-md-block">
-                    <h3 v-if="item.heading">{{item.heading}}</h3>
-                    <p v-if="item.text" v-html="item.text"></p>
-                </div>
-            </div>
+  <div 
+    class="carousel slide"
+    v-bind:id              ="name"
+    v-bind:data-interval   ="model.interval"
+    v-bind:data-pause      ="model.pause"
+    v-bind:data-ride       ="model.ride"
+    v-bind:data-indicators ="model.indicators"
+    v-bind:data-controls   ="model.controls"
+    v-bind:data-wrap       ="model.wrap"
+    v-bind:data-keyboard   ="model.keyboard"
+    v-bind:data-per-path   ="model.path"
+    style="min-height: 300px;background-color:#eceff1;">
+    <ol v-if="model.indicators" class="carousel-indicators">
+      <li 
+        v-for="(slide, index) in model.children"
+        :data-target="`#${name}`"
+        :data-slide-to="index"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+      <div v-for="(slide, index) in model.children" :class="`carousel-item ${index === 0 ? 'active' : ''}`">
+        <img v-if="slide.imagePath" :src="slide.imagePath" :alt="slide.alt" />
+        <div v-if="slide.heading || slide.text" class="carousel-caption">
+          <h3 v-if="slide.heading">{{slide.heading}}</h3>
+          <p v-if="slide.text" v-html="slide.text"></p>
         </div>
-        <a class="carousel-control-prev" v-bind:href="'#'+name" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" v-bind:href="'#'+name" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
+      </div>
     </div>
+    <template v-if="model.controls">
+      <a class="carousel-control-prev" :href="`#${name}`" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+      </a>
+      <a class="carousel-control-next" :href="`#${name}`" role="button" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+      </a>
+    </template>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: ['model'],
-        computed: {
-            name() {
-                return this.model.path.split('/').slice(1).join('-').slice(4)
-            }
-        },
-        methods: {
-            augmentEditorSchema(form) {
-                form.fields[0].buttons[0].onclick = function(model) {
-                    if(!model.children) {
-                        window.parent.$perAdminApp.getApp().$set(model, 'children', [])
-                    }
-                    model.children.push( {
-                        name: 'child'+(model.children.length+1),
-                        'component': 'example-components-carousel-item',
-                        'path': model.path + '/child' + (model.children.length+1)
-                    } )
-                    window.parent.$perAdminApp.getApp().$set(model, 'children', model.children.slice(0))
-//                    window.parent.$perAdminApp.getApp().$set(model, 'selection' , model.children[model.children.length -1])
-                }
-                form.fields[0].buttons[1].onclick = function(model) {
-                    if(model.selection) {
-                        for(let i = 0; i < model.children.length; i++) {
-                            if(model.selection.name === model.children[i].name) {
-                                model.children.splice(i, 1)
-                                window.parent.$perAdminApp.getApp().$set(model, 'children', model.children.slice(0))
-                                break;
-                            }
-                        }
-                    }
-                    window.parent.$perAdminApp.getApp().$set(model, 'children', model.children.slice(0))
-                    model.selection = {}
-                }
-                form.fields[0].values = function(model, schema) {
-                    if(!model.children) {
-                        window.parent.$perAdminApp.getApp().$set(model, 'children', [])
-                    }
-                    return model.children
-                }
-                form.fields[1].visible = function(model) { return model.selection}
-                form.fields[2].visible = function(model) { return model.selection}
-                form.fields[3].visible = function(model) { return model.selection}
-                form.fields[4].visible = function(model) { return model.selection}
-                form.fields[5].visible = function(model) { return model.selection}
-                return form
-            },
-            beforeSave(data) {
-                delete data.selection
-                while(data.children.length > 0) {
-                    let name = data.children[0].name
-                    if(data.children[0].path) {
-                        name = data.children[0].path.split('/').pop()
-                    }
-                    delete data.children[0].path
-                    delete data.children[0].component
-                    data.children[0]['sling:resourceType'] = 'example/components/carouselItem'
-                    data[name] = data.children[0]
-                    data.children = data.children.slice(1)
-                }
-                return data
-            }
-        }
+  export default {
+    props: ['model'],
+    computed: {
+      name() {
+          return this.model.path.split('/').slice(1).join('-').slice(4)
+      }
     }
+  }
 </script>
