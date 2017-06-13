0ee9e55c3059de56d3b19b10328840d69db642b2
Byron Ponce <byron@polished-web.com>
2017-06-13 14:21:22 -0700

    add default carousel model

--- example-vue-site/core/src/main/java/com/example/site/models/CarouselModel.java
+++ example-vue-site/core/src/main/java/com/example/site/models/CarouselModel.java
@@ -3,10 +3,10 @@ package com.example.site.models;
 import com.peregrine.nodetypes.models.Container;
 import com.peregrine.nodetypes.models.IComponent;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
-
 import javax.inject.Inject;
 
 /**
@@ -32,6 +32,56 @@ public class CarouselModel extends Container {
     public CarouselModel(Resource resource) {
         super(resource);
     }
+
+    @Inject @Default(values = "5000")
+    private String interval;
+
+    @Inject @Default(values = "hover")
+    private String pause;
+
+    @Inject @Default(values = "true")
+    private String ride;
+
+    @Inject @Default(values = "true")
+    private String indicators;
+
+    @Inject @Default(values = "true")
+    private String controls;
+
+    @Inject @Default(values = "true")
+    private String wrap;
+
+    @Inject @Default(values = "true")
+    private String keyboard;
+
+    public String getInterval() {
+        return interval;
+    }
+
+    public String getPause() {
+        return pause;
+    }
+
+    public String getRide() {
+        return ride;
+    }
+
+    public String getControls() {
+        return controls;
+    }
+
+    public String getIndicators() {
+        return indicators;
+    }
+
+    public String getWrap() {
+        return wrap;
+    }
+
+    public String getKeyboard() {
+        return keyboard;
+    }
+
 
     //GEN]
 }
