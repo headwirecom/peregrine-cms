22ad227e46f5ea253ff3d3cb58b1765bddde8408
reusr1 <rr@headwire.com>
2017-06-17 08:17:39 -0700

    added container to example site

---
+++ example-vue-site/ui.apps/src/main/content/jcr_root/apps/example/components/container/template.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <div v-for="child in model.children">
+            <component v-bind:is="child.component" v-bind:model="child"></component>
+        </div>
+        <pagerender-vue-components-placeholder v-bind:model="{ path: model.path, component: model.component }"></pagerender-vue-components-placeholder>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [ 'model' ]
+    }
+</script>
