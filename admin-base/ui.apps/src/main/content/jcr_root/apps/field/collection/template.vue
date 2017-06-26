3938c2b14876844c18ce84fe639b51a857be53d9
reusr1 <rr@headwire.com>
2017-06-26 06:35:28 -0700

    was missing /ul

--- admin-base/ui.apps/src/main/content/jcr_root/apps/field/collection/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/field/collection/template.vue
@@ -26,16 +26,17 @@
 	<div class="edit-collection" style="flex:1;">
 		<h5>{{schema.title}} <button type="button" class="btn btn-primary" v-on:click="onAddItem">add</button></h5>
 		<ul class="collapsible">
-	    <li v-for="(item, index) in schema.items" v-bind:class="activeItem === index ? 'active' : ''">
-	      <div class="collapsible-header" v-on:click="onSetActiveItem(index)">
-	      	<i class="material-icons" v-on:click="onRemoveItem(index)">clear</i>Item #{{index}}
-	      </div>
-	      <div class="collapsible-body">
-	      	<vue-form-generator 
-		        :schema="item" 
-		        :model="model.children[index]"></vue-form-generator>
-	      </div>
-	    </li>
+            <li v-for="(item, index) in schema.items" v-bind:class="activeItem === index ? 'active' : ''">
+              <div class="collapsible-header" v-on:click="onSetActiveItem(index)">
+                <i class="material-icons" v-on:click="onRemoveItem(index)">clear</i>Item #{{index}}
+              </div>
+              <div class="collapsible-body">
+                <vue-form-generator
+                    :schema="item"
+                    :model="model.children[index]"></vue-form-generator>
+              </div>
+            </li>
+        </ul>
 	</div>
 </template>
 
