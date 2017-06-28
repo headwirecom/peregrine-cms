75c4f92522b2055529ef2a6a3b857db62cdaa9c6
Andreas Schaefer <as@headwire.com>
2017-06-28 14:16:43 -0700

    Delete a Page used the wrong name for the suffix
    Signed-off-by: Andreas Schaefer <as@headwire.com>

--- admin-base/ui.apps/src/main/js/apiImpl.js
+++ admin-base/ui.apps/src/main/js/apiImpl.js
@@ -283,7 +283,7 @@ class PerAdminImpl {
     deletePage(path) {
         return new Promise( (resolve, reject) => {
             let data = new FormData()
-            updateWithForm('/admin/deletePage.json'+parentPath, data)
+            updateWithForm('/admin/deletePage.json'+path, data)
                 .then( (data) => this.populateNodesForBrowser(path) )
                 .then( () => resolve() )
         })
