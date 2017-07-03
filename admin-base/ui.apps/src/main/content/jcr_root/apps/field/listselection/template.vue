4ab4a56d776cd41d9a2e85157919f8c00008342d
reusr1 <rr@headwire.com>
2017-07-03 07:36:46 -0700

    first stab at a server driven list component

---
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/field/listselection/template.vue
@@ -0,0 +1,71 @@
+<!--
+  #%L
+  admin base - UI Apps
+  %%
+  Copyright (C) 2017 headwire inc.
+  %%
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+  
+  http://www.apache.org/licenses/LICENSE-2.0
+  
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  KIND, either express or implied.  See the License for the
+  specific language governing permissions and limitations
+  under the License.
+  #L%
+  -->
+<template>
+    <div>
+        <!--
+        <input
+            ref="field"
+            class="form-control"
+            type="text"
+            :disabled="disabled"
+            :maxlength="schema.max"
+            :placeholder="schema.placeholder"
+            :readonly="schema.readonly" >
+        <button v-on:click.stop.prevent="add" class="btn-flat">
+          <i class="material-icons">insert_drive_file</i>
+        </button>
+        <div v-for="item in value">
+            <a href="#" v-on:click.stop.prevent="remove(item)">D</a> {{item}}
+        </div>
+        -->
+        <select v-model="value" class="browser-default" style="height: 8rem" multiple>
+            <option v-bind:value="val" v-for="val in schema.values">{{val}}</option>
+        </select>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['model'],
+        mixins: [ VueFormGenerator.abstractField ],
+        beforeMount() {
+            if(!this.schema.values) {
+                axios.get(this.schema.valuesFrom).then( (response) => {
+                    console.log(response.data)
+                    if(!this.schema.values) {
+                        Vue.set(this.schema, 'values', [])
+                    }
+                    for(var key in response.data) {
+                        console.log(key)
+                        if(response.data[key]['jcr:title']) {
+                            console.log(response.data[key]['jcr:title'])
+                            Vue.set(this.schema.values, this.schema.values.length, response.data[key]['jcr:title'])
+                        }
+                    }
+                })
+            }
+        }
+    }
+</script>
