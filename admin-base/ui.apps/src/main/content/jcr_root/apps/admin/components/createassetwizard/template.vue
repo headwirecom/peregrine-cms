8b3838cd2e4530f2efc2daa52f57fd6e1aab1bba
Byron Ponce <byron@polished-web.com>
2017-07-24 17:24:53 -0700

    add createassetwizard content nodes

---
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/createassetwizard/template.vue
@@ -0,0 +1,106 @@
+<!--
+  #%L
+  admin base - UI Apps
+  %%
+  Copyright (C) 2017 headwire inc.
+  %%
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+  
+  http://www.apache.org/licenses/LICENSE-2.0
+  
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  KIND, either express or implied.  See the License for the
+  specific language governing permissions and limitations
+  under the License.
+  #L%
+  -->
+<template>
+<div class="container">
+    <form-wizard v-bind:title="'create a folder'" 
+                 v-bind:subtitle="''" 
+                 @on-complete="onComplete" 
+                 color="#37474f">
+        <tab-content title="choose folder name" 
+                     :before-change="leaveTabOne">
+            <vue-form-generator :model="formmodel"
+                                :schema="nameSchema"
+                                :options="formOptions"
+                                ref="nameTab">
+            </vue-form-generator>
+        </tab-content>
+        <tab-content title="verify">
+            <pre v-html="JSON.stringify(formmodel, true, 2)"></pre>
+        </tab-content>
+    </form-wizard>
+</div>
+</template>
+
+<script>
+    export default {
+        props: ['model'],
+        data:
+            function() {
+                let formModelAssets = $perAdminApp.getNodeFromView('/state/tools/assets')
+                console.log('formModelAssets: ', formModelAssets)
+                return {
+                    formmodel: {
+                        path: formModelAssets,
+                        name: ''
+                    },
+                    formOptions: {
+                        validationErrorClass: "has-error",
+                        validationSuccessClass: "has-success",
+                        validateAfterChanged: true,
+                        focusFirstField: true
+                    },
+                    nameSchema: {
+                        fields: [{
+                            type: "input",
+                            inputType: "text",
+                            label: "Folder Name",
+                            model: "name",
+                            required: true,
+                            validator: this.nameAvailable
+                        }]
+                    }
+                }
+
+        },
+        computed: {
+            
+        },
+        methods: {
+            onComplete: function() {
+                let payload = { parent: this.formmodel.path, name: this.formmodel.name }
+                console.log('createFolder payload: ', payload)
+                $perAdminApp.stateAction('createAsset', payload)
+            },
+            nameAvailable(value) {
+                if(!value || value.length === 0) {
+                    return ['name is required']
+                } else {
+                    const folder = $perAdminApp.findNodeFromPath($perAdminApp.getView().admin.nodes, this.formmodel.path)
+                    for(let i = 0; i < folder.children.length; i++) {
+                        if(folder.children[i].name === value) {
+                            return ['name aready in use']
+                        }
+                    }
+                    return []
+                }
+                return true
+            },
+            leaveTabOne: function() {
+                return this.$refs.nameTab.validate()
+            }
+
+        }
+    }
+</script>
