4b54980c2ddb00ab50c76e60402117712a410d7d
Byron Ponce <byron@polished-web.com>
2017-07-28 20:22:09 -0700

    add loading spinner component

---
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/spinner/template.vue
@@ -0,0 +1,14 @@
+<template>
+<div :class="`spinner ${position}`" :style="`width: ${width}px`">
+  <svg class="circular" viewBox="25 25 50 50">
+    <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>
+  </svg>
+</div>
+
+</template>
+
+<script>
+export default {
+  props: ['width', 'position']
+}
+</script>
