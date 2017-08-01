47913153da10750bf72b85e05dd59ef0361a55c3
Andreas Schaefer <as@headwire.com>
2017-07-31 21:21:50 -0700

    Fixed an issuew with new content nodes in an update of a resource including thre removal of deleteed entries
    Signed-off-by: Andreas Schaefer <as@headwire.com>

--- admin-base/core/src/main/java/com/peregrine/admin/resource/AdminResourceHandlerService.java
+++ admin-base/core/src/main/java/com/peregrine/admin/resource/AdminResourceHandlerService.java
@@ -9,13 +9,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.peregrine.adaption.PerAsset;
 import com.peregrine.admin.replication.ImageMetadataSelector;
 import com.peregrine.rendition.BaseResourceHandler;
-import com.peregrine.commons.util.PerConstants;
 import com.peregrine.commons.util.PerUtil;
+import com.sun.media.sound.ModelIdentifier;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.apache.sling.api.resource.ModifiableValueMap;
 import org.apache.sling.api.resource.PersistenceException;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.api.resource.ValueMap;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
@@ -69,12 +69,6 @@ public class AdminResourceHandlerService
     ResourceRelocation resourceRelocation;
     @Reference
     BaseResourceHandler baseResourceHandler;
-//    @Reference
-//    MimeTypeService mimeTypeService;
-//    @Reference
-//    private ImageTransformationConfigurationProvider imageTransformationConfigurationProvider;
-//    @Reference
-//    private ImageTransformationProvider imageTransformationProvider;
 
     private List<ImageMetadataSelector> imageMetadataSelectors = new ArrayList<>();
     @Reference(
@@ -379,72 +373,6 @@ public class AdminResourceHandlerService
         return answer;
     }
 
-//    public ImageContext createRendition(Resource resource, String renditionName, String sourceMimeType) throws ManagementException {
-//        if(resource == null) {
-//            throw new ManagementException("No Asset Resource provided");
-//        }
-//        PerAsset asset = resource.adaptTo(PerAsset.class);
-//        if(asset == null) {
-//            throw new ManagementException("Resource: " + resource.getPath() + " could not be adapted to an Asset");
-//        }
-//        if(renditionName == null || renditionName.isEmpty()) {
-//            throw new ManagementException("No Rendition Name provided");
-//        }
-//        if(sourceMimeType == null || sourceMimeType.isEmpty()) {
-//            throw new ManagementException("No Source Mime Type provided");
-//        }
-//        int index = renditionName.indexOf('/');
-//        if(index >= 0) {
-//            renditionName = renditionName.substring(index + 1);
-//        }
-//        ImageContext answer = null;
-//
-//        String targetMimeType = mimeTypeService.getMimeType(renditionName);
-//
-//        List<ImageTransformationConfiguration> imageTransformationConfigurationList =
-//            imageTransformationConfigurationProvider.getImageTransformationConfigurations(renditionName);
-//        if(imageTransformationConfigurationList != null) {
-//            InputStream assetRenditionStream = asset.getRenditionStream(renditionName);
-//            if(assetRenditionStream != null) {
-//                answer = new ImageContext(sourceMimeType, targetMimeType, assetRenditionStream);
-//            }
-//            if(answer == null) {
-//                try {
-//                    InputStream sourceStream = asset.getRenditionStream((Resource) null);
-//                    if(sourceStream != null) {
-//                        ImageContext imageContext = transform(renditionName, sourceMimeType, sourceStream, targetMimeType, imageTransformationConfigurationList, false);
-//                        asset.addRendition(renditionName, imageContext.getImageStream(), targetMimeType);
-//                        updateModification(asset.getResource());
-//                        answer.resetImageStream(asset.getRenditionStream(renditionName));
-//                    } else {
-//                        logger.error("Resource: '{}' does not contain a data element", resource.getName());
-//                    }
-//                } catch(TransformationException e) {
-//                    logger.error("Transformation failed, image ignore", e);
-//                } catch(RepositoryException e) {
-//                    logger.error("Failed to create Rendition Node for Resource: '{}', rendition name: '{}'", resource, renditionName);
-//                } catch(PersistenceException e) {
-//                    logger.error("Failed to save Rendition Node for Resource: '{}', rendition name: '{}'", resource, renditionName);
-//                }
-//            }
-//            if(answer == null) {
-//                // Rendition was not found and could not be created therefore load and thumbnail the broken image
-//                String imagePath = ETC_FELIBS_ADMIN_IMAGES_BROKEN_IMAGE_SVG;
-//                Resource brokenImageResource = resource.getResourceResolver().getResource(imagePath);
-//                if(brokenImageResource != null) {
-//                    try {
-//                        InputStream brokenImageStream = getDataStream(brokenImageResource);
-//                        imageTransformationConfigurationList = imageTransformationConfigurationProvider.getImageTransformationConfigurations("thumbnail.png");
-//                        answer = transform(renditionName, "image/svg+xml", brokenImageStream, "image/png", imageTransformationConfigurationList, true);
-//                    } catch(TransformationException e) {
-//                        logger.error("Transformation failed, image ignore", e);
-//                    }
-//                }
-//            }
-//        }
-//        return answer;
-//    }
-
     public Resource createNode(Resource parent, String name, String primaryType, String resourceType) throws ManagementException {
         Map<String, Object> properties = new HashMap<>();
         properties.put(JCR_PRIMARY_TYPE, primaryType);
@@ -474,60 +402,6 @@ public class AdminResourceHandlerService
         return newNode;
     }
 
-//    /**
-//     * Takes the given source data streams and transforms it into the desired rendition
-//     * @param renditionName Name of the Rendition (node name)
-//     * @param sourceMimeType Mime Type of the Source
-//     * @param sourceStream Data Stream of the Source
-//     * @param targetMimeType Desired Target Mime Type
-//     * @param imageTransformationConfigurationList List of Image Transformation Configuration
-//     * @param noCrop If true then a thumbnail will not crop the image and hence the image might be smaller or shorter
-//     * @return Image Context that contains the final Data Stream
-//     * @throws TransformationException If the Transformation failed
-//     */
-//    private ImageContext transform(
-//        String renditionName, String sourceMimeType, InputStream sourceStream, String targetMimeType,
-//        List<ImageTransformationConfiguration> imageTransformationConfigurationList, boolean noCrop
-//    )
-//        throws TransformationException
-//    {
-//        ImageContext imageContext = new ImageContext(sourceMimeType, targetMimeType, sourceStream);
-//
-//        for(ImageTransformationConfiguration imageTransformationConfiguration : imageTransformationConfigurationList) {
-//            ImageTransformation imageTransformation = imageTransformationProvider.getImageTransformation(imageTransformationConfiguration.getTransformationName());
-//            if(imageTransformation != null) {
-//                Map<String, String> parameters = null;
-//                if(noCrop) {
-//                    parameters = new HashMap<>(imageTransformationConfiguration.getParameters());
-//                    parameters.put("noCrop", "true");
-//                } else {
-//                    parameters = imageTransformationConfiguration.getParameters();
-//                }
-//                OperationContext operationContext = new OperationContext(renditionName, parameters);
-//                // Disabled Transformations will stop the rendition creation as it does create incomplete or non-renditioned images
-//                imageTransformation.transform(imageContext, operationContext);
-//            }
-//        }
-//        return imageContext;
-//    }
-
-    /**
-     * Obtains the Data Stream of the given resource
-     * @param resource Resource containing the data. If this is not the jcr:content node then this node will be obtained first
-     * @return Input Stream if resource, properties and data property was found otherwise <code>null</code>
-     */
-    private InputStream getDataStream(Resource resource) {
-        InputStream answer = null;
-        if(resource != null && !PerConstants.JCR_CONTENT.equals(resource.getName())) {
-            resource = resource.getChild(PerConstants.JCR_CONTENT);
-        }
-        if(resource != null) {
-            ValueMap properties = resource != null ? resource.getValueMap() : null;
-            answer = properties != null ? properties.get(PerConstants.JCR_DATA, InputStream.class) : null;
-        }
-        return answer;
-    }
-
     @Override
     public Resource updateResource(ResourceResolver resourceResolver, String path, String jsonContent) throws ManagementException {
         Resource answer = null;
@@ -550,56 +424,54 @@ public class AdminResourceHandlerService
 
     private void updateResourceTree(Resource resource, Map<String, Object> properties) throws ManagementException {
         ModifiableValueMap updateProperties = getModifiableProperties(resource, false);
+        // Find all children nodes to remove the one not provided
+        List<String> children = new ArrayList<>();
+        for(Resource child: resource.getChildren()) {
+            children.add(child.getName());
+        }
         for(Entry<String, Object> entry: properties.entrySet()) {
             String name = entry.getKey();
             Object value = entry.getValue();
             if(value instanceof Map) {
+                Map childProperties = (Map) value;
                 Resource child = resource.getChild(name);
+                // If child is missing then create it
                 if(child == null) {
-                    throw new ManagementException("Property: '" + name + "' is a map but not child resource found with that name");
+                    Object val = childProperties.get(SLING_RESOURCE_TYPE);
+                    if(val instanceof String) {
+                        String resourceType = (String) val;
+                        childProperties.remove("name");
+                        childProperties.remove(SLING_RESOURCE_TYPE);
+                        childProperties.remove(JCR_PRIMARY_TYPE);
+                        child = createNode(resource, name, NT_UNSTRUCTURED, resourceType);
+                        // Now update the child with any remaining properties
+                        ModifiableValueMap newChildProperties = getModifiableProperties(child, false);
+                        for(Object childPropertyKey : childProperties.keySet()) {
+                            newChildProperties.put(childPropertyKey + "", childProperties.get(childPropertyKey));
+                        }
+                    } else {
+                        throw new ManagementException("Property: '" + name + "' is a map, is not found and cannot be created due to missing Sling Resource Type");
+                    }
+                } else {
+                    updateResourceTree(child, (Map) value);
+                    children.remove(name);
                 }
-                updateResourceTree(child, (Map) value);
             } else {
                 updateProperties.put(name, value);
             }
         }
+        for(String childNameToBeRemoved: children) {
+            Resource childToBeRemoved = getResource(resource, childNameToBeRemoved);
+            if(childNameToBeRemoved != null) {
+                try {
+                    resource.getResourceResolver().delete(childToBeRemoved);
+                } catch(PersistenceException e) {
+                    throw new ManagementException("Deletion of Child: '" + childNameToBeRemoved + "' could not be deleted from parent: " + resource.getPath(), e);
+                }
+            }
+        }
         baseResourceHandler.updateModification(resource);
     }
-
-//    @Override
-//    public void updateModification(ResourceResolver resourceResolver, Node node) {
-//        if(resourceResolver != null && node != null) {
-//            try {
-//                Resource resource = resourceResolver.getResource(node.getPath());
-//                updateModification(resource);
-//            } catch(RepositoryException e) {
-//                // Ignore
-//            }
-//        }
-//    }
-//
-//    /** @param resource Adds / Update the Last Modified and Last Modified By property on this resource **/
-//    @Override
-//    public void updateModification(Resource resource) {
-//        if(resource != null) {
-//            String user = resource.getResourceResolver().getUserID();
-//            Calendar now = Calendar.getInstance();
-//            ModifiableValueMap properties = getModifiableProperties(resource, false);
-//            properties.put(JCR_LAST_MODIFIED_BY, user);
-//            properties.put(JCR_LAST_MODIFIED, now);
-//            PerPage page = resource.adaptTo(PerPage.class);
-//            if(page != null) {
-//                page.markAsModified();
-//            } else {
-//                Resource jcrContent = PerUtil.getResource(resource, JCR_CONTENT);
-//                if(jcrContent != null) {
-//                    properties = getModifiableProperties(jcrContent, false);
-//                    properties.put(JCR_LAST_MODIFIED_BY, user);
-//                    properties.put(JCR_LAST_MODIFIED, now);
-//                }
-//            }
-//        }
-//    }
 
     public static Map convertToMap(String json) throws IOException {
         Map answer = new LinkedHashMap();
--- testing/testing.basic/src/main/java/com/peregrine/it/basic/TestConstants.java
+++ testing/testing.basic/src/main/java/com/peregrine/it/basic/TestConstants.java
@@ -8,6 +8,8 @@ public class TestConstants {
 
     public static final String EXAMPLE_PAGE_TYPE_PATH = "example/components/page";
     public static final String EXAMPLE_JUMBOTRON_TYPE_PATH = "example/components/jumbotron";
+    public static final String EXAMPLE_CAROUSEL_TYPE_PATH = "example/components/carousel";
+    public static final String EXAMPLE_CAROUSEL_ITEM_TYPE_PATH = "example/components/carouselItem";
 
     public static final String EXAMPLE_OBJECT_TYPE_PATH = "example/objects/sample";
 }
