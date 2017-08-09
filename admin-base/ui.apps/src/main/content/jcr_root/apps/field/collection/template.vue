7e3d1292a2d5736057bc109e371a7a60644c60d8
Byron Ponce <byron@polished-web.com>
2017-08-09 16:51:05 -0700

    use material accordion JS when multifield === true. Hide items when deleted, and add deleted === true prop to model

--- admin-base/ui.apps/src/main/content/jcr_root/apps/field/collection/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/field/collection/template.vue
@@ -30,10 +30,12 @@
         <i class="material-icons">add</i>
       </button>
     </h5>
-    <ul v-if="schema.multifield" class="collapsible">
-      <li v-for="(item, index) in schema.items" v-bind:class="activeItem === index ? 'active' : ''">
-        <div class="collapsible-header" v-on:click="onSetActiveItem(index)">
-          {{parseInt(index + 1)}} <i class="material-icons" v-on:click="onRemoveItem(index)">delete</i>
+    <ul v-if="schema.multifield" class="collapsible" ref="collapsible">
+      <li 
+        v-for="(item, index) in schema.items" 
+        v-bind:class="getItemClass(item, index)">
+        <div class="collapsible-header" v-on:click.stop.prevent="onSetActiveItem(index)">
+          {{itemName(item, index)}} <i class="material-icons" v-on:click.stop.prevent="onRemoveItem(item, index)">delete</i>
         </div>
         <div class="collapsible-body">
           <vue-form-generator
@@ -78,9 +80,18 @@
         this.value = []
       }
   	},
+    mounted(){
+      console.log('this.model: ', this.model)
+      if(this.schema.multifield){
+        $(this.$refs.collapsible).collapsible()
+      }
+    },
+    beforeDestroy(){
+      $(this.$refs.collapsible).collapsible('destroy')
+    },
   	data(){
   		return{
-  			activeItem: 0
+  			activeItem: null
   		}
   	},
     computed: {
@@ -94,22 +105,57 @@
     	}
     },
     methods: {
+      getItemClass(item, index){
+        if(this.activeItem === index){
+          return 'active'
+        }
+        if(item.deleted){
+          return'deleted'
+        }
+        return false
+      },
+      itemName(item, index) {
+        const len = this.schema.fieldLabel.length
+        for(let i=0; i<len; i++){
+          let label = this.schema.fieldLabel[i]
+          let childItem = this.model.children[index]
+          console.log('child item: ', childItem)
+          if(childItem[label]){
+            return childItem[label]
+          }
+        }
+        return parseInt(index) + 1
+      },
       onAddItem(e){
-        console.log('itemModel: ', this.itemModel)
         this.schema.items.push({ fields: JSON.parse(JSON.stringify(this.schema.fields.slice(0)))})
         if(!this.schema.multifield){
           this.schema.items[this.schema.items.length - 1].fields[0].model = this.schema.model + '['+(this.schema.items.length - 1)+']'
           this.value.push('')
         } else {
             this.value.push({ name: 'n' +Date.now()})
+            this.onSetActiveItem(this.schema.items.length - 1)
         }
       },
-      onRemoveItem(index){
-        this.schema.items.splice(index, 1)
-        this.value.splice(index, 1)
+      onRemoveItem(item, index){
+        item.deleted = true
+        let modelItem = this.value[index]
+        modelItem.deleted = true
+        this.$set(this.schema.items, index, item)
+        this.$set(this.value, index, modelItem)
       },
       onSetActiveItem(index){
-      	this.activeItem = index
+        if(index !== this.activeItem){
+          this.$nextTick(function () {
+            if(this.activeItem !== null){
+              $(this.$refs.collapsible).collapsible('close', this.activeItem)
+            }
+            $(this.$refs.collapsible).collapsible('open', index)
+            this.activeItem = index
+            // focus first field of expanded item
+            let firstField = this.$refs.collapsible.querySelector('li.active input')
+            firstField.focus()
+          })
+        }
       }
 
     }
