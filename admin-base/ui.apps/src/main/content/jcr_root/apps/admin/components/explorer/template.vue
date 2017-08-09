6dafe99fbe37ea3247b06e18271d52aa73a66679
DJ Pelland <djp@headwire.com>
2017-08-09 15:25:20 -0700

    added parameter for drop type (above/below)

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/explorer/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/explorer/template.vue
@@ -38,7 +38,7 @@
                     v-on:click.stop.prevent="selectParent()"
                     v-on:dragenter.stop.prevent ="onDragEnterRow"
                     v-on:dragleave.stop.prevent ="onDragLeaveRow" 
-                    v-on:drop.prevent      ="onDropRow(pt.children[0], $event)"
+                    v-on:drop.prevent      ="onDropRow(pt.children[0], $event, 'before')"
                     class="collection-item">
                     <admin-components-action
                             v-bind:model="{
@@ -59,7 +59,7 @@
                     v-on:dragenter.stop.prevent ="onDragEnterRow"
                     v-on:dragover.stop.prevent  ="onDragOverRow"
                     v-on:dragleave.stop.prevent ="onDragLeaveRow" 
-                    v-on:drop.prevent      ="onDropRow(child, $event)"
+                    v-on:drop.prevent      ="onDropRow(child, $event, 'after')"
                     v-on:click.stop.prevent="selectItem(child)">
                     <admin-components-action
                         v-bind:model="{
@@ -259,7 +259,7 @@
                 }
             },
 
-            onDropRow(item, ev) {
+            onDropRow(item, ev, type) {
                 if(this.isDraggingRow){
                     ev.target.classList.remove('active-drop-zone')
                     /* reorder row logic */
@@ -280,7 +280,7 @@
                     $perAdminApp.stateAction(action, {
                         path: ev.dataTransfer.getData("text"),
                         to: item.path,
-                        type: 'after'
+                        type: type 
                     })
                 }
             },
