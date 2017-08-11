6f7dbbf777cd8034c31ef02ff68309df0f987d4a
DJ Pelland <djp@headwire.com>
2017-08-11 09:55:31 -0700

    Add field material-textarea files

---
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/field/material-textarea/template.vue
@@ -0,0 +1,20 @@
+<template>
+	<textarea 
+		class="form-control materialize-textarea" 
+		v-model="value" 
+		:id="getFieldID(schema)" 
+		:disabled="disabled" 
+		:maxlength="schema.max" 
+		:minlength="schema.min" 
+		:placeholder="schema.placeholder" 
+		:readonly="schema.readonly" 
+		:rows="schema.rows || 2" 
+		:name="schema.inputName">
+	</textarea>
+</template>
+
+<script>	
+	export default {
+		mixins: [ VueFormGenerator.abstractField ]
+	}
+</script>
