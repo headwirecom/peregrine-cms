194b7815faeab7e65317935339931eeb0847f2f3
DJ Pelland <djp@headwire.com>
2017-08-16 15:26:35 -0700

    added top pagination

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/sourceimagewizard/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/sourceimagewizard/template.vue
@@ -30,6 +30,15 @@
         </form>
 
         <div v-if="state.results" class="search-content">
+                <!-- Pagination -->
+                <div v-if="state.numPages > 0" class="image-pagination">
+                    <span>Displaying page {{state.currentPage}} of {{state.numPages}}</span>
+                    <ul class="pagination">
+                        <li class="waves-effect"><a href="#!" v-on:click.stop="selectPage(currentPage - 1)"><i class="material-icons">chevron_left</i></a></li>
+                        <li v-for="(page,i) in state.numPages" :class="[{'active': state.currentPage == i+1}]"><a href="#!" v-on:click.stop="selectPage(i + 1)">{{ i + 1}}</a></li>
+                        <li class="waves-effect"><a href="#!" v-on:click.stop="selectPage(currentPage + 1)"><i class="material-icons">chevron_right</i></a></li>
+                    </ul>
+                </div>
             <span v-if="state.results.length < 1" class="no-results">No images found for '{{ state.input }}'</span>
 
             <!-- Image Preview --> 
