817c8fd4ff58bc263d51e5c522e585e2f2e97127
reusr1 <rr@headwire.com>
2017-08-29 10:16:09 -0700

    non translated resources that are hooked up for translation now show T[...] in the UI

--- admin-base/ui.apps/src/main/js/experiences.js
+++ admin-base/ui.apps/src/main/js/experiences.js
@@ -1,7 +1,7 @@
 
 function experience(model, name, defaultValue) {
+    const experience = 'lang:'+$perAdminApp.getView().state.language
     if(model.experiences) {
-        const experience = 'lang:'+$perAdminApp.getView().state.language
         for (let i = 0; i < model.experiences.length; i++) {
             const exp = model.experiences[i]
             if(exp.experiences.indexOf(experience) >= 0) {
@@ -9,6 +9,9 @@ function experience(model, name, defaultValue) {
                 return ret ? ret : defaultValue
             }
         }
+    }
+    if(experience !== 'lang:en' && experience.indexOf('lang:') === 0) {
+        return 'T[' + (model[name] ? model[name] : defaultValue) +']'
     }
     return model[name] ? model[name] : defaultValue
 
