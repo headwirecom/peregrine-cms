8af1088e469cf29b4870ebf2bbc41244aa65545b
Andreas Schaefer <as@headwire.com>
2017-10-23 14:54:01 -0700

    Added an IT test method to make sure that when the first entry of a list if remvoed the update still works fine.
    Signed-off-by: Andreas Schaefer <as@headwire.com>

--- testing/testing.client.side/src/test/java/com/peregrine/it/admin/UpdateResourceServletIT.java
+++ testing/testing.client.side/src/test/java/com/peregrine/it/admin/UpdateResourceServletIT.java
@@ -230,6 +230,88 @@ public class UpdateResourceServletIT
     }
 
     @Test
+    public void testUpdatePageWithDeletedFirstChildNodeByFlag() throws Exception {
+        SlingClient client = slingInstanceRule.getAdminClient();
+        SlingHttpResponse response = null;
+        String folderPath = ROOT_PATH + "/test-upwdfcnbf";
+        createFolderStructure(client, folderPath);
+        // Create a new source page
+        Calendar before = createTimestampAndWait();
+        String pageName = "test-page-1";
+        response = createPage(client, folderPath, pageName, EXAMPLE_TEMPLATE_PATH, 200);
+        logger.info("Response from creating test page: '{}'", response.getContent());
+
+        TestPage emptyPage = new TestPage(pageName, EXAMPLE_PAGE_TYPE_PATH, EXAMPLE_TEMPLATE_PATH);
+        checkResourceByJson(client, folderPath + "/" + pageName, 2, emptyPage.toJSon(), true);
+        checkLastModified(client, folderPath + "/" + pageName, before);
+
+        insertNodeAtAsComponent(client, folderPath + "/" + pageName + "/" + JCR_CONTENT, "/apps/" + EXAMPLE_CAROUSEL_TYPE_PATH, "into-after", 302);
+        Map<String, Map> children = extractChildNodes(listResourceAsJson(client, folderPath + "/" + pageName + "/" + JCR_CONTENT, 1));
+        logger.info("List ");
+        assertFalse("No Children found of: " + folderPath + "/" + pageName + "/" + JCR_CONTENT, children.isEmpty());
+        assertTrue("Too many Children found of: " + folderPath + "/" + pageName + "/" + JCR_CONTENT, children.size() == 1);
+        String carouselNodeName = children.keySet().iterator().next() + "";
+        assertFalse("Carousel Name is not provided", carouselNodeName == null || carouselNodeName.isEmpty());
+
+        TestPage pageWithCarousel = new TestPage(pageName, EXAMPLE_PAGE_TYPE_PATH, EXAMPLE_TEMPLATE_PATH);
+        pageWithCarousel.getContent()
+            .addChild(new ChildObject(carouselNodeName, EXAMPLE_CAROUSEL_TYPE_PATH));
+        checkResourceByJson(client, folderPath + "/" + pageName, 3, pageWithCarousel.toJSon(), true);
+        checkLastModified(client, folderPath + "/" + pageName, before);
+
+        String slide1Name = "slide-1";
+        String image1Path = "/content/asset/slide-1-image";
+        String slide2Name = "slide-2";
+        String image2Path = "/content/asset/slide-2-image";
+        before = createTimestampAndWait();
+        NoNameObject insertSlide = (NoNameObject) new NoNameObject(NT_UNSTRUCTURED).addSlingResourceType(EXAMPLE_CAROUSEL_TYPE_PATH)
+            .addChild(new ChildObject(slide1Name, EXAMPLE_CAROUSEL_ITEM_TYPE_PATH)
+                .addProperties(new Prop("name", slide1Name), new Prop("imagePath", image1Path))
+            )
+            .addChild(new ChildObject(slide2Name, EXAMPLE_CAROUSEL_ITEM_TYPE_PATH)
+                .addProperties(new Prop("name", slide2Name), new Prop("imagePath", image2Path))
+            );
+        response = updateResource(client, folderPath + "/" + pageName + "/" + JCR_CONTENT + "/" + carouselNodeName, insertSlide.toJSon(), 200);
+
+        // Check page now
+        TestPage pageWithCarouselAndSlides = new TestPage(pageName, EXAMPLE_PAGE_TYPE_PATH, EXAMPLE_TEMPLATE_PATH);
+        pageWithCarouselAndSlides.getContent()
+            .addChild(new ChildObject(carouselNodeName, EXAMPLE_CAROUSEL_TYPE_PATH)
+                .addChild(new ChildObject(slide1Name, EXAMPLE_CAROUSEL_ITEM_TYPE_PATH)
+                    .addProperties(new Prop("imagePath", image1Path))
+//AS TODO: Check if ignoring the 'name' property is done on purpose
+//                    .addProperties(new Prop("name", slide1Name), new Prop("imagePath", image1Path))
+                )
+                .addChild(new ChildObject(slide2Name, EXAMPLE_CAROUSEL_ITEM_TYPE_PATH)
+//                    .addProperties(new Prop("name", slide2Name))
+                    .addProperties(new Prop("imagePath", image2Path))
+                )
+            );
+        checkResourceByJson(client, folderPath + "/" + pageName, 3, pageWithCarouselAndSlides.toJSon(), true);
+        checkLastModified(client, folderPath + "/" + pageName, before);
+
+        // Now remove that child by indicating that the node has to be deleted
+        before = createTimestampAndWait();
+        // Not we are ready to update that component by adding a slide
+        NoNameObject deleteNode = (NoNameObject) new NoNameObject(NT_UNSTRUCTURED).addSlingResourceType(EXAMPLE_CAROUSEL_TYPE_PATH)
+            .addChild(new ChildObject(slide1Name, EXAMPLE_CAROUSEL_ITEM_TYPE_PATH)
+                .addProperties(new Prop(DELETION_PROPERTY_NAME, "true"))
+            );
+        response = updateResource(client, folderPath + "/" + pageName + "/" + JCR_CONTENT + "/" + carouselNodeName, deleteNode.toJSon(), 200);
+
+        // Check page now
+        TestPage pageWithCarouselWithoutSlide = new TestPage(pageName, EXAMPLE_PAGE_TYPE_PATH, EXAMPLE_TEMPLATE_PATH);
+        pageWithCarouselWithoutSlide.getContent()
+            .addChild(new ChildObject(carouselNodeName, EXAMPLE_CAROUSEL_TYPE_PATH)
+                .addChild(new ChildObject(slide2Name, EXAMPLE_CAROUSEL_ITEM_TYPE_PATH)
+//                    .addProperties(new Prop("name", slide2Name))
+                    .addProperties(new Prop("imagePath", image2Path))
+                )
+            );
+        checkResourceByJson(client, folderPath + "/" + pageName, 3, pageWithCarouselWithoutSlide.toJSon(), true);        checkLastModified(client, folderPath + "/" + pageName, before);
+    }
+
+    @Test
     public void testUpdatePageWithDeletedChildNodeByPath() throws Exception {
         SlingClient client = slingInstanceRule.getAdminClient();
         SlingHttpResponse response = null;
