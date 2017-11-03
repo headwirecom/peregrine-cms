742211fe2397ae0b548ca034a0f0bdda98f7b387
Li Qin <lq@headwire.com>
2017-11-03 15:47:40 -0700

    article header: check in

---
+++ themes/themeclean/fragments/articleheader/htmltovue.js
@@ -0,0 +1,11 @@
+module.exports = {
+    convert: function($, f) {
+    	f.wrap($, 'themeclean-components-block')
+        f.bindAttribute($.parent(),'model','model')
+        f.mapField( $.find('h2'), 'model.title')
+        f.mapField( $.find('h3'), 'model.subtitle')
+        f.addIf( $.find('h2'), 'model.showtitle == \'true\'')
+        f.addIf( $.find('h3'), 'model.showsubtitle == \'true\'')
+        f.bindAttribute( $ , 'class', 'model.textalign', false)
+    }
+}
---
+++ themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/articleheader/template.vue
@@ -0,0 +1,14 @@
+<template>
+  <themeclean-components-block v-bind:model="model">
+    <div class="col-12 col-md-8" v-bind:class="model.textalign">
+      <h2 class="col-12 p-0 pb-3" v-if="model.showtitle == 'true'">{{model.title}}</h2>
+      <h3 class="col-12 p-0 pb-3" v-if="model.showsubtitle == 'true'">{{model.subtitle}}</h3>
+    </div>
+  </themeclean-components-block>
+</template>
+
+<script>
+    export default {
+        props: ['model']
+    }
+</script>
