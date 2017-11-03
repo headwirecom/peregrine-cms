d09da783cd93d0f40a62d8ab5053dd52c021c6b8
Li Qin <lq@headwire.com>
2017-11-03 15:08:04 -0700

    Article block quote: check in

---
+++ themes/themeclean/core/src/main/java/com/themeclean/models/ArticleblockquoteModel.java
@@ -0,0 +1,288 @@
+package com.themeclean.models;
+
+import com.peregrine.nodetypes.models.AbstractComponent;
+import com.peregrine.nodetypes.models.IComponent;
+import com.peregrine.nodetypes.models.Container;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/*
+    //GEN[:DATA
+    {
+  "definitions": {
+    "Articleblockquote": {
+      "type": "object",
+      "x-type": "component",
+      "properties": {
+        "text": {
+          "type": "string",
+          "x-source": "inject",
+          "x-form-type": "texteditor"
+        },
+        "bg": {
+          "x-form-type": "reference",
+          "type": "object",
+          "x-type": "component",
+          "properties": {
+            "backgroundtype": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Background Type",
+              "x-form-type": "materialradio",
+              "x-default": "color",
+              "properties": {
+                "image": {
+                  "x-form-name": "Image",
+                  "x-form-value": "image"
+                },
+                "color": {
+                  "x-form-name": "Color",
+                  "x-form-value": "color"
+                },
+                "gradient": {
+                  "x-form-name": "Gradient",
+                  "x-form-value": "gradient"
+                }
+              }
+            },
+            "bgimage": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Background Image",
+              "x-form-type": "pathbrowser",
+              "x-form-visible": "model.backgroundtype == 'image'"
+            },
+            "overlay": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Overlay",
+              "x-form-type": "materialswitch",
+              "x-form-visible": "model.backgroundtype == 'image'"
+            },
+            "overlaycolor": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Overlay Color",
+              "x-form-type": "color",
+              "x-form-visible": "model.overlay == 'true' and model.backgroundtype == 'image'",
+              "x-default": "#ffffff"
+            },
+            "overlayopacity": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Overlay opacity",
+              "x-form-type": "range",
+              "x-form-min": 0,
+              "x-form-max": 100,
+              "x-form-visible": "model.overlay == 'true' and model.backgroundtype == 'image'",
+              "x-default": "50"
+            },
+            "bgcolor": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Background Color",
+              "x-form-type": "color",
+              "x-form-visible": "model.backgroundtype == 'color' or model.backgroundtype == 'gradient'",
+              "x-default": "#ffffff"
+            },
+            "color2": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Color 2",
+              "x-form-type": "color",
+              "x-form-visible": "model.backgroundtype == 'gradient'",
+              "x-default": "#c0c0c0"
+            },
+            "fullwidth": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Full Width",
+              "x-form-type": "materialswitch"
+            },
+            "fullheight": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Full Height",
+              "x-form-type": "materialswitch"
+            },
+            "toppadding": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Top Padding",
+              "x-form-type": "range",
+              "x-form-min": 0,
+              "x-form-max": 120,
+              "x-form-visible": "model.fullheight != 'true'"
+            },
+            "bottompadding": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Bottom Padding",
+              "x-form-type": "range",
+              "x-form-min": 0,
+              "x-form-max": 120,
+              "x-form-visible": "model.fullheight != 'true'"
+            }
+          }
+        }
+      }
+    }
+  },
+  "name": "Articleblockquote",
+  "componentPath": "themeclean/components/articleblockquote",
+  "package": "com.themeclean.models",
+  "modelName": "Articleblockquote",
+  "classNameParent": "AbstractComponent"
+}
+//GEN]
+*/
+
+//GEN[:DEF
+@Model(
+        adaptables = Resource.class,
+        resourceType = "themeclean/components/articleblockquote",
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
+        adapters = IComponent.class
+)
+@Exporter(
+        name = "jackson",
+        extensions = "json"
+)
+
+//GEN]
+public class ArticleblockquoteModel extends AbstractComponent {
+
+    public ArticleblockquoteModel(Resource r) { super(r); }
+
+    //GEN[:INJECT
+    	/* {"type":"string","x-source":"inject","x-form-type":"texteditor"} */
+	@Inject
+	private String text;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Background Type","x-form-type":"materialradio","x-default":"color","properties":{"image":{"x-form-name":"Image","x-form-value":"image"},"color":{"x-form-name":"Color","x-form-value":"color"},"gradient":{"x-form-name":"Gradient","x-form-value":"gradient"}}} */
+	@Inject
+	@Default(values ="color")
+	private String backgroundtype;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Background Image","x-form-type":"pathbrowser","x-form-visible":"model.backgroundtype == 'image'"} */
+	@Inject
+	private String bgimage;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Overlay","x-form-type":"materialswitch","x-form-visible":"model.backgroundtype == 'image'"} */
+	@Inject
+	private String overlay;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Overlay Color","x-form-type":"color","x-form-visible":"model.overlay == 'true' and model.backgroundtype == 'image'","x-default":"#ffffff"} */
+	@Inject
+	@Default(values ="#ffffff")
+	private String overlaycolor;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Overlay opacity","x-form-type":"range","x-form-min":0,"x-form-max":100,"x-form-visible":"model.overlay == 'true' and model.backgroundtype == 'image'","x-default":"50"} */
+	@Inject
+	@Default(values ="50")
+	private String overlayopacity;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Background Color","x-form-type":"color","x-form-visible":"model.backgroundtype == 'color' or model.backgroundtype == 'gradient'","x-default":"#ffffff"} */
+	@Inject
+	@Default(values ="#ffffff")
+	private String bgcolor;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Color 2","x-form-type":"color","x-form-visible":"model.backgroundtype == 'gradient'","x-default":"#c0c0c0"} */
+	@Inject
+	@Default(values ="#c0c0c0")
+	private String color2;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Full Width","x-form-type":"materialswitch"} */
+	@Inject
+	private String fullwidth;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Full Height","x-form-type":"materialswitch"} */
+	@Inject
+	private String fullheight;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Top Padding","x-form-type":"range","x-form-min":0,"x-form-max":120,"x-form-visible":"model.fullheight != 'true'"} */
+	@Inject
+	private String toppadding;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Bottom Padding","x-form-type":"range","x-form-min":0,"x-form-max":120,"x-form-visible":"model.fullheight != 'true'"} */
+	@Inject
+	private String bottompadding;
+
+
+//GEN]
+
+    //GEN[:GETTERS
+    	/* {"type":"string","x-source":"inject","x-form-type":"texteditor"} */
+	public String getText() {
+		return text;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Background Type","x-form-type":"materialradio","x-default":"color","properties":{"image":{"x-form-name":"Image","x-form-value":"image"},"color":{"x-form-name":"Color","x-form-value":"color"},"gradient":{"x-form-name":"Gradient","x-form-value":"gradient"}}} */
+	public String getBackgroundtype() {
+		return backgroundtype;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Background Image","x-form-type":"pathbrowser","x-form-visible":"model.backgroundtype == 'image'"} */
+	public String getBgimage() {
+		return bgimage;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Overlay","x-form-type":"materialswitch","x-form-visible":"model.backgroundtype == 'image'"} */
+	public String getOverlay() {
+		return overlay;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Overlay Color","x-form-type":"color","x-form-visible":"model.overlay == 'true' and model.backgroundtype == 'image'","x-default":"#ffffff"} */
+	public String getOverlaycolor() {
+		return overlaycolor;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Overlay opacity","x-form-type":"range","x-form-min":0,"x-form-max":100,"x-form-visible":"model.overlay == 'true' and model.backgroundtype == 'image'","x-default":"50"} */
+	public String getOverlayopacity() {
+		return overlayopacity;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Background Color","x-form-type":"color","x-form-visible":"model.backgroundtype == 'color' or model.backgroundtype == 'gradient'","x-default":"#ffffff"} */
+	public String getBgcolor() {
+		return bgcolor;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Color 2","x-form-type":"color","x-form-visible":"model.backgroundtype == 'gradient'","x-default":"#c0c0c0"} */
+	public String getColor2() {
+		return color2;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Full Width","x-form-type":"materialswitch"} */
+	public String getFullwidth() {
+		return fullwidth;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Full Height","x-form-type":"materialswitch"} */
+	public String getFullheight() {
+		return fullheight;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Top Padding","x-form-type":"range","x-form-min":0,"x-form-max":120,"x-form-visible":"model.fullheight != 'true'"} */
+	public String getToppadding() {
+		return toppadding;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Bottom Padding","x-form-type":"range","x-form-min":0,"x-form-max":120,"x-form-visible":"model.fullheight != 'true'"} */
+	public String getBottompadding() {
+		return bottompadding;
+	}
+
+
+//GEN]
+
+    //GEN[:CUSTOMGETTERS
+    //GEN]
+
+}
---
+++ themes/themeclean/fragments/articleblockquote/template.vue
@@ -0,0 +1,13 @@
+<template>
+  <themeclean-components-block v-bind:model="model">
+    <div class="col-12 col-md-8 border border-right-0 border-bottom-0 border-top-0">
+      <blockquote class="blockquote pl-3" v-html="model.text"></blockquote>
+    </div>
+  </themeclean-components-block>
+</template>
+
+<script>
+    export default {
+        props: ['model']
+    }
+</script>
