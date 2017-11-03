c8d585d1949431381b2683310a11841668bb8529
Li Qin <lq@headwire.com>
2017-11-03 11:48:01 -0700

    check in article columns components

---
+++ themes/themeclean/fragments/articlecolumns/htmltovue.js
@@ -0,0 +1,8 @@
+module.exports = {
+    convert: function($, f) {
+        f.wrap($, 'themeclean-components-block')
+        f.bindAttribute($.parent(),'model','model')
+        f.mapRichField( $.find('div').eq(0), 'model.textleft')
+        f.mapRichField( $.find('div').eq(1), 'model.textright')
+    }
+}
---
+++ themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/articlecolumns/template.vue
@@ -0,0 +1,15 @@
+<template>
+  <themeclean-components-block v-bind:model="model">
+    <div class="row col-12 d-flex justify-content-center">
+      <div class="col-12 col-md-4" v-html="model.textleft"></div>
+      <div class="col-12 col-md-4" v-html="model.textright"></div>
+    </div>
+  </themeclean-components-block>
+</template>
+
+<script>
+    export default {
+        props: ['model']
+    }
+</script>
+
