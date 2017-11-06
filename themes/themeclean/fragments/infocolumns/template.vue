358a32d70940e6e4d707c00b9349bdeafafddb03
Li Qin <lq@headwire.com>
2017-11-06 11:14:24 -0800

    info columns: check in

---
+++ themes/themeclean/fragments/infocolumns/template.vue
@@ -0,0 +1,22 @@
+<template>
+  <themeclean-components-block v-bind:model="model">
+    <div class="row col-12 col-md-10 align-items-center justify-content-start">
+      <div class="col-12 p-0">
+        <h2 class="text-uppercase" v-if="model.showtitle == 'true'"
+        v-html="model.title"></h2>
+        <h4 class="" v-if="model.showsubtitle == 'true'" v-html="model.subtitle"></h4>
+        <p v-if="model.showtext == 'true'" v-html="model.text"></p>
+      </div>
+      <div class="col-12 p-0" v-if="model.showbutton == 'true'">
+        <a class="btn btn-secondary btn-lg m-2" v-for="(item,i) in model.buttons"
+        :key="i" v-bind:href="item.buttonlink" v-html="item.buttontext" v-bind:style="`backgroundColor:${item.buttoncolor};borderColor:${item.buttoncolor};`"></a>
+      </div>
+    </div>
+  </themeclean-components-block>
+</template>
+
+<script>
+    export default {
+        props: ['model']
+    }
+</script>
