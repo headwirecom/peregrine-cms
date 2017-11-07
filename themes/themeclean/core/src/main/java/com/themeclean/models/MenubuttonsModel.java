c3e55b1f849e6e6c3ea113dd7ca613b807df6d82
Li Qin <lq@headwire.com>
2017-11-07 14:38:24 -0800

    update sling models

---
+++ themes/themeclean/core/src/main/java/com/themeclean/models/MenubuttonsModel.java
@@ -0,0 +1,99 @@
+package com.themeclean.models;
+
+import com.peregrine.nodetypes.models.AbstractComponent;
+import com.peregrine.nodetypes.models.IComponent;
+import com.peregrine.nodetypes.models.Container;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/*
+    //GEN[:DATA
+    {
+  "definitions": {
+    "Menubuttons": {
+      "type": "object",
+      "x-type": "component",
+      "properties": {
+        "buttons": {
+          "type": "string",
+          "x-source": "inject",
+          "x-form-label": "Buttons",
+          "x-form-type": "collection",
+          "properties": {
+            "buttontext": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Button Text",
+              "x-form-type": "text"
+            },
+            "buttonlink": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Button Link",
+              "x-form-type": "pathbrowser"
+            },
+            "buttoncolor": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Button Color",
+              "x-form-type": "color"
+            }
+          }
+        }
+      }
+    }
+  },
+  "name": "Menubuttons",
+  "componentPath": "themeclean/components/menubuttons",
+  "package": "com.themeclean.models",
+  "modelName": "Menubuttons",
+  "classNameParent": "AbstractComponent"
+}
+//GEN]
+*/
+
+//GEN[:DEF
+@Model(
+        adaptables = Resource.class,
+        resourceType = "themeclean/components/menubuttons",
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
+        adapters = IComponent.class
+)
+@Exporter(
+        name = "jackson",
+        extensions = "json"
+)
+
+//GEN]
+public class MenubuttonsModel extends AbstractComponent {
+
+    public MenubuttonsModel(Resource r) { super(r); }
+
+    //GEN[:INJECT
+    	/* {"type":"string","x-source":"inject","x-form-label":"Buttons","x-form-type":"collection","properties":{"buttontext":{"type":"string","x-source":"inject","x-form-label":"Button Text","x-form-type":"text"},"buttonlink":{"type":"string","x-source":"inject","x-form-label":"Button Link","x-form-type":"pathbrowser"},"buttoncolor":{"type":"string","x-source":"inject","x-form-label":"Button Color","x-form-type":"color"}}} */
+	@Inject
+	private List<IComponent> buttons;
+
+
+//GEN]
+
+    //GEN[:GETTERS
+    	/* {"type":"string","x-source":"inject","x-form-label":"Buttons","x-form-type":"collection","properties":{"buttontext":{"type":"string","x-source":"inject","x-form-label":"Button Text","x-form-type":"text"},"buttonlink":{"type":"string","x-source":"inject","x-form-label":"Button Link","x-form-type":"pathbrowser"},"buttoncolor":{"type":"string","x-source":"inject","x-form-label":"Button Color","x-form-type":"color"}}} */
+	public List<IComponent> getButtons() {
+		return buttons;
+	}
+
+
+//GEN]
+
+    //GEN[:CUSTOMGETTERS
+    //GEN]
+
+}
---
+++ themes/themeclean/core/src/main/java/com/themeclean/models/TextlinksModel.java
@@ -0,0 +1,93 @@
+package com.themeclean.models;
+
+import com.peregrine.nodetypes.models.AbstractComponent;
+import com.peregrine.nodetypes.models.IComponent;
+import com.peregrine.nodetypes.models.Container;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/*
+    //GEN[:DATA
+    {
+  "definitions": {
+    "Textlinks": {
+      "type": "object",
+      "x-type": "component",
+      "properties": {
+        "links": {
+          "type": "object",
+          "x-source": "inject",
+          "x-form-type": "collection",
+          "x-form-label": "Links",
+          "properties": {
+            "text": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Link Text",
+              "x-form-type": "text"
+            },
+            "link": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Link Url",
+              "x-form-type": "pathbrowser"
+            }
+          }
+        }
+      }
+    }
+  },
+  "name": "Textlinks",
+  "componentPath": "themeclean/components/textlinks",
+  "package": "com.themeclean.models",
+  "modelName": "Textlinks",
+  "classNameParent": "AbstractComponent"
+}
+//GEN]
+*/
+
+//GEN[:DEF
+@Model(
+        adaptables = Resource.class,
+        resourceType = "themeclean/components/textlinks",
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
+        adapters = IComponent.class
+)
+@Exporter(
+        name = "jackson",
+        extensions = "json"
+)
+
+//GEN]
+public class TextlinksModel extends AbstractComponent {
+
+    public TextlinksModel(Resource r) { super(r); }
+
+    //GEN[:INJECT
+    	/* {"type":"object","x-source":"inject","x-form-type":"collection","x-form-label":"Links","properties":{"text":{"type":"string","x-source":"inject","x-form-label":"Link Text","x-form-type":"text"},"link":{"type":"string","x-source":"inject","x-form-label":"Link Url","x-form-type":"pathbrowser"}}} */
+	@Inject
+	private List<IComponent> links;
+
+
+//GEN]
+
+    //GEN[:GETTERS
+    	/* {"type":"object","x-source":"inject","x-form-type":"collection","x-form-label":"Links","properties":{"text":{"type":"string","x-source":"inject","x-form-label":"Link Text","x-form-type":"text"},"link":{"type":"string","x-source":"inject","x-form-label":"Link Url","x-form-type":"pathbrowser"}}} */
+	public List<IComponent> getLinks() {
+		return links;
+	}
+
+
+//GEN]
+
+    //GEN[:CUSTOMGETTERS
+    //GEN]
+
+}
