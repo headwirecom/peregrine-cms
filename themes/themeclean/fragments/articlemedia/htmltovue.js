f6ff69fd0f6ca10e32b83b3de36535402a49c6f1
Li Qin <lq@headwire.com>
2017-11-07 13:43:47 -0800

    article media: commit

--- themes/themeclean/fragments/articlemedia/htmltovue.js
+++ themes/themeclean/fragments/articlemedia/htmltovue.js
@@ -1,5 +1,8 @@
 module.exports = {
     convert: function($, f) {
-        f.bindPath($)
+    	f.wrap($, 'themeclean-components-block')
+        f.bindAttribute($.parent(),'model','model')
+        f.replace( $.find('div').eq(0), '<themeclean-components-media v-bind:model="model"></themeclean-components-media>')
+        f.mapRichField( $.find('div').eq(0), "model.text")
     }
 }
--- themes/themeclean/fragments/articlemedia/template.vue
+++ themes/themeclean/fragments/articlemedia/template.vue
@@ -1,6 +1,11 @@
 <template>
-    <div v-bind:data-per-path="model.path" v-html="model.text">
+  <themeclean-components-block v-bind:model="model">
+    <div class="col-12 col-md-8 d-md-flex align-items-center">
+      <themeclean-components-media v-bind:model="model"></themeclean-components-media>
+      <div class="col-sm-12 col-md p-0 p-md-3"
+      v-html="model.text"></div>
     </div>
+  </themeclean-components-block>
 </template>
 
 <script>
@@ -8,4 +13,3 @@
         props: ['model']
     }
 </script>
-
