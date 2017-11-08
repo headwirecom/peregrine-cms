64c74c9053fd84f4d44ed6df45d880b44be2cae6
DJ Pelland <djp@headwire.com>
2017-11-08 11:32:18 -0800

    update articlecolumns

--- themes/themeclean/fragments/articlecolumns/template.vue
+++ themes/themeclean/fragments/articlecolumns/template.vue
@@ -1,8 +1,8 @@
 <template>
   <themeclean-components-block v-bind:model="model">
-    <div class="row col-12 d-flex justify-content-center">
-      <div class="col-12 col-md-4" v-html="model.textleft"></div>
-      <div class="col-12 col-md-4" v-html="model.textright"></div>
+    <div class="row col-12 col-md-8 d-flex justify-content-center">
+      <div class="col-12 col-md p-0 pr-md-3" v-html="model.textleft"></div>
+      <div class="col-12 col-md p-0 pl-md-3" v-html="model.textright"></div>
     </div>
   </themeclean-components-block>
 </template>
