4e02d46d9e0058c9e4f2da4df225ccbb7046aa24
DJ Pelland <djp@headwire.com>
2017-11-07 17:17:06 -0800

    implement video background

--- themes/themeclean/fragments/block/htmltovue.js
+++ themes/themeclean/fragments/block/htmltovue.js
@@ -4,7 +4,11 @@ module.exports = {
         f.bindAttribute( $, 'style', '[styles, sticky]', false)
         f.bindAttribute( $, 'data-per-path', 'model.path')
 
-        f.bindAttribute( $.find('div').first(), 'class', "model.fullwidth === 'true' ? 'container-fluid' : 'container'")
-        $.find('div>div').first().append( '<slot>')
+        f.addIf( $.find('div.embed-responsive').first(), "model.backgroundtype == 'video' && model.bgvideo" )
+        f.bindAttribute( $.find('iframe').first(), 'src', "model.bgvideo + '?autoplay=1&loop=1&controls=0&mute=1'" )
+        f.addStyle( $.find('div.embed-responsive').first(), 'position', "'absolute'")
+        f.addStyle( $.find('div.embed-responsive').first(), 'pointer-events', "'none'")
+        f.bindAttribute( $.find('div.container').eq(), 'class', "model.fullwidth === 'true' ? 'container-fluid' : 'container'")
+        $.find('div.container>div').first().append( '<slot>')
     }
 }
