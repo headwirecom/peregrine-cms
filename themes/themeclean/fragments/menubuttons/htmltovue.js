9ab19b7af126671203d8467dfbfb904cec4894c6
Li Qin <lq@headwire.com>
2017-11-10 11:21:17 -0800

    menubuttons: add button style

--- themes/themeclean/fragments/menubuttons/htmltovue.js
+++ themes/themeclean/fragments/menubuttons/htmltovue.js
@@ -4,5 +4,7 @@ module.exports = {
     	f.addFor($.find('a').first(), 'model.buttons')
         f.bindAttribute($.find('a').first(), 'href', 'item.buttonlink')
         f.mapRichField($.find('span').eq(1), "item.buttontext")
+        f.addStyle($.find('a.btn').first(), 'backgroundColor', 'item.buttoncolor')
+        f.addStyle($.find('a.btn').first(), 'borderColor', 'item.buttoncolor')
     }
 }
--- themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/menubuttons/template.vue
+++ themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/menubuttons/template.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <a class="btn btn-primary" v-for="(item,i) in model.buttons" :key="i"
-    v-bind:href="item.buttonlink">
+    v-bind:href="item.buttonlink" v-bind:style="`backgroundColor:${item.buttoncolor};borderColor:${item.buttoncolor};`">
       <span></span>
       <span v-html="item.buttontext"></span>
     </a>
