49687a91fce429896489abec59aa56de42c70e0f
Li Qin <lq@headwire.com>
2017-11-13 13:33:59 -0800

    articlequote: add isEditAndEmpty function

--- themes/themeclean/fragments/articlequote/htmltovue.js
+++ themes/themeclean/fragments/articlequote/htmltovue.js
@@ -2,6 +2,7 @@ module.exports = {
     convert: function($, f) {
     	f.wrap($, 'themeclean-components-block')
         f.bindAttribute($.parent(),'model','model')
+        f.addIf($.find('.perIsEditAndEmpty').first(), 'isEditAndEmpty')
         f.mapRichField( $.find('div.text-center'), 'model.text')
         f.addStyle( $.find('hr'), 'width', '`${model.linewidth}%`')
     }
--- themes/themeclean/fragments/articlequote/template.vue
+++ themes/themeclean/fragments/articlequote/template.vue
@@ -2,6 +2,7 @@
   <themeclean-components-block v-bind:model="model">
     <div class="col-12 col-md-8">
       <hr class="line" v-bind:style="`width:${`${model.linewidth}%`};`">
+      <div class="perIsEditAndEmpty" v-if="isEditAndEmpty">no content defined for component</div>
       <div class="text-center" v-html="model.text"></div>
       <hr class="line" v-bind:style="`width:${`${model.linewidth}%`};`">
     </div>
@@ -10,6 +11,12 @@
 
 <script>
     export default {
-        props: ['model']
+        props: ['model'],
+        computed: {
+        	isEditAndEmpty() {
+                if(!$peregrineApp.isAuthorMode()) return false
+                return !(this.model.text)
+            }
+        }
     }
 </script>
