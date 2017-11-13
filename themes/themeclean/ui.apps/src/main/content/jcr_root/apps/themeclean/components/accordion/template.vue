756099e5c607242665986fdc6715c48d70a0f910
DJ Pelland <djp@headwire.com>
2017-11-13 09:29:53 -0800

    fix accordion styles

--- themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/accordion/template.vue
+++ themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/accordion/template.vue
@@ -2,13 +2,13 @@
   <themeclean-components-block v-bind:model="model">
     <div class="col-12 col-md-8" v-bind:id="`accordion${_uid}`">
       <h2 class="text-center" v-if="model.showtitle == 'true'">{{model.title}}</h2>
-      <div class="item card border-0" v-for="(item,i) in model.tabs"
-      :key="i">
-        <a class="card-header border rounded-0" data-toggle="collapse" aria-expanded="true"
-        v-bind:data-parent="`#accordion${_uid}`" v-bind:href="`#accordion${_uid}${i}`"
+      <div class="item card bg-transparent border-0 rounded-0"
+      v-for="(item,i) in model.tabs" :key="i">
+        <a aria-expanded="false" class="card-header border rounded-0 text-dark"
+        data-toggle="collapse" v-bind:data-parent="`#accordion${_uid}`" v-bind:href="`#accordion${_uid}${i}`"
         v-bind:aria-controls="`accordion${_uid}${i}`">{{item.title}}</a>
-        <div class="collapse" role="tabpanel" v-bind:id="`accordion${_uid}${i}`">
-          <div class="card-body" v-html="item.text"></div>
+        <div class="collapse rounded-0" role="tabpanel" v-bind:id="`accordion${_uid}${i}`">
+          <div class="card-body rounded-0" v-html="item.text"></div>
         </div>
       </div>
     </div>
