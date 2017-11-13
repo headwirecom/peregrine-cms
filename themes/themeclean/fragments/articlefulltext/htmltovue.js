e548d2248296b3e195bc5cda91826fc9b797cf90
Li Qin <lq@headwire.com>
2017-11-13 12:10:28 -0800

    article full text: add isEditandEmpty function

--- themes/themeclean/fragments/articlefulltext/htmltovue.js
+++ themes/themeclean/fragments/articlefulltext/htmltovue.js
@@ -2,6 +2,7 @@ module.exports = {
     convert: function($, f) {
     	f.wrap($, 'themeclean-components-block')
         f.bindAttribute($.parent(),'model','model')
-        f.mapRichField( $ , 'model.text')
+        f.addIf($.find('.perIsEditAndEmpty').first(), 'isEditAndEmpty')
+        f.mapRichField( $.find('div.col-12') , 'model.text')
     }
 }
--- themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/articlefulltext/template.vue
+++ themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/articlefulltext/template.vue
@@ -1,11 +1,20 @@
 <template>
   <themeclean-components-block v-bind:model="model">
-    <div class="col-12 col-md-8" v-html="model.text"></div>
+    <div>
+      <div class="perIsEditAndEmpty" v-if="isEditAndEmpty">no content defined for component</div>
+      <div class="col-12 col-md-8" v-html="model.text"></div>
+    </div>
   </themeclean-components-block>
 </template>
 
 <script>
     export default {
-        props: ['model']
+        props: ['model'],
+        computed: {
+        	isEditAndEmpty() {
+                if(!$peregrineApp.isAuthorMode()) return false
+                return !(this.model.text)
+            }
+        }
     }
 </script>
