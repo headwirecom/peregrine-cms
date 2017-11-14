79ae328db313e4a4dbd96e49331a9fab06f07e82
DJ Pelland <djp@headwire.com>
2017-11-14 14:18:24 -0800

    fix alignment of nav for mobile

--- themes/themeclean/fragments/menubuttons/template.vue
+++ themes/themeclean/fragments/menubuttons/template.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="ml-3">
-    <a class="btn btn-lg btn-primary" v-for="(item,i) in model.buttons" :key="i"
-    v-bind:href="item.buttonlink" v-bind:style="`backgroundColor:${item.buttoncolor};borderColor:${item.buttoncolor};`">
+  <div class="d-flex justify-content-center flex-column flex-sm-row">
+    <a class="btn btn-lg btn-primary mb-2 m-sm-2" v-for="(item,i) in model.buttons"
+    :key="i" v-bind:href="item.buttonlink" v-bind:style="`backgroundColor:${item.buttoncolor};borderColor:${item.buttoncolor};`">
       <span></span>
       <span v-html="item.buttontext"></span>
     </a>
