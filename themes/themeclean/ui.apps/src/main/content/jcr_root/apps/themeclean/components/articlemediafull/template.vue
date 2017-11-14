694ea5098dad7c415a2615a59967d2cd6bb1022d
Li Qin <lq@headwire.com>
2017-11-13 16:10:18 -0800

    article media full: add isEditAndEmpty block

--- themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/articlemediafull/template.vue
+++ themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/articlemediafull/template.vue
@@ -1,6 +1,7 @@
 <template>
   <themeclean-components-block v-bind:model="model">
     <div class="col-12 col-md-8 row justify-content-center">
+      <div class="perIsEditAndEmpty" v-if="isEditAndEmpty">no content defined for component</div>
       <themeclean-components-media :model="model"></themeclean-components-media>
     </div>
   </themeclean-components-block>
@@ -8,6 +9,12 @@
 
 <script>
     export default {
-        props: ['model']
+        props: ['model'],
+        computed: {
+        	isEditAndEmpty() {
+                if(!$peregrineApp.isAuthorMode()) return false
+                return !(this.model.imagesrc || this.model.videosrc)
+            }
+        }
     }
 </script>
