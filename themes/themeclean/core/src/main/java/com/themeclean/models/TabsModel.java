2835a057512c932e0abc4cf8dd4e9ec6f3ef6613
Li Qin <lq@headwire.com>
2017-11-14 10:47:31 -0800

    tabs: add sample.json

--- themes/themeclean/core/src/main/java/com/themeclean/models/TabsModel.java
+++ themes/themeclean/core/src/main/java/com/themeclean/models/TabsModel.java
@@ -156,7 +156,7 @@ import javax.inject.Named;
               "x-form-label": "Bottom Padding",
               "x-form-type": "range",
               "x-form-min": 0,
-              "x-form-max": 120,
+              "x-form-max": 150,
               "x-form-visible": "model.fullheight != 'true'"
             }
           }
@@ -245,7 +245,7 @@ public class TabsModel extends AbstractComponent {
 	@Inject
 	private String toppadding;
 
-	/* {"type":"string","x-source":"inject","x-form-label":"Bottom Padding","x-form-type":"range","x-form-min":0,"x-form-max":120,"x-form-visible":"model.fullheight != 'true'"} */
+	/* {"type":"string","x-source":"inject","x-form-label":"Bottom Padding","x-form-type":"range","x-form-min":0,"x-form-max":150,"x-form-visible":"model.fullheight != 'true'"} */
 	@Inject
 	private String bottompadding;
 
@@ -313,7 +313,7 @@ public class TabsModel extends AbstractComponent {
 		return toppadding;
 	}
 
-	/* {"type":"string","x-source":"inject","x-form-label":"Bottom Padding","x-form-type":"range","x-form-min":0,"x-form-max":120,"x-form-visible":"model.fullheight != 'true'"} */
+	/* {"type":"string","x-source":"inject","x-form-label":"Bottom Padding","x-form-type":"range","x-form-min":0,"x-form-max":150,"x-form-visible":"model.fullheight != 'true'"} */
 	public String getBottompadding() {
 		return bottompadding;
 	}
