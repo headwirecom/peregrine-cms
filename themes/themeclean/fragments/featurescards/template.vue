833670616044e901e95e46e3cd7122cc047f24f5
Li Qin <lq@headwire.com>
2017-11-15 13:57:02 -0800

    featurescards, featurescolumns: add f.pathToUrl()

--- themes/themeclean/fragments/featurescards/template.vue
+++ themes/themeclean/fragments/featurescards/template.vue
@@ -4,14 +4,14 @@
       <div class="perIsEditAndEmpty" v-if="isEditAndEmpty">no content defined for component</div>
       <div class="card p-0 col-12 col-md border-0"
       v-for="(item,i) in model.cards" :key="i" v-bind:style="`background-color:${model.cardcolor};`">
-        <img class="card-img pb-1" v-bind:src="item.image">
+        <img class="card-img pb-1" v-bind:src="$helper.pathToUrl(item.image)">
         <div class="card-body">
           <h5 class="card-title" v-if="model.showtitle == 'true'" v-html="item.title"
           v-bind:style="`color:${item.color};`"></h5>
           <p v-if="model.showtext == 'true'" v-html="item.text"></p>
           <div class="text-center">
-            <a class="btn btn-lg btn-primary" v-bind:href="item.buttonlink" v-html="item.buttontext"
-            v-bind:style="`backgroundColor:${item.buttoncolor};borderColor:${item.buttoncolor};`"></a>
+            <a class="btn btn-lg btn-primary" v-bind:href="$helper.pathToUrl(item.buttonlink)"
+            v-html="item.buttontext" v-bind:style="`backgroundColor:${item.buttoncolor};borderColor:${item.buttoncolor};`"></a>
           </div>
         </div>
       </div>
