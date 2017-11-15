187315ee0d13e8e73d5057ef8f37e1b4b27452e9
Li Qin <lq@headwire.com>
2017-11-15 13:11:49 -0800

    footer: replace url type with pathbrowser, set browser

--- themes/themeclean/core/src/main/java/com/themeclean/models/FooterModel.java
+++ themes/themeclean/core/src/main/java/com/themeclean/models/FooterModel.java
@@ -32,7 +32,8 @@ import javax.inject.Named;
           "type": "string",
           "x-source": "inject",
           "x-form-label": "Logo Url",
-          "x-form-type": "url"
+          "x-form-type": "pathbrowser",
+          "x-form-browserRoot": "/content/sites"
         },
         "logosize": {
           "type": "string",
@@ -335,7 +336,7 @@ public class FooterModel extends AbstractComponent {
 	@Inject
 	private String logo;
 
-	/* {"type":"string","x-source":"inject","x-form-label":"Logo Url","x-form-type":"url"} */
+	/* {"type":"string","x-source":"inject","x-form-label":"Logo Url","x-form-type":"pathbrowser","x-form-browserRoot":"/content/sites"} */
 	@Inject
 	private String logourl;
 
@@ -457,7 +458,7 @@ public class FooterModel extends AbstractComponent {
 		return logo;
 	}
 
-	/* {"type":"string","x-source":"inject","x-form-label":"Logo Url","x-form-type":"url"} */
+	/* {"type":"string","x-source":"inject","x-form-label":"Logo Url","x-form-type":"pathbrowser","x-form-browserRoot":"/content/sites"} */
 	public String getLogourl() {
 		return logourl;
 	}
