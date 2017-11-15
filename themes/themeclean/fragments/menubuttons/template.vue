cb771a33b06c82e776246e13bc1d2d9cc44454c8
Li Qin <lq@headwire.com>
2017-11-15 13:42:09 -0800

    footer, menubuttons, textlinks: add pathToUrl function

--- themes/themeclean/fragments/menubuttons/template.vue
+++ themes/themeclean/fragments/menubuttons/template.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex justify-content-center flex-column flex-sm-row">
     <a class="btn btn-lg btn-primary mb-2 m-sm-2" v-for="(item,i) in model.buttons"
-    :key="i" v-bind:href="item.buttonlink" v-bind:style="`backgroundColor:${item.buttoncolor};borderColor:${item.buttoncolor};`">
+    :key="i" v-bind:href="$helper.pathToUrl(item.buttonlink)" v-bind:style="`backgroundColor:${item.buttoncolor};borderColor:${item.buttoncolor};`">
       <span></span>
       <span v-html="item.buttontext"></span>
     </a>
--- themes/themeclean/fragments/textlinks/template.vue
+++ themes/themeclean/fragments/textlinks/template.vue
@@ -1,7 +1,8 @@
 <template>
   <ul class="navbar-nav nav-dropdown text-right">
     <li class="nav-item" v-for="(item,i) in model.links" :key="i">
-      <a class="nav-link" v-bind:href="item.link" v-html="item.text" v-bind:class="model.colorscheme === 'light' ? 'text-dark':'text-light'"></a>
+      <a class="nav-link" v-bind:href="$helper.pathToUrl(item.link)" v-html="item.text"
+      v-bind:class="model.colorscheme === 'light' ? 'text-dark':'text-light'"></a>
     </li>
   </ul>
 </template>
