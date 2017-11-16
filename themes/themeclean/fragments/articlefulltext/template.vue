2774611182f008c103907a4a6e84e0bacc9d2a14
Li Qin <lq@headwire.com>
2017-11-16 12:39:19 -0800

    articlefulltext: update isEditAndEmpty

--- themes/themeclean/fragments/articlefulltext/template.vue
+++ themes/themeclean/fragments/articlefulltext/template.vue
@@ -13,7 +13,8 @@
         computed: {
         	isEditAndEmpty() {
                 if(!$peregrineApp.isAuthorMode()) return false
-                return this.model.text == '<p><br></p>'
+                //return this.model.text == '<p><br></p>'
+                return this.$helper.areAllEmpty(this.model.text)
             }
         }
     }
