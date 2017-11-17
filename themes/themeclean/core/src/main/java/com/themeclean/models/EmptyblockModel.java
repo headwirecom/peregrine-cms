ade5ca9b54649c90b29fd89e49051df4f8921c59
DJ Pelland <djp@headwire.com>
2017-11-17 14:00:53 -0800

    add empty block component

---
+++ themes/themeclean/core/src/main/java/com/themeclean/models/EmptyblockModel.java
@@ -0,0 +1,78 @@
+package com.themeclean.models;
+
+import com.peregrine.nodetypes.models.AbstractComponent;
+import com.peregrine.nodetypes.models.IComponent;
+import com.peregrine.nodetypes.models.Container;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/*
+    //GEN[:DATA
+    {
+  "definitions": {
+    "Emptyblock": {
+      "type": "object",
+      "x-type": "component",
+      "properties": {
+        "text": {
+          "type": "string",
+          "x-source": "inject",
+          "x-form-type": "texteditor"
+        }
+      }
+    }
+  },
+  "name": "Emptyblock",
+  "componentPath": "themeclean/components/emptyblock",
+  "package": "com.themeclean.models",
+  "modelName": "Emptyblock",
+  "classNameParent": "AbstractComponent"
+}
+//GEN]
+*/
+
+//GEN[:DEF
+@Model(
+        adaptables = Resource.class,
+        resourceType = "themeclean/components/emptyblock",
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
+        adapters = IComponent.class
+)
+@Exporter(
+        name = "jackson",
+        extensions = "json"
+)
+
+//GEN]
+public class EmptyblockModel extends AbstractComponent {
+
+    public EmptyblockModel(Resource r) { super(r); }
+
+    //GEN[:INJECT
+    	/* {"type":"string","x-source":"inject","x-form-type":"texteditor"} */
+	@Inject
+	private String text;
+
+
+//GEN]
+
+    //GEN[:GETTERS
+    	/* {"type":"string","x-source":"inject","x-form-type":"texteditor"} */
+	public String getText() {
+		return text;
+	}
+
+
+//GEN]
+
+    //GEN[:CUSTOMGETTERS
+    //GEN]
+
+}
