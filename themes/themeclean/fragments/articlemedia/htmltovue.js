42e4e8b14263ef0dbeaf210ba3bd63880222175f
DJ Pelland <djp@headwire.com>
2017-11-17 11:51:13 -0800

    fix width adjustment for articlemedia

--- themes/themeclean/fragments/articlemedia/htmltovue.js
+++ themes/themeclean/fragments/articlemedia/htmltovue.js
@@ -2,6 +2,7 @@ module.exports = {
     convert: function($, f) {
     	f.wrap($, 'themeclean-components-block')
         f.bindAttribute($.parent(),'model','model')
+        f.bindAttribute($.find('div').first(), 'style', "{flexBasis: `${model.mediawidth}%`}")
         f.bindAttribute($,  'class', "model.reverselayout === 'true' ? 'flex-row': 'flex-row-reverse'", false)
         f.replace( $.find('div.col-md-8').eq(0), '<themeclean-components-media v-bind:model="model"></themeclean-components-media>')
         f.mapRichField( $.find('div').eq(1), "model.text")
