cab092b669119a6f90c180738efdd69c02fb3b05
Byron Ponce <byron@polished-web.com>
2017-11-17 11:27:03 -0800

    fix undefined value

--- admin-base/ui.apps/src/main/content/jcr_root/apps/field/material-datetime/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/field/material-datetime/template.vue
@@ -1,13 +1,18 @@
 <template>
-		<datetime v-model="value" type="datetime" placeholder="Select datetime"></datetime>
+		<datetime v-model="sanitizedValue" type="datetime" placeholder="Select datetime"></datetime>
 </template>
 
 <script>	
 	export default {
 		mixins: [ VueFormGenerator.abstractField ],
-	  data: function(){
-			return {
-				dateTime: '2017-09-25T07:00:00.000Z'
+		computed: {
+			sanitizedValue: {
+				get () {
+      		return this.value ? this.value : ''
+				},
+				set (newValue) {
+					this.value = newValue
+				}
 			}
 		}
 	}
