6146908fd9a6dcf3c8b399dce68047eb5e6b3a39
DJ Pelland <djp@headwire.com>
2017-11-20 08:41:18 -0800

    convert footer to columns

--- themes/themeclean/core/src/main/java/com/themeclean/models/FooterModel.java
+++ themes/themeclean/core/src/main/java/com/themeclean/models/FooterModel.java
@@ -21,6 +21,12 @@ import javax.inject.Named;
       "type": "object",
       "x-type": "component",
       "properties": {
+        "showlogo": {
+          "type": "string",
+          "x-source": "inject",
+          "x-form-label": "Show Logo",
+          "x-form-type": "materialswitch"
+        },
         "logo": {
           "type": "string",
           "x-source": "inject",
@@ -41,71 +47,34 @@ import javax.inject.Named;
           "x-form-label": "Logo Size",
           "x-form-type": "range",
           "x-form-min": 1,
-          "x-form-max": 10
+          "x-form-max": 300
         },
-        "title1": {
+        "columns": {
           "type": "string",
           "x-source": "inject",
-          "x-form-label": "Column 1 Title ",
-          "x-form-type": "text"
-        },
-        "text1": {
-          "type": "string",
-          "x-source": "inject",
-          "x-form-label": "Column 1 Text",
-          "x-form-type": "texteditor"
-        },
-        "title2": {
-          "type": "string",
-          "x-source": "inject",
-          "x-form-label": "Column 2 Title",
-          "x-form-type": "text"
-        },
-        "text2": {
-          "type": "string",
-          "x-source": "inject",
-          "x-form-label": "Column 2 Text",
-          "x-form-type": "texteditor"
+          "x-form-label": "Columns",
+          "x-form-type": "collection",
+          "x-form-multifield": "true",
+          "properties": {
+            "title": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Title",
+              "x-form-type": "text"
+            },
+            "text": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Text",
+              "x-form-type": "texteditor"
+            }
+          }
         },
         "copyright": {
           "type": "string",
           "x-source": "inject",
           "x-form-label": "Copyright Text",
           "x-form-type": "text"
-        },
-        "linksref": {
-          "x-form-type": "reference",
-          "type": "object",
-          "x-type": "component",
-          "properties": {
-            "title": {
-              "type": "string",
-              "x-source": "inject",
-              "x-form-label": "Links Section Title",
-              "x-form-type": "text"
-            },
-            "links": {
-              "type": "object",
-              "x-source": "inject",
-              "x-form-type": "collection",
-              "x-form-label": "Links",
-              "properties": {
-                "text": {
-                  "type": "string",
-                  "x-source": "inject",
-                  "x-form-label": "Link Text",
-                  "x-form-type": "text"
-                },
-                "link": {
-                  "type": "string",
-                  "x-source": "inject",
-                  "x-form-label": "Link Url",
-                  "x-form-type": "pathbrowser",
-                  "x-form-browserRoot": "/content/sites"
-                }
-              }
-            }
-          }
         },
         "socialref": {
           "x-form-type": "reference",
@@ -336,7 +305,11 @@ public class FooterModel extends AbstractComponent {
     public FooterModel(Resource r) { super(r); }
 
     //GEN[:INJECT
-    	/* {"type":"string","x-source":"inject","x-form-label":"Logo","x-form-type":"pathbrowser","x-form-browserRoot":"/content/assets"} */
+    	/* {"type":"string","x-source":"inject","x-form-label":"Show Logo","x-form-type":"materialswitch"} */
+	@Inject
+	private String showlogo;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Logo","x-form-type":"pathbrowser","x-form-browserRoot":"/content/assets"} */
 	@Inject
 	private String logo;
 
@@ -344,37 +317,17 @@ public class FooterModel extends AbstractComponent {
 	@Inject
 	private String logourl;
 
-	/* {"type":"string","x-source":"inject","x-form-label":"Logo Size","x-form-type":"range","x-form-min":1,"x-form-max":10} */
+	/* {"type":"string","x-source":"inject","x-form-label":"Logo Size","x-form-type":"range","x-form-min":1,"x-form-max":300} */
 	@Inject
 	private String logosize;
 
-	/* {"type":"string","x-source":"inject","x-form-label":"Column 1 Title ","x-form-type":"text"} */
+	/* {"type":"string","x-source":"inject","x-form-label":"Columns","x-form-type":"collection","x-form-multifield":"true","properties":{"title":{"type":"string","x-source":"inject","x-form-label":"Title","x-form-type":"text"},"text":{"type":"string","x-source":"inject","x-form-label":"Text","x-form-type":"texteditor"}}} */
 	@Inject
-	private String title1;
-
-	/* {"type":"string","x-source":"inject","x-form-label":"Column 1 Text","x-form-type":"texteditor"} */
-	@Inject
-	private String text1;
-
-	/* {"type":"string","x-source":"inject","x-form-label":"Column 2 Title","x-form-type":"text"} */
-	@Inject
-	private String title2;
-
-	/* {"type":"string","x-source":"inject","x-form-label":"Column 2 Text","x-form-type":"texteditor"} */
-	@Inject
-	private String text2;
+	private List<IComponent> columns;
 
 	/* {"type":"string","x-source":"inject","x-form-label":"Copyright Text","x-form-type":"text"} */
 	@Inject
 	private String copyright;
-
-	/* {"type":"string","x-source":"inject","x-form-label":"Links Section Title","x-form-type":"text"} */
-	@Inject
-	private String title;
-
-	/* {"type":"object","x-source":"inject","x-form-type":"collection","x-form-label":"Links","properties":{"text":{"type":"string","x-source":"inject","x-form-label":"Link Text","x-form-type":"text"},"link":{"type":"string","x-source":"inject","x-form-label":"Link Url","x-form-type":"pathbrowser","x-form-browserRoot":"/content/sites"}}} */
-	@Inject
-	private List<IComponent> links;
 
 	/* {"type":"string","x-source":"inject","x-form-label":"Icon Color","x-default":"#000000","x-form-type":"color"} */
 	@Inject
@@ -457,7 +410,12 @@ public class FooterModel extends AbstractComponent {
 //GEN]
 
     //GEN[:GETTERS
-    	/* {"type":"string","x-source":"inject","x-form-label":"Logo","x-form-type":"pathbrowser","x-form-browserRoot":"/content/assets"} */
+    	/* {"type":"string","x-source":"inject","x-form-label":"Show Logo","x-form-type":"materialswitch"} */
+	public String getShowlogo() {
+		return showlogo;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Logo","x-form-type":"pathbrowser","x-form-browserRoot":"/content/assets"} */
 	public String getLogo() {
 		return logo;
 	}
@@ -467,44 +425,19 @@ public class FooterModel extends AbstractComponent {
 		return logourl;
 	}
 
-	/* {"type":"string","x-source":"inject","x-form-label":"Logo Size","x-form-type":"range","x-form-min":1,"x-form-max":10} */
+	/* {"type":"string","x-source":"inject","x-form-label":"Logo Size","x-form-type":"range","x-form-min":1,"x-form-max":300} */
 	public String getLogosize() {
 		return logosize;
 	}
 
-	/* {"type":"string","x-source":"inject","x-form-label":"Column 1 Title ","x-form-type":"text"} */
-	public String getTitle1() {
-		return title1;
-	}
-
-	/* {"type":"string","x-source":"inject","x-form-label":"Column 1 Text","x-form-type":"texteditor"} */
-	public String getText1() {
-		return text1;
-	}
-
-	/* {"type":"string","x-source":"inject","x-form-label":"Column 2 Title","x-form-type":"text"} */
-	public String getTitle2() {
-		return title2;
-	}
-
-	/* {"type":"string","x-source":"inject","x-form-label":"Column 2 Text","x-form-type":"texteditor"} */
-	public String getText2() {
-		return text2;
+	/* {"type":"string","x-source":"inject","x-form-label":"Columns","x-form-type":"collection","x-form-multifield":"true","properties":{"title":{"type":"string","x-source":"inject","x-form-label":"Title","x-form-type":"text"},"text":{"type":"string","x-source":"inject","x-form-label":"Text","x-form-type":"texteditor"}}} */
+	public List<IComponent> getColumns() {
+		return columns;
 	}
 
 	/* {"type":"string","x-source":"inject","x-form-label":"Copyright Text","x-form-type":"text"} */
 	public String getCopyright() {
 		return copyright;
-	}
-
-	/* {"type":"string","x-source":"inject","x-form-label":"Links Section Title","x-form-type":"text"} */
-	public String getTitle() {
-		return title;
-	}
-
-	/* {"type":"object","x-source":"inject","x-form-type":"collection","x-form-label":"Links","properties":{"text":{"type":"string","x-source":"inject","x-form-label":"Link Text","x-form-type":"text"},"link":{"type":"string","x-source":"inject","x-form-label":"Link Url","x-form-type":"pathbrowser","x-form-browserRoot":"/content/sites"}}} */
-	public List<IComponent> getLinks() {
-		return links;
 	}
 
 	/* {"type":"string","x-source":"inject","x-form-label":"Icon Color","x-default":"#000000","x-form-type":"color"} */
--- themes/themeclean/fragments/footer/htmltovue.js
+++ themes/themeclean/fragments/footer/htmltovue.js
@@ -2,14 +2,13 @@ module.exports = {
     convert: function($, f) {
         //f.bindPath($)
         f.wrap($, 'themeclean-components-block')
-        f.bindAttribute($.parent(),'model','model')
+		f.bindAttribute($.parent(),'model','model')
+		f.addIf($.find('div.col-12').first(), "model.showlogo === 'true'")
     	f.bindAttribute($.find('a').first(), 'href', f.pathToUrl('model.logourl'))
     	f.bindAttribute($.find('img').first(), 'src', f.pathToUrl('model.logo'))
-    	f.addStyle($.find('img').first(), 'height', 'parseInt(model.logosize)', 'rem')
-    	f.mapRichField($.find('h5').first(), "model.title1")
-    	f.mapRichField($.find('p').first(), "model.text1")
-    	f.mapRichField($.find('h5').eq(1), "model.title2")
-    	f.mapRichField($.find('p').eq(1), "model.text2")
-    	f.mapRichField($.find('p').eq(2), "model.copyright")
+    	f.addStyle($.find('img').first(), 'height', 'parseInt(model.logosize)', 'px')
+		f.mapRichField($.find('p').eq(1), "model.copyright")
+		f.addFor( $.find('div.col-12').eq(1), "model.columns")
+		f.mapRichField($.find('div.col-12').eq(1), "item.text")
     }
 }
