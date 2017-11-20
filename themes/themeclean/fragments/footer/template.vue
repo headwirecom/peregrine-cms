1308048e6dd3506737ed3d3acacb5fd4dd40442f
DJ Pelland <djp@headwire.com>
2017-11-20 08:41:18 -0800

    convert footer to columns

--- themes/themeclean/fragments/footer/template.vue
+++ themes/themeclean/fragments/footer/template.vue
@@ -2,21 +2,14 @@
   <themeclean-components-block v-bind:model="model">
     <footer class="col-12">
       <div class="row justify-content-between text-center text-md-left">
-        <div class="col-12 col-md-3">
+        <div class="col-12 col-md-auto" v-if="model.showlogo === 'true'">
           <a v-bind:href="$helper.pathToUrl(model.logourl)">
             <img class="mb-3" alt="logo" v-bind:src="$helper.pathToUrl(model.logo)"
-            v-bind:style="`height:${parseInt(model.logosize)}rem;`">
+            v-bind:style="`height:${parseInt(model.logosize)}px;`">
           </a>
         </div>
-        <div class="col-12 col-md-3">
-          <h5 class="" v-html="model.title1"></h5>
-          <p v-html="model.text1"></p>
-        </div>
-        <div class="col-12 col-md-3">
-          <h5 class="" v-html="model.title2"></h5>
-          <p v-html="model.text2"></p>
-        </div>
-        <themeclean-components-textlinksvertical v-bind:model="model"></themeclean-components-textlinksvertical>
+        <div class="col-12 col-md" v-for="(item,i) in model.columns" :key="i"
+        v-html="item.text"></div>
       </div>
       <hr>
       <div class="row text-center text-md-left">
