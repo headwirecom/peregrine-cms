82b46d6757d172b2d7854f5b207b0b63a8ebaed7
DJ Pelland <djp@headwire.com>
2017-11-21 09:17:43 -0800

    cleanup

--- themes/themeclean/fragments/infocolumns/htmltovue.js
+++ themes/themeclean/fragments/infocolumns/htmltovue.js
@@ -1,14 +1,17 @@
 module.exports = {
     convert: function($, f) {
-        //f.bindPath($)
     	f.wrap($, 'themeclean-components-block')
         f.bindAttribute($.parent(),'model','model')
+
+        //Text
     	f.addIf($.find('h2').first(), 'model.showtitle == \'true\'')
         f.mapRichField($.find('h2').first(), "model.title")
         f.addIf($.find('h4').first(), 'model.showsubtitle == \'true\'')
         f.mapRichField($.find('h4').first(), "model.subtitle")
         f.addIf($.find('p').first(), 'model.showtext == \'true\'')
         f.mapRichField($.find('p').first(), "model.text")
+
+        //Buttons
         f.addIf($.find('div.col-12.p-0').eq(1), 'model.showbutton == \'true\'')
         f.bindAttribute($.find('div.col-12.p-0').eq(1), 'class', '`justify-content-${model.alignbuttons}`',false)
         f.addFor($.find('div.col-12>a').first(), 'model.buttons')
@@ -18,6 +21,7 @@ module.exports = {
         f.addStyle($.find('a.btn').first(), 'borderColor', 'item.buttoncolor')
         f.addStyle($.find('a.btn').first(), 'margin-left', "i == 0 ? 0 : '0.5rem'")
         f.addStyle($.find('a.btn').first(), 'margin-right', "i == model.buttons.length-1 ? 0: '0.5rem'")
+
         $.append('<div v-if="isEditAndEmpty">no content defined for component</div>')
     }
 }
