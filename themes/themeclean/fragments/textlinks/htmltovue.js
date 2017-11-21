8ae362f9615fd90a83f43669d755a5ff3fc26961
DJ Pelland <djp@headwire.com>
2017-11-21 12:36:45 -0800

    style text link based on active path

--- themes/themeclean/fragments/textlinks/htmltovue.js
+++ themes/themeclean/fragments/textlinks/htmltovue.js
@@ -5,5 +5,6 @@ module.exports = {
         f.bindAttribute($.find('a').first(), 'href', f.pathToUrl('item.link'))
         f.mapRichField($.find('a').first(), "item.text")
         f.bindAttribute($.find('a').first(), 'class', "model.colorscheme === 'light' ? 'text-dark':'text-light'", false)
+        f.addStyle( $.find('a').first(), 'font-weight', "$root.path === item.link ? 'bold': 'normal'")
     }
 }
--- themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/textlinks/template.vue
+++ themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/textlinks/template.vue
@@ -2,14 +2,20 @@
   <ul class="navbar-nav nav-dropdown text-right">
     <li class="nav-item" v-for="(item,i) in model.links" :key="i">
       <a class="nav-link" v-bind:href="$helper.pathToUrl(item.link)" v-html="item.text"
-      v-bind:class="model.colorscheme === 'light' ? 'text-dark':'text-light'"></a>
+      v-bind:class="model.colorscheme === 'light' ? 'text-dark':'text-light'"
+      v-bind:style="`font-weight:${$root.path === item.link ? 'bold': 'normal'};`"></a>
     </li>
   </ul>
 </template>
 
 <script>
     export default {
-        props: ['model']
+        props: ['model'],
+        computed: {
+          activeClass (url) {
+            return {'per-active': this.$data.path === url}
+          }
+        }
     }
 </script>
 
