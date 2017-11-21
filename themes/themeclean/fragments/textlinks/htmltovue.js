000f723569b2f89587e261fabfb2e594fe5916bb
DJ Pelland <djp@headwire.com>
2017-11-21 13:49:31 -0800

    fix nav link styling bug

--- themes/themeclean/fragments/textlinks/htmltovue.js
+++ themes/themeclean/fragments/textlinks/htmltovue.js
@@ -1,10 +1,14 @@
 module.exports = {
     convert: function($, f) {
-        //f.bindPath($)
     	f.addFor($.find('li').first(), 'model.links')
         f.bindAttribute($.find('a').first(), 'href', f.pathToUrl('item.link'))
         f.mapRichField($.find('a').first(), "item.text")
-        f.bindAttribute($.find('a').first(), 'class', "model.colorscheme === 'light' ? 'text-dark':'text-light'", false)
+
+
+        let colorscheme = 
+        "{'text-dark' : model.colorscheme === 'light'," +
+        "'text-light' : model.colorscheme === 'dark'}"
+        f.bindAttribute($.find('a').first(), 'class', colorscheme, false)
         f.addStyle( $.find('a').first(), 'font-weight', "$root.path === item.link ? 'bold': 'normal'")
     }
 }
