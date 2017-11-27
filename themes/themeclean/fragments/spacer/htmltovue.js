7afb2390578511985ac36e7ab53b63aa56a082d0
Li Qin <lq@headwire.com>
2017-11-27 15:37:18 -0800

    #127: update htmltovue to check for height = 0 bug

--- themes/themeclean/fragments/spacer/htmltovue.js
+++ themes/themeclean/fragments/spacer/htmltovue.js
@@ -2,6 +2,8 @@ module.exports = {
     convert: function($, f) {
     	f.wrap($, 'themeclean-components-block')
         f.bindAttribute($.parent(),'model','model')
-        f.addStyle($, 'height', 'model.height', 'vh')
+        f.addStyle($.find("div").first(), 'height', 'model.height', 'vh')
+        
+        $.append('<div v-if="isEditAndEmpty">no content defined for component, height needs to be greater than 0</div>')
     }
 }
