481d0150b0a39fb8a1cb48970d18a31a50deada8
reusr1 <rr@headwire.com>
2017-11-27 10:42:03 -0800

    fixed container component

--- themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/container/template.vue
+++ themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/container/template.vue
@@ -1,11 +1,10 @@
 <template>
-  <div class="container" v-bind:data-per-path="model.path">
+  <div v-bind:data-per-path="model.path">
     <pagerender-vue-components-placeholder v-bind:model="{ path: model.path, component: model.component, location: 'before' }"></pagerender-vue-components-placeholder>
-    <div v-for="(child,i) in model.children"
-    :key="i">
+    <template v-for="child in model.children">
       <component v-bind:is="child.component" v-bind:model="child"></component>
-    </div>
-    <pagerender-vue-components-placeholder v-bind:model="{ path: model.path, component: model.component, location: 'after'  }"></pagerender-vue-components-placeholder>
+    </template>
+    <pagerender-vue-components-placeholder v-bind:model="{ path: model.path, component: model.component, location: 'after' }"></pagerender-vue-components-placeholder>
   </div>
 </template>
 
