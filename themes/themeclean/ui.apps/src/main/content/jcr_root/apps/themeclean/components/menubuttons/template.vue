4edfbc83dfc78e94b1a8d3fe70376d24bc700c74
DJ Pelland <djp@headwire.com>
2017-11-28 12:48:10 -0800

    fix bottom margin in non tablet view

--- themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/menubuttons/template.vue
+++ themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/menubuttons/template.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-flex justify-content-end flex-column flex-sm-row">
-    <a class="btn btn-primary mb-2 ml-2 mr-0" v-for="(item,i) in model.buttons"
+    <a class="btn btn-primary mb-2 mb-sm-0 ml-2 mr-0" v-for="(item,i) in model.buttons"
     :key="i" v-bind:href="$helper.pathToUrl(item.buttonlink)" v-bind:style="`backgroundColor:${item.buttoncolor};borderColor:${item.buttoncolor};`">
       <span></span>
       <span v-html="item.buttontext"></span>
