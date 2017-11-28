e890d07255987e450050c1051fce9327e74a5dd6
DJ Pelland <djp@headwire.com>
2017-11-28 12:44:26 -0800

    fix button alignment for tablet header

--- themes/themeclean/fragments/menubuttons/template.vue
+++ themes/themeclean/fragments/menubuttons/template.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="d-flex justify-content-center flex-column flex-sm-row">
-    <a class="btn btn-primary mb-2 m-sm-2" v-for="(item,i) in model.buttons"
+  <div class="d-flex justify-content-end flex-column flex-sm-row">
+    <a class="btn btn-primary mb-2 ml-2 mr-0" v-for="(item,i) in model.buttons"
     :key="i" v-bind:href="$helper.pathToUrl(item.buttonlink)" v-bind:style="`backgroundColor:${item.buttoncolor};borderColor:${item.buttoncolor};`">
       <span></span>
       <span v-html="item.buttontext"></span>
