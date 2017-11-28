f8120e63d807a8fbea02a1607a358392bd7e1e0e
DJ Pelland <djp@headwire.com>
2017-11-28 09:10:30 -0800

    adjust styles for pager to fit article width

--- themes/themeclean/fragments/pager/template.vue
+++ themes/themeclean/fragments/pager/template.vue
@@ -1,8 +1,8 @@
 <template>
   <themeclean-components-block v-bind:model="model">
-    <div class="w-100 d-flex justify-content-between">
-      <a class="btn btn-outline-primary" v-bind:href="$helper.pathToUrl(model.previous)">Prev</a>
-      <a class="btn btn-outline-primary" v-bind:href="$helper.pathToUrl(model.next)">Next</a>
+    <div class="d-flex justify-content-between col-12 col-md-8">
+      <a class="btn btn-sm btn-outline-primary percms-btn-pager" v-bind:href="$helper.pathToUrl(model.previous)">Prev</a>
+      <a class="btn btn-sm btn-outline-primary percms-btn-pager" v-bind:href="$helper.pathToUrl(model.next)">Next</a>
     </div>
   </themeclean-components-block>
 </template>
