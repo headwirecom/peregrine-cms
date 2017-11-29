0872f3563c4a7f10bfd4d091b51ae0d1acc324d6
Li Qin <lq@headwire.com>
2017-11-29 11:19:32 -0800

    article text media: update media position lable: left/right to Before/After Content

--- themes/themeclean/fragments/articletextmedia/htmltovue.js
+++ themes/themeclean/fragments/articletextmedia/htmltovue.js
@@ -3,7 +3,7 @@ module.exports = {
     	f.wrap($, 'themeclean-components-block')
         f.bindAttribute($.parent(),'model','model')
         f.bindAttribute($.find('div').first(), 'style', "{flexBasis: `${model.mediawidth}%`}")
-        f.bindAttribute($,  'class', "model.mediaposition === 'right' ? 'flex-row-reverse': 'flex-row'", false)
+        f.bindAttribute($,  'class', "model.mediaposition === 'after' ? 'flex-row-reverse': 'flex-row'", false)
         f.replace( $.find('div.col-md-8').eq(0), '<themeclean-components-media v-bind:model="model"></themeclean-components-media>')
         f.mapRichField( $.find('div').eq(1), "model.text")
         f.addIf($.find('.perIsEditAndEmpty').first(), 'isEditAndEmpty')
