c11692eb8530e78bd9683b5181c229bdc5c0a5c5
Li Qin <lq@headwire.com>
2017-11-29 12:15:35 -0800

    teaser horizontal: add "make primary" check box

--- themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/teaserhorizontal/template.vue
+++ themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/teaserhorizontal/template.vue
@@ -10,9 +10,18 @@
             'text-center': model.aligncontent === 'center',
             'text-right': model.aligncontent === 'right'
         }" v-bind:style="`flex-basis:${model.textwidth}%;`">
-        <h2 class="" v-if="model.showtitle == 'true'" v-html="model.title"></h2>
-        <h4 class="" v-if="model.showsubtitle == 'true'" v-html="model.subtitle"></h4>
-        <p v-if="model.showtext == 'true'" v-html="model.text"></p>
+        <h2 class="" v-if="model.showtitle === 'true' &amp;&amp; model.isprimary === false"
+        v-html="model.title"></h2>
+        <h4 class="" v-if="model.showsubtitle === 'true' &amp;&amp; model.isprimary === false"
+        v-html="model.subtitle"></h4>
+        <p v-if="model.showtext === 'true' &amp;&amp; model.isprimary === false"
+        v-html="model.text"></p>
+        <h1 class="" v-if="model.showtitle == 'true' &amp;&amp; model.isprimary === true"
+        v-html="model.title"></h1>
+        <h3 class="" v-if="model.showsubtitle === 'true' &amp;&amp; model.isprimary === true"
+        v-html="model.subtitle"></h3>
+        <h5 v-if="model.showtext === 'true' &amp;&amp; model.isprimary === true"
+        v-html="model.text"></h5>
       </div>
       <div class="d-flex flex-wrap" v-if="model.showbutton == 'true'"
       v-bind:class="{
