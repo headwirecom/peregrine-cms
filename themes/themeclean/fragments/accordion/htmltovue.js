2787677331c675da083855b722e228ff8c764bca
Nicholaus Perez <hea02160@adobe.com>
2017-11-29 18:50:27 -0800

    Merged Accordion with toggle and added media with alignments

--- themes/themeclean/fragments/accordion/htmltovue.js
+++ themes/themeclean/fragments/accordion/htmltovue.js
@@ -2,16 +2,20 @@ module.exports = {
     convert: function($, f) {
     	f.wrap($, 'themeclean-components-block')
         f.bindAttribute($.parent(),'model','model')
-        f.addFor( $.find('div').first(), 'model.tabs')
-        f.mapField($.find('h2').first(), 'model.title')
-        f.addIf( $.find('h2').first(), 'model.showtitle == \'true\'')
-        f.mapField($.find('a').first(), 'item.title')
-        f.mapRichField($.find('div.card-body').first(), 'item.text')
-        f.bindAttribute($, 'id', '`accordion${_uid}`')
+        f.replace( $.find('div.media'), '<themeclean-components-mediavisible :model="model"></themeclean-components-mediavisible>')
+        f.bindAttribute($.find('div.flex-wrap div').first(), 'style', "{flexBasis:`${model.mediawidth}%`}")
+    	f.addIf($.find('h2').first(), 'model.showtitle == \'true\'')
+        f.mapRichField($.find('h2').first(), "model.title")
+        f.addFor($.find('div.card').first(), 'model.accordiontoggle')
+
         f.bindAttribute($.find('a').first(), 'data-parent', '`#accordion${_uid}`')
         f.bindAttribute($.find('a').first(), 'href', '`#accordion${_uid}${i}`')
         f.bindAttribute($.find('a').first(), 'aria-controls', '`accordion${_uid}${i}`')
+        f.mapRichField($.find('a').first(), "item.title")
+
         f.bindAttribute($.find('div.collapse').first(), 'id', '`accordion${_uid}${i}`')
-        $.append('<div v-if="isEditAndEmpty">no content defined for component</div>')
+        f.mapRichField($.find('div.card-body').first(), "item.text")
+        f.addIf($.find('.perIsEditAndEmpty').first(), 'isEditAndEmpty')
+        f.bindAttribute($.find('div.flex-wrap').first(),  'class', "model.mediaposition === 'right' ? 'flex-row-reverse': 'flex-row'", false)
     }
 }
--- themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/accordion/template.vue
+++ themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/accordion/template.vue
@@ -1,17 +1,26 @@
 <template>
   <themeclean-components-block v-bind:model="model">
-    <div class="col-12 col-md-8" v-bind:id="`accordion${_uid}`">
-      <h2 class="text-center" v-if="model.showtitle == 'true'">{{model.title}}</h2>
-      <div class="item card bg-transparent border-0 rounded-0"
-      v-for="(item,i) in model.tabs" :key="i">
-        <a aria-expanded="false" class="card-header border rounded-0" data-toggle="collapse"
-        v-bind:data-parent="`#accordion${_uid}`" v-bind:href="`#accordion${_uid}${i}`"
-        v-bind:aria-controls="`accordion${_uid}${i}`">{{item.title}}</a>
-        <div class="collapse rounded-0" role="tabpanel" v-bind:id="`accordion${_uid}${i}`">
-          <div class="card-body rounded-0" v-html="item.text"></div>
+    <div class="container">
+      <div class="perIsEditAndEmpty" v-if="isEditAndEmpty">no content defined for component</div>
+      <h2 class="text-center pb-4" v-if="model.showtitle == 'true'"
+      v-html="model.title"></h2>
+      <div class="row col-12 p-0 d-md-flex flex-wrap justify-content-center"
+      v-bind:class="model.mediaposition === 'right' ? 'flex-row-reverse': 'flex-row'">
+        <div class="percms-image-wrap px-3" v-bind:style="{flexBasis:`${model.mediawidth}%`}">
+          <themeclean-components-mediavisible :model="model"></themeclean-components-mediavisible>
+        </div>
+        <div class="col-sm-12 col-md">
+          <div class="item card bg-transparent border-0 rounded-0" v-for="(item,i) in model.accordiontoggle"
+          :key="i">
+            <a aria-expanded="false" class="card-header border rounded-0" data-toggle="collapse"
+            v-bind:data-parent="`#accordion${_uid}`" v-bind:href="`#accordion${_uid}${i}`"
+            v-bind:aria-controls="`accordion${_uid}${i}`" v-html="item.title"></a>
+            <div class="collapse rounded-0" role="tabpanel" v-bind:id="`accordion${_uid}${i}`">
+              <div class="card-body rounded-0" v-html="item.text"></div>
+            </div>
+          </div>
         </div>
       </div>
-      <div v-if="isEditAndEmpty">no content defined for component</div>
     </div>
   </themeclean-components-block>
 </template>
@@ -22,7 +31,7 @@
         computed: {
         	isEditAndEmpty() {
                 if(!$peregrineApp.isAuthorMode()) return false
-                return !(this.model.showtitle === 'true' || this.model.tabs.length > 0)
+                return this.$helper.areAllEmpty( this.model.showtitle === 'true' , this.model.tabs );
             }
         }
     }
