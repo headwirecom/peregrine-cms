f4b98bf8d21fec52cf1655f91553ab2e37fb4cb5
Li Qin <lq@headwire.com>
2017-11-30 15:09:03 -0800

    remove components not used any more

--- themes/themeclean/fragments/textlinksvertical/htmltovue.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    convert: function($, f) {
-        //f.bindPath($)
-        f.mapRichField($.find('h5').first(), "model.title")
-        f.addFor($.find('a').first(), 'model.links')
-        f.bindAttribute($.find('a').first(), 'href', f.pathToUrl('item.link'))
-        f.mapRichField($.find('a').first(), "item.text")
-    }
-}
