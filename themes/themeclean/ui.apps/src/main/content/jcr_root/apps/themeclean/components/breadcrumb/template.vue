6dff4b22da150ff7cebffec818a4f463effddef5
Li Qin <lq@headwire.com>
2017-12-01 12:22:17 -0800

    update breadcrumb sling model.

--- themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/breadcrumb/template.vue
+++ themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/breadcrumb/template.vue
@@ -1,13 +1,10 @@
 <template>
   <div v-bind:data-per-path="model.path">
     <ol class="breadcrumb">
-      <li class="breadcrumb-item">
-        <a href="#">Home</a>
+      <li v-bind:class="i+1 === model.links.length ? 'breadcrumb-item active' : 'breadcrumb-item'" v-for="(item,i) in model.links" :key="i">
+        <a v-if="i+1 < model.links.length" v-bind:href="$helper.pathToUrl(item.link)">{{item.text}}</a>
+        <span v-if="i+1 === model.links.length" v-html="item.text"/>
       </li>
-      <li class="breadcrumb-item">
-        <a href="#">Library</a>
-      </li>
-      <li class="breadcrumb-item active">Data</li>
     </ol>
   </div>
 </template>
