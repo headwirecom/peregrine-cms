1a75f21fbcf3af4020882610caa0a67532b85544
DJ Pelland <djp@headwire.com>
2017-12-01 10:38:26 -0800

    fix dissapearing video on mobile

--- themes/themeclean/fragments/articletextmedia/htmltovue.js
+++ themes/themeclean/fragments/articletextmedia/htmltovue.js
@@ -2,10 +2,13 @@ module.exports = {
     convert: function($, f) {
     	f.wrap($, 'themeclean-components-block')
         f.bindAttribute($.parent(),'model','model')
-        f.bindAttribute($.find('div').first(), 'style', "{flexBasis: `${model.mediawidth}%`}")
-        f.bindAttribute($,  'class', "model.mediaposition === 'after' ? 'flex-row-reverse': 'flex-row'", false)
-        f.replace( $.find('div.col-md-8').eq(0), '<themeclean-components-media v-bind:model="model"></themeclean-components-media>')
-        f.mapRichField( $.find('div').eq(1), "model.text")
         f.addIf($.find('.perIsEditAndEmpty').first(), 'isEditAndEmpty')
+
+        f.bindAttribute($,  'class', "model.mediaposition === 'after' ? 'flex-row-reverse': 'flex-row'", false)
+
+        f.bindAttribute($.find('div').first(), 'style', "{width: `${model.mediawidth}%`}")
+        f.replace( $.find('img'), '<themeclean-components-media v-bind:model="model"></themeclean-components-media>')
+
+        f.mapRichField( $.find('div').eq(1), "model.text")
     }
 }
--- themes/themeclean/fragments/articletextmedia/template.vue
+++ themes/themeclean/fragments/articletextmedia/template.vue
@@ -1,11 +1,11 @@
 <template>
   <themeclean-components-block v-bind:model="model">
-    <div class="row col-12 col-md-8 p-0 d-md-flex align-items-center justify-content-center flex-wrap article"
+    <div class="row col-12 col-md-8 p-0 d-flex align-items-center justify-content-center flex-wrap article"
     v-bind:class="model.mediaposition === 'after' ? 'flex-row-reverse': 'flex-row'">
-      <div class="percms-image-wrap px-3" v-bind:style="{flexBasis: `${model.mediawidth}%`}">
+      <div class="col-12 col-lg-auto p0 px-md-3" v-bind:style="{width: `${model.mediawidth}%`}">
         <themeclean-components-media v-bind:model="model"></themeclean-components-media>
       </div>
-      <div class="col-sm-12 col-md" v-html="model.text"></div>
+      <div class="col-12 col-md" v-html="model.text"></div>
       <div class="perIsEditAndEmpty" v-if="isEditAndEmpty">no content defined for component</div>
     </div>
   </themeclean-components-block>
