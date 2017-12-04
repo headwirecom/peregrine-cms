fdd55dc429c037e1cc1c072dd94331e5dfb9ce9d
Nicholaus Perez <hea02160@adobe.com>
2017-12-04 11:09:16 -0800

    Issue #102, pagelist empty fix

--- themes/themeclean/fragments/pagelist/template.vue
+++ themes/themeclean/fragments/pagelist/template.vue
@@ -1,12 +1,13 @@
 <template>
   <themeclean-components-block v-bind:model="model">
     <div class="col-12">
+      <div class="perIsEditAndEmpty" v-if="isEditAndEmpty">no content defined for component</div>
       <ul class="root" v-if="model.includeroot === 'true'">
         <li class="root">
-          <a v-bind:href="model.rootPageLink">{{model.rootPageTitle}}</a>
+          <a v-bind:href="$helper.pathToUrl(model.rootPageLink)">{{model.rootPageTitle}}</a>
           <ul>
             <li class="children" v-for="(child,i) in model.childrenPages" :key="i">
-              <a v-bind:href="child.path">{{child.title}}</a>
+              <a v-bind:href="$helper.pathToUrl(child.path)">{{child.title}}</a>
               <themeclean-components-pagelistnested v-bind:model="child"
               v-if="child.hasChildren"></themeclean-components-pagelistnested>
             </li>
@@ -15,7 +16,7 @@
       </ul>
       <ul class="noroot" v-if="model.includeroot !== 'true'">
         <li class="childrennoroot" v-for="(child,i) in model.childrenPages" :key="i">
-          <a v-bind:href="child.path">{{child.title}}</a>
+          <a v-bind:href="$helper.pathToUrl(child.path)">{{child.title}}</a>
           <themeclean-components-pagelistnested v-bind:model="child"
           v-if="child.hasChildren"></themeclean-components-pagelistnested>
         </li>
@@ -32,6 +33,12 @@
                 delete data.childrenPages
                 return data
             }
+        },
+        computed: {
+        	isEditAndEmpty() {
+            if(!$peregrineApp.isAuthorMode()) return false
+            return this.$helper.areAllEmpty(this.model.rootpage && this.model.levels);
+          }
         }
     }
 </script>
