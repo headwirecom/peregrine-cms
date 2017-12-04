3be367ffa3e879745e0ed20177eea782d096e90b
DJ Pelland <djp@headwire.com>
2017-12-04 10:31:27 -0800

    convert header buttons colors to a theme picker

--- themes/themeclean/fragments/menubuttons/template.vue
+++ themes/themeclean/fragments/menubuttons/template.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="d-flex justify-content-end flex-column flex-sm-row">
-    <a class="btn btn-primary mb-2 mb-sm-0 ml-2 mr-0" v-for="(item,i) in model.buttons"
-    :key="i" v-bind:href="$helper.pathToUrl(item.buttonlink)" v-bind:style="`backgroundColor:${item.buttoncolor};borderColor:${item.buttoncolor};`">
-      <span></span>
-      <span v-html="item.buttontext"></span>
-    </a>
+    <a class="btn mb-2 mb-sm-0 ml-2 mr-0" v-for="(item,i) in model.buttons"
+    :key="i" v-bind:href="$helper.pathToUrl(item.buttonlink)" v-bind:class="{
+            'btn-lg': model.buttonsize === 'large',
+            'btn-sm': model.buttonsize === 'small',
+            'btn-primary': model.buttoncolor === 'primary',
+            'btn-secondary': model.buttoncolor === 'secondary'
+        }" v-html="item.buttontext"></a>
   </div>
 </template>
 
