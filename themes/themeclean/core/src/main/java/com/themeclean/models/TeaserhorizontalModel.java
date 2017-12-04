4fa33ef166411a9d083d6fbe4fd125baf173dd0f
DJ Pelland <djp@headwire.com>
2017-12-04 13:27:26 -0800

    convert teaserhorizontal buttons to theme picker instead of color

--- themes/themeclean/core/src/main/java/com/themeclean/models/TeaserhorizontalModel.java
+++ themes/themeclean/core/src/main/java/com/themeclean/models/TeaserhorizontalModel.java
@@ -122,6 +122,28 @@ import javax.inject.Named;
             }
           }
         },
+        "buttonsize": {
+          "type": "string",
+          "x-source": "inject",
+          "x-form-label": "Button Size",
+          "x-form-type": "materialselect",
+          "x-form-visible": "model.showbutton == 'true'",
+          "x-default": "default",
+          "properties": {
+            "default": {
+              "x-form-name": "Default",
+              "x-form-value": "default"
+            },
+            "large": {
+              "x-form-name": "Large",
+              "x-form-value": "large"
+            },
+            "small": {
+              "x-form-name": "Small",
+              "x-form-value": "small"
+            }
+          }
+        },
         "buttons": {
           "type": "string",
           "x-source": "inject",
@@ -146,7 +168,18 @@ import javax.inject.Named;
               "type": "string",
               "x-source": "inject",
               "x-form-label": "Button Color",
-              "x-form-type": "color"
+              "x-form-type": "materialselect",
+              "x-default": "primary",
+              "properties": {
+                "primary": {
+                  "x-form-name": "Primary",
+                  "x-form-value": "primary"
+                },
+                "secondary": {
+                  "x-form-name": "Secondary",
+                  "x-form-value": "secondary"
+                }
+              }
             }
           }
         },
@@ -375,7 +408,12 @@ public class TeaserhorizontalModel extends AbstractComponent {
 	@Default(values ="right")
 	private String buttonside;
 
-	/* {"type":"string","x-source":"inject","x-form-label":"Buttons","x-form-type":"collection","x-form-visible":"model.showbutton == 'true'","properties":{"buttontext":{"type":"string","x-source":"inject","x-form-label":"Button Text","x-form-type":"text"},"buttonlink":{"type":"string","x-source":"inject","x-form-label":"Button Link","x-form-type":"pathbrowser","x-form-browserRoot":"/content/sites"},"buttoncolor":{"type":"string","x-source":"inject","x-form-label":"Button Color","x-form-type":"color"}}} */
+	/* {"type":"string","x-source":"inject","x-form-label":"Button Size","x-form-type":"materialselect","x-form-visible":"model.showbutton == 'true'","x-default":"default","properties":{"default":{"x-form-name":"Default","x-form-value":"default"},"large":{"x-form-name":"Large","x-form-value":"large"},"small":{"x-form-name":"Small","x-form-value":"small"}}} */
+	@Inject
+	@Default(values ="default")
+	private String buttonsize;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Buttons","x-form-type":"collection","x-form-visible":"model.showbutton == 'true'","properties":{"buttontext":{"type":"string","x-source":"inject","x-form-label":"Button Text","x-form-type":"text"},"buttonlink":{"type":"string","x-source":"inject","x-form-label":"Button Link","x-form-type":"pathbrowser","x-form-browserRoot":"/content/sites"},"buttoncolor":{"type":"string","x-source":"inject","x-form-label":"Button Color","x-form-type":"materialselect","x-default":"primary","properties":{"primary":{"x-form-name":"Primary","x-form-value":"primary"},"secondary":{"x-form-name":"Secondary","x-form-value":"secondary"}}}}} */
 	@Inject
 	private List<IComponent> buttons;
 
@@ -501,7 +539,12 @@ public class TeaserhorizontalModel extends AbstractComponent {
 		return buttonside;
 	}
 
-	/* {"type":"string","x-source":"inject","x-form-label":"Buttons","x-form-type":"collection","x-form-visible":"model.showbutton == 'true'","properties":{"buttontext":{"type":"string","x-source":"inject","x-form-label":"Button Text","x-form-type":"text"},"buttonlink":{"type":"string","x-source":"inject","x-form-label":"Button Link","x-form-type":"pathbrowser","x-form-browserRoot":"/content/sites"},"buttoncolor":{"type":"string","x-source":"inject","x-form-label":"Button Color","x-form-type":"color"}}} */
+	/* {"type":"string","x-source":"inject","x-form-label":"Button Size","x-form-type":"materialselect","x-form-visible":"model.showbutton == 'true'","x-default":"default","properties":{"default":{"x-form-name":"Default","x-form-value":"default"},"large":{"x-form-name":"Large","x-form-value":"large"},"small":{"x-form-name":"Small","x-form-value":"small"}}} */
+	public String getButtonsize() {
+		return buttonsize;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Buttons","x-form-type":"collection","x-form-visible":"model.showbutton == 'true'","properties":{"buttontext":{"type":"string","x-source":"inject","x-form-label":"Button Text","x-form-type":"text"},"buttonlink":{"type":"string","x-source":"inject","x-form-label":"Button Link","x-form-type":"pathbrowser","x-form-browserRoot":"/content/sites"},"buttoncolor":{"type":"string","x-source":"inject","x-form-label":"Button Color","x-form-type":"materialselect","x-default":"primary","properties":{"primary":{"x-form-name":"Primary","x-form-value":"primary"},"secondary":{"x-form-name":"Secondary","x-form-value":"secondary"}}}}} */
 	public List<IComponent> getButtons() {
 		return buttons;
 	}
