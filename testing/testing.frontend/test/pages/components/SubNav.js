f236351559ea3c4c0aab627ab34a1a99e5bb389c
dna113p <djpelland@gmail.com>
2017-12-06 15:18:09 -0800

    simplify

--- testing/testing.frontend/test/pages/components/SubNav.js
+++ testing/testing.frontend/test/pages/components/SubNav.js
@@ -1,19 +1,7 @@
 class SubNav {
-    get buttons() {
-        const items = $$('.span > a')
-        return items.map(item => {
-            return new Item(item);
-        })
-    }
     get container() { return $('.sub-nav') }
-}
-
-class Item {
-    constructor(container){
-        this.container = container;
-    }
-    get text()     { return this.container.$(`span > a`).getAttribute('title')}
-    get button()    { return this.container.$(`span > a`) }
+    get addFolderButton() {return browser.element('.sub-nav > span:nth-child(2) > a')}
+    get addObjectButton() {return browser.element('.sub-nav > span:nth-child(3) > a')}
 }
 
 module.exports = SubNav
