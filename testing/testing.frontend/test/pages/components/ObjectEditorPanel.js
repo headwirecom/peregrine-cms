ff2c5e159a8deff8e562e020fa6c7f174ebee2c5
Li Qin <lq@headwire.com>
2017-12-11 14:13:22 -0800

    update editorPanel to include pathbrowserContainer

--- testing/testing.frontend/test/pages/components/ObjectEditorPanel.js
+++ testing/testing.frontend/test/pages/components/ObjectEditorPanel.js
@@ -5,6 +5,12 @@ class ObjectEditorPanel {
     get textEditor() {
     	return $('.trumbowyg-editor')
     }
+    get pathBrowserButtons() {
+    	return $$('btn-flat')
+    }
+    get pathBrowserContainer() {
+    	return $('.modal-container')
+    }
     get previewContainer() { return $('.vfg-preview') }
     get container() { return $('.vue-form-generator')}
     get buttonContainer() { return $('.explorer-confirm-dialog')}
