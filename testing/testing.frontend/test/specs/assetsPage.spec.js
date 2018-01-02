0d399c9277ff7c38fd2878637a922c5618eb84ae
Li Qin <lq@headwire.com>
2018-01-02 13:53:19 -0800

    add assets explorer test

--- testing/testing.frontend/test/specs/assetsPage.spec.js
+++ testing/testing.frontend/test/specs/assetsPage.spec.js
@@ -7,6 +7,18 @@ describe('Peregrine assets page', function () {
         expect( browser.getUrl() ).to.contain('content/admin/assets.html')
     })
 
+     describe('Assets Explorer', function() {
+        let exampleFolder
+        
+        it('should have a folder titled "example"', function(){
+        	Explorer.container.waitForVisible()
+            const folders = Explorer.folders
+            const i = folders.findIndex( folder => folder.text.indexOf('example') > -1 ) 
+            exampleFolder = folders[i]
+            expect( exampleFolder.text ).to.contain('example')
+        })
+        
+    })
     
 })
 
