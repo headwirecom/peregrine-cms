5d82a0947eb74542942d04bf0ede99f39b08f994
Li Qin <lq@headwire.com>
2018-01-03 11:01:00 -0800

    add component: AssetEditPanel

---
+++ testing/testing.frontend/test/pages/components/AssetEditPanel.js
@@ -0,0 +1,6 @@
+class AssetPreviewPanel {
+	get menuContainer() { return $('.explorer-preview-nav')} 
+	get contentContainer() { return $('.vue-form-generator')} 
+}
+
+module.exports = AssetPreviewPanel
