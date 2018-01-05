8fb1f1b9f9fb16ada9651ef50ac510779c7f2b25
Li Qin <lq@headwire.com>
2018-01-05 14:00:54 -0800

    add multiselect functions

--- testing/testing.frontend/test/pages/components/AssetEditorPanel.js
+++ testing/testing.frontend/test/pages/components/AssetEditorPanel.js
@@ -1,12 +1,12 @@
 class AssetEditorPanel {
-	get descriptionField() {
-		return this.contentContainer.$('#description')
-	}
 	get titleField() {
 		return this.contentContainer.$('#title')
 	}
+	get descriptionField() {
+		return this.contentContainer.$('#description')
+	}
 	get tagField() {
-		return this.inputs[4]
+		return new MultiSelect(this.contentContainer.$('.multiselect'))
 	}
 	get inputs() {
         return $$('input,textarea')
@@ -15,6 +15,29 @@ class AssetEditorPanel {
 	get contentContainer() { return $('.vue-form-generator')} 
 	get buttonContainer() { return $('.explorer-confirm-dialog')}
     get save() {return this.buttonContainer.$('button[title="save page properties"]')}
+}
+
+class MultiSelect {
+	constructor(container){
+        this.container = container;
+    }
+	get selectButton() { return this.container.$('.multiselect__select')} 
+	get contentWrapper() { return this.container.$('.multiselect__content-wrapper')}
+	get tag() { return this.container.$('.multiselect__single')}
+	get items(){
+		const items = this.container.$$('ul > li')
+        return items.map(item => {
+            return new MultiSelectItem(item);
+        })
+	}
+}
+
+class MultiSelectItem {
+    constructor(container){
+        this.container = container;
+    }
+    get text()     { return this.container.$('.multiselect__option > span').getText()}
+    get span()    { return this.container.$('.multiselect__option > span')}
 }
 
 module.exports = AssetEditorPanel
