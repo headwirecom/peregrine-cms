d7f0d7040f361d810a9af05d4f256825c2ddadae
reusr1 <ruben.reusser@mirumagency.com>
2019-02-27 09:23:04 -0800

    removed some extra log statements

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/createpagewizard/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/createpagewizard/template.vue
@@ -103,14 +103,12 @@
         ,
         computed: {
             pageSchema: function() {
-                console.log('getting schema')
                 if(this.formmodel.templatePath !== '') {
                     const definitions = $perAdminApp.getNodeFromView('/admin/componentDefinitions')
                     if(definitions) {
                         // todo: component should be resolved through the template
                         const comp = 'pagerender-vue-structure-page'
                         const def = $perAdminApp.getNodeFromView('/admin/componentDefinitions')[comp]
-                        console.log(def)
                         return def
                     }
                 }
@@ -120,7 +118,6 @@
                 const siteRootParts = this.formmodel.path.split('/').slice(0,4)
                 siteRootParts[2] = 'templates'
                 const siteRoot = siteRootParts.join('/')
-                console.log(siteRoot)
                 return templates.filter( (item) => item.path.startsWith(siteRoot))
             }
         }
