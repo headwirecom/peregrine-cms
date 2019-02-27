c23304098f4877fd1ed34d00b151764aac6c9c55
djp918 <djp@headwire.com>
2019-02-27 13:44:09 -0800

    add material-range component

---
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/field/material-range/template.vue
@@ -0,0 +1,23 @@
+<template>
+
+	<vue-form-generator :schema="{...schema,...rangeSchema}" :model="{value}"></vue-form-generator>
+
+</template>
+
+<script>	
+	export default {
+		mixins: [ VueFormGenerator.abstractField ],
+		data: function (){
+			return {
+				rangeSchema: {
+            fields: [{
+                type: "input",
+								inputType: "range",
+								debounceFormatTimeout: 0
+            }]
+				},
+				value: parseInt(this.value)
+			}
+		}
+	}
+</script>
