f6507408e25ce9da5eae2069fb34f021cf83033f
DJ Pelland <djpelland@gmail.com>
2019-03-04 15:55:28 -0800

    remove extra vue multiselect

--- admin-base/ui.apps/src/main/content/jcr_root/apps/field/material-multiselect/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/field/material-multiselect/template.vue
@@ -1,14 +1,22 @@
 <template>
 
 	<div v-if="!schema.preview">
-		<multiselect 
-			v-model="value" 
-			:options="schema.values" 
+    <vue-multiselect 
+      v-if="!schema.preview"
+      v-model="value" 
 			:multiple="true"
-			:taggable="schema.selectOptions.taggable" 
-			:custom-label="customLabel"
-			@tag="addItem">
-		</multiselect>
+      :track-by="trackBy"
+      :label="label"
+      :deselectLabel="deselectLabel"
+      :options="schema.values"
+      :searchable="false"
+			:taggable="true" 
+      :clear-on-select="false"
+      :close-on-select="false"
+      :placeholder="placeholder"
+      :allow-empty="allowEmpty"
+      :show-labels="false">
+    </vue-multiselect>
 	</div>
 
 	<ul v-else class="collection">
@@ -22,13 +30,57 @@
 		mixins: [ VueFormGenerator.abstractField ], 
 
 		computed: {
-		},
-
-		methods: {
-			addItem(item) {
-				this.value.push(item);
-				this.schema.values.push(item);
+			placeholder () {
+				if(this.schema.selectOptions && this.schema.selectOptions.placeholder){
+					return this.schema.selectOptions.placeholder
+				}
+				return 'Nothing selected'
 			},
+			allowEmpty () {
+				return this.schema.required ? false : true
+			},
+			trackBy () {
+				if(this.schema.selectOptions && this.schema.selectOptions.value){
+					return this.schema.selectOptions.value
+				}
+				return 'value'
+			},
+			label () {
+				if(this.schema.selectOptions && this.schema.selectOptions.name){
+					return this.schema.selectOptions.name
+				}
+				return 'name'
+			},
+			deselectLabel () {
+				if(this.schema.selectOptions && this.schema.selectOptions.deselectLabel){
+					return this.schema.selectOptions.deselectLabel
+				}
+				return ''
+			},
+			modelFromValue: {
+				get () {
+					// will catch falsy, null or undefined
+					if(this.value && this.value != null){
+						// if model is a string, convert to object with name and value
+						if(typeof this.value === 'string'){ 
+							return this.schema.values.filter(item => item.value === this.value)[0]     
+						} else {
+							return this.value
+						}
+					} else {
+						return ''
+					}
+				},
+				set (newValue) {
+					if(newValue && newValue != null){
+						this.value = newValue[this.trackBy]
+					} else {
+						this.value = ''
+					}
+				}
+			}
+		},
+		methods: {
 			customLabel(label) {
 				return typeof label === 'object' ? label.name: label
 			}
