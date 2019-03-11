2cfc8d6ebbd0c42b32c3b91ed49337b2e8e0055b
djp918 <djp@headwire.com>
2019-03-11 15:06:31 -0700

    simplify multiselect

--- admin-base/ui.apps/src/main/content/jcr_root/apps/field/material-multiselect/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/field/material-multiselect/template.vue
@@ -4,17 +4,10 @@
     <vue-multiselect 
       v-if="!schema.preview"
       v-model="value" 
-			:multiple="true"
-      :track-by="trackBy"
-      :label="label"
-      :deselectLabel="deselectLabel"
+			v-bind="schema.selectOptions"
       :options="schema.values"
-      :searchable="false"
-			:taggable="true" 
-      :clear-on-select="false"
-      :close-on-select="false"
-      :placeholder="placeholder"
-      :show-labels="false"
+			:multiple="true"
+			:clearOnSelect="true"
 			@remove="removeOption">
     </vue-multiselect>
 	</div>
@@ -22,39 +15,11 @@
 	<ul v-else class="collection">
 		<div v-for="item in value" class="collection-item">{{item.name || item}}</div>
 	</ul>
-
 </template>
 
 <script>	
 	export default {
 		mixins: [ VueFormGenerator.abstractField ], 
-		computed: {
-			placeholder () {
-				if(this.schema.selectOptions && this.schema.selectOptions.placeholder){
-					return this.schema.selectOptions.placeholder
-				}
-				return 'Nothing selected'
-			},
-			trackBy () {
-				if(this.schema.selectOptions && this.schema.selectOptions.value){
-					return this.schema.selectOptions.value
-				}
-				return 'value'
-			},
-			label () {
-				if(this.schema.selectOptions && this.schema.selectOptions.name){
-					return this.schema.selectOptions.name
-				}
-				return 'name'
-			},
-			deselectLabel () {
-				if(this.schema.selectOptions && this.schema.selectOptions.deselectLabel){
-					return this.schema.selectOptions.deselectLabel
-				}
-				return ''
-			}
-		},
-
 		methods: {
 			removeOption: function(removedOption, id) {
 				if( "jcr:primaryType"  in removedOption ) {
