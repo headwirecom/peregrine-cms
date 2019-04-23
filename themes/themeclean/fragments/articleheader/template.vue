072bea4d08a649258780dc3856632672e359a073
Ruben Reusser <rr@headwire.com>
2019-04-23 12:04:37 -0700

    fix for articleheader

--- themes/themeclean/fragments/articleheader/template.vue
+++ themes/themeclean/fragments/articleheader/template.vue
@@ -1,6 +1,6 @@
 <template>
   <themeclean-components-block v-bind:model="model">
-    <div class="col-12 col-md-8 article" v-bind:class="model.textalign">
+    <div class="col-12 article" v-bind:class="model.textalign">
       <div class="perIsEditAndEmpty" v-if="isEditAndEmpty">no content defined for component</div>
       <h2 class="col-12 p-0 pb-3" v-if="model.showtitle == 'true'">{{model.title}}</h2>
       <h3 class="col-12 p-0 pb-3" v-if="model.showsubtitle == 'true'">{{model.subtitle}}</h3>
