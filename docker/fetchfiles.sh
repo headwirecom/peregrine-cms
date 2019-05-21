c5151535874b8147e501204f89f68cb1b4e51a90
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2019-05-21 14:20:02 -0700

    ISSUE-75 - Resolved miscellaneous Docker build issues when running from Mac OS X. 1) Fixed file copy fetchfiles.sh to work under Mac OS X; 2) Replaced wget with curl since most Mac OS X systems do not have HTTPS support build into wget; 3) Added support for checking if Node has already been dowloaded.

--- docker/fetchfiles.sh
+++ docker/fetchfiles.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-find ../ | grep "target.*zip$" | grep -v "docker" | xargs --max-args=1 -i cp '{}' files
+# Copy all packages to 'files' directory
+find ../ -type f -name \*.zip | grep "/target/" | while read package; do cp $package files; done;
 cp ../resources/com.peregrine-cms.sling.launchpad-9.1.jar files/sling-9.jar
 
-# TODO: should check if the file already has been downloaded or not to avoid redownloading
-wget https://nodejs.org/dist/v10.15.3/node-v10.15.3-linux-x64.tar.xz -O files/node-v10.15.3-linux-x64.tar.xz
+NODE_VERSION=v10.15.3
+NODE_TARBALL=node-${NODE_VERSION}-linux-x64.tar.xz
+NODE_URL=https://nodejs.org/dist/${NODE_VERSION}/${NODE_TARBALL}
+
+if [ ! -e files/${NODE_TARBALL} ]; then
+  curl -L ${NODE_URL} -o files/${NODE_TARBALL}
+fi 
