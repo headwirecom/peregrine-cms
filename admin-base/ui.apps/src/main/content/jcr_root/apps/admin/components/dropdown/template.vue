afafe2dca25e68cb7dabcd4097208430b4326bbb
Felix Ulbrich <felix.ulbrich@metaproc.de>
2019-06-27 12:54:36 +0200

    Revert "METDEV-1267 change template of dropdown for testing purposes"
    This reverts commit a8f21a1873f28b8391dff5030f2db5faf99e52dd.

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/dropdown/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/dropdown/template.vue
@@ -5,11 +5,7 @@
     </button>
     <div class="drpdwn-content" >
       <template v-for="child in model.children">
-        <template v-if="child.className==drpdwn-content">
-          <template v-for="child in child.children">
-            <div v-bind:is="child.component" v-bind:model="child" v-on:click="icon=child.icon"></div>
-          </template>
-        </template>
+        <div v-bind:is="child.component" v-bind:model="child" v-on:click="icon=phone_android"></div>
       </template>
     </div>
   </div>
