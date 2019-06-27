7fc25eec580dbcd1755a9c0deb46900e0a88140c
Felix Ulbrich <felix.ulbrich@metaproc.de>
2019-06-27 12:38:05 +0200

    METDEV-1267 change icon on click to android phone for testing purposes

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/dropdown/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/dropdown/template.vue
@@ -5,7 +5,7 @@
     </button>
     <div class="drpdwn-content" >
       <template v-for="child in model.children">
-        <div v-bind:is="child.component" v-bind:model="child" v-on:click="icon=child.icon"></div>
+        <div v-bind:is="child.component" v-bind:model="child" v-on:click="icon=phone_android"></div>
       </template>
     </div>
   </div>
