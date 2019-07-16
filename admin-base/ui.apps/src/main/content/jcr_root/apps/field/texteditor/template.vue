abcfb15ddcbc35c20da2e37f43225c42c29537b2
Felix Pütz <felix.puetz@metaproc.de>
2019-07-16 18:12:28 +0200

    METDEV-1452 add "plugins" to config

--- admin-base/ui.apps/src/main/content/jcr_root/apps/field/texteditor/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/field/texteditor/template.vue
@@ -89,6 +89,10 @@
                             cfg.btnsDef = btnsDef
                         }
                     }
+                    let plugins = this.schema.config.plugins;
+                    if (plugins) {
+                        cfg.plugins = plugins;
+                    }
                 }
                 return cfg;
             }
