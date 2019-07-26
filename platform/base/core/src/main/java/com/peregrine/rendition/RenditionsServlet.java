45c582239a0febeeba761d36fe8c7fb0cb06ea36
Andreas Schaefer <schaefera@me.com>
2019-07-26 16:56:42 -0700

    Fix of an NPE and a clarification in the error message when a rendition could not be created

--- platform/base/core/src/main/java/com/peregrine/rendition/RenditionsServlet.java
+++ platform/base/core/src/main/java/com/peregrine/rendition/RenditionsServlet.java
@@ -133,7 +133,7 @@ public class RenditionsServlet extends AbstractBaseServlet {
             ImageContext imageContext = null;
             try {
                 imageContext = renditionHandler.createRendition(resource, renditionName, sourceMimeType);
-                if(imageContext.canBeStored()) {
+                if(imageContext != null && imageContext.canBeStored()) {
                     request.getResourceResolver().commit();
                 } else {
                     request.getResourceResolver().revert();
@@ -145,7 +145,10 @@ public class RenditionsServlet extends AbstractBaseServlet {
                 // Got a output stream -> send it back
                 answer = new StreamResponse(imageContext.getTargetMimeType(), imageContext.getImageStream());
             } else {
-                return new ErrorResponse().setHttpErrorCode(SC_BAD_REQUEST).setErrorMessage("Failed to create a rendition of image: " + asset.getName() + ", rendition: " + renditionName);
+                return new ErrorResponse().setHttpErrorCode(SC_BAD_REQUEST).setErrorMessage(
+                    "Failed to create a rendition of image: " + asset.getName() + ", rendition: " + renditionName
+                    + ". Most likely due to misspelt or non-existing Rendition"
+                );
             }
         } else {
             // This was not a request for a rendition but just the original asset -> load and send it back
