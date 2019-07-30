76bfdb886ffae39cd7ced2a9f3e1b25343cc3eb7
Niklas Fonseca Luis <niklas.fonseca-luis@cybercon.de>
2019-07-30 16:29:04 +0200

    METDEV-1530: add EventBus to possible Vue-initialization

--- pagerenderer/vue/ui.apps/src/main/content/jcr_root/apps/pagerender/vue/scripts/ssr.js
+++ pagerenderer/vue/ui.apps/src/main/content/jcr_root/apps/pagerender/vue/scripts/ssr.js
@@ -3,6 +3,13 @@ process.env.NODE_PATH = process.cwd()
 const Vue = require('vue')
 const renderer = require('vue-server-renderer').createRenderer()
 
+const EventBus = {
+  install(v, options){
+    v.prototype.$eventBus = new Vue();
+  }
+};
+Vue.use( EventBus );
+
 const app = new Vue({
     template: `<div>{{path}}</div>`,
     data: {
