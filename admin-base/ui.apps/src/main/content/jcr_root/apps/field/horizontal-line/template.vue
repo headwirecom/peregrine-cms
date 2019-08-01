82821e55ab2bbfa8749ff0190b57e369ecaaf298
Aaron Seggelke <aaron.seggelke@metaproc.de>
2019-08-01 15:42:16 +0200

    METDEV-1571 added horizontal line option

---
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/field/horizontal-line/template.vue
@@ -0,0 +1,15 @@
+<template>
+    <hr class="hr" :data-per-path="model.path"/>
+</template>
+
+<script>
+    export default {
+        props: ['model']
+    }
+</script>
+
+<style scoped>
+    .hr {
+        width: 100%;
+    }
+</style>
