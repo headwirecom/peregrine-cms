872a57ec110440882e4f62a01a52a153e1b4b830
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2019-08-08 17:21:59 -0700

    Updated Docker build script to exit the build if the Maven fails.
    (cherry picked from commit 9a7c2b0371500caf6b61c4ef223fe680a336517c)

--- docker/builddocker.sh
+++ docker/builddocker.sh
@@ -2,6 +2,12 @@
 if [ "$1" != "skipMaven" ]; then
   cd ..
   mvn clean install --show-version
+  
+  if [ $? -ne 0 ]; then
+    echo "Exiting build with error due to failed Maven build."
+    exit 1
+  fi
+
   cd docker
 fi
 
