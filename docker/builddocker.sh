1e229e732180dc66fac876cdefb2b51dd16439ab
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2019-08-16 16:21:46 -0600

    Added initial support for deploying signed artifacts from Travis CI.

--- docker/builddocker.sh
+++ docker/builddocker.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
-if [ "$1" != "skipMaven" ]; then
-  cd ..
-  mvn clean install --show-version
-  
-  if [ $? -ne 0 ]; then
-    echo "Exiting build with error due to failed Maven build."
-    exit 1
-  fi
 
-  cd docker
-fi
+usage () {
+  echo "Usage: `basename $0` <build type>"
+  echo "  <build type>   build|deploy)"
+}
+
+case $1 in
+  build)
+    if [ "$2" != "skipMaven" ]; then
+      cd ..
+      mvn clean install --show-version
+  
+      if [ $? -ne 0 ]; then
+        echo "Exiting build with error due to failed Maven build."
+        exit 1
+      fi
+
+      cd docker
+    fi
+  ;;
+  deploy)
+    if [ "$2" != "skipMaven" ]; then
+      cd ..
+      gpg --version
+      mvn clean deploy -Peregrine-release --show-version --settings settings.xml
+  
+      if [ $? -ne 0 ]; then
+        echo "Exiting build with error due to failed Maven build."
+        exit 1
+      fi
+
+      cd docker
+    fi
+  ;;
+  *)
+    echo "Unsupported build specification: $1"
+    usage
+    exit 1
+esac
+
 
 echo "Removing old build artifacts..."
 rm files/*.zip files/*.xz files/*.jar
