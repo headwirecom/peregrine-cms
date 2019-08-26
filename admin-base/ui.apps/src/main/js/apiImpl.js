6b41f0ec46ea70a7c5f7d3af40e7b88f855b0e15
djp918 <djp@headwire.com>
2019-08-26 08:46:00 -0700

    Revert "Merge branch 'issues/186' into issues/97"
    This reverts commit f95cc46572bf12863f177eaf41563b94aa928122, reversing
    changes made to a97000129f41bf4db7407968852d7a6b771c75e1.

--- admin-base/ui.apps/src/main/js/apiImpl.js
+++ admin-base/ui.apps/src/main/js/apiImpl.js
@@ -251,66 +251,32 @@ class PerAdminImpl {
 
                     let promises = []
                     if(data && data.model) {
-                        console.log(data.model)
-                        if(data.model.groups) {
-                            for(let j = 0; j < data.model.groups.lenght; j++) {
-                                for(let i = 0; i < data.model.groups[j].fields.length; i++) {
-                                    let from = data.model.groups[j].fields[i].valuesFrom
-                                    if(from) {
-                                        data.model.groups[j].fields[i].values = []
-                                        let promise = axios.get(from).then( (response) => {
-                                            for(var key in response.data) {
-                                                if(response.data[key]['jcr:title']) {
-                                                    const nodeName = key
-                                                    const val = from.replace('.infinity.json', '/'+nodeName)
-                                                    let name = response.data[key].name
-                                                    if(!name) {
-                                                        name = response.data[key]['jcr:title']
-                                                    }
-                                                    data.model.groups[j].fields[i].values.push({ value: val, name: name })
-                                                }
+                        for(let i = 0; i < data.model.fields.length; i++) {
+                            let from = data.model.fields[i].valuesFrom
+                            if(from) {
+                                data.model.fields[i].values = []
+                                let promise = axios.get(from).then( (response) => {
+                                    for(var key in response.data) {
+                                        if(response.data[key]['jcr:title']) {
+                                            const nodeName = key
+                                            const val = from.replace('.infinity.json', '/'+nodeName)
+                                            let name = response.data[key].name
+                                            if(!name) {
+                                                name = response.data[key]['jcr:title']
                                             }
-                                        }).catch( (error) => {
-                                            logger.error('missing node', data.model.groups[j].fields[i].valuesFrom, 'for list population in dialog', error)
-                                        })
-                                        promises.push(promise)
-                                    }
-                                    let visible = data.model.groups[j].fields[i].visible
-                                    if(visible) {
-                                        data.model.groups[j].fields[i].visible = function(model) { 
-                                            return exprEval.Parser.evaluate( visible, this );
-                                        } 
-                                    }
-                                }
-                            }
-                        } else {
-                            for(let i = 0; i < data.model.fields.length; i++) {
-                                let from = data.model.fields[i].valuesFrom
-                                if(from) {
-                                    data.model.fields[i].values = []
-                                    let promise = axios.get(from).then( (response) => {
-                                        for(var key in response.data) {
-                                            if(response.data[key]['jcr:title']) {
-                                                const nodeName = key
-                                                const val = from.replace('.infinity.json', '/'+nodeName)
-                                                let name = response.data[key].name
-                                                if(!name) {
-                                                    name = response.data[key]['jcr:title']
-                                                }
-                                                data.model.fields[i].values.push({ value: val, name: name })
-                                            }
+                                            data.model.fields[i].values.push({ value: val, name: name })
                                         }
-                                    }).catch( (error) => {
-                                        logger.error('missing node', data.model.fields[i].valuesFrom, 'for list population in dialog', error)
-                                    })
-                                    promises.push(promise)
-                                }
-                                let visible = data.model.fields[i].visible
-                                if(visible) {
-                                    data.model.fields[i].visible = function(model) { 
-                                        return exprEval.Parser.evaluate( visible, this );
-                                    } 
-                                }
+                                    }
+                                }).catch( (error) => {
+                                    logger.error('missing node', data.model.fields[i].valuesFrom, 'for list population in dialog', error)
+                                })
+                                promises.push(promise)
+                            }
+                            let visible = data.model.fields[i].visible
+                            if(visible) {
+                                data.model.fields[i].visible = function(model) { 
+                                    return exprEval.Parser.evaluate( visible, this );
+                                } 
                             }
                         }
                     }
