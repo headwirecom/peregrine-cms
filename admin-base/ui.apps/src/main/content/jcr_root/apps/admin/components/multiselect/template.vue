d63615d7df5ab2d886b06984095c49d35829ec86
Ruben Reusser <rr@headwire.com>
2019-09-12 08:13:20 -0700

    add multiselect component based on vue-multiselect

---
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/multiselect/template.vue
@@ -0,0 +1,76 @@
+<!--
+  #%L
+  admin base - UI Apps
+  %%
+  Copyright (C) 2017 headwire inc.
+  %%
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+  
+  http://www.apache.org/licenses/LICENSE-2.0
+  
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  KIND, either express or implied.  See the License for the
+  specific language governing permissions and limitations
+  under the License.
+  #L%
+  -->
+<template>
+  <span>
+    <vue-multiselect
+      :value="mode"
+      deselect-label
+      track-by="title"
+      label="title"
+      placeholder="Select one"
+      :options="devices"
+      :searchable="false"
+      :allow-empty="false"
+      @select="onSelect"
+    >
+      <template slot="option" slot-scope="props">
+        <i class="material-icons" :alt="props.option.title">{{props.option.icon}}</i>
+      </template>
+      <template slot="singleLabel" slot-scope="props">
+        <i class="material-icons" :alt="props.option.title">{{props.option.icon}}</i>
+      </template>
+      <template slot="caret" slot-scope="props"><span></span></template>
+    </vue-multiselect>
+  </span>
+</template>
+
+<script>
+export default {
+  props: ["model"],
+  computed: {
+    mode() {
+      let title = $perAdminApp.getNodeFromViewOrNull(
+        this.model.children[0].stateFrom
+      );
+      if (!title) { 
+        title = "desktop"
+      }
+      const selected = this.devices.find(function(el) {
+        return el.title === title;
+      });
+      return selected;
+    },
+    devices() {
+      return this.model.children;
+    }
+  },
+  methods: {
+    onSelect(item) {
+      $perAdminApp.action(this, item.command, item.target);
+    }
+  }
+};
+</script>
+
