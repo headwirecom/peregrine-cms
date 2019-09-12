c9073d0c42d3198caf1a836fed503e58d8712df8
Radosław Wesołowski <radoslaw.wesolowski@gmail.com>
2019-09-12 14:10:03 +0200

    Add mock test

---
+++ admin-base/core/src/test/java/com/peregrine/admin/resource/AdminResourceHandlerServiceTest.java
@@ -0,0 +1,85 @@
+package com.peregrine.admin.resource;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public final class AdminResourceHandlerServiceTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void addImageMetadataSelector() {
+    }
+
+    @Test
+    public void removeImageMetadataSelector() {
+    }
+
+    @Test
+    public void createFolder() {
+    }
+
+    @Test
+    public void createObject() {
+    }
+
+    @Test
+    public void createPage() {
+    }
+
+    @Test
+    public void createTemplate() {
+    }
+
+    @Test
+    public void deleteResource() {
+    }
+
+    @Test
+    public void deleteResource1() {
+    }
+
+    @Test
+    public void insertNode() {
+    }
+
+    @Test
+    public void moveNode() {
+    }
+
+    @Test
+    public void rename() {
+    }
+
+    @Test
+    public void createAssetFromStream() {
+    }
+
+    @Test
+    public void createNode() {
+    }
+
+    @Test
+    public void copyNode() {
+    }
+
+    @Test
+    public void copySite() {
+    }
+
+    @Test
+    public void deleteSite() {
+    }
+
+    @Test
+    public void updateResource() {
+    }
+
+    @Test
+    public void handleAssetDimensions() {
+    }
+}
