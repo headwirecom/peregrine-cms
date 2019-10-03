d3594afee30448ca9f9eb34b5ec2ab6b8985a6d3
Ben Kahn <bk@headwire.com>
2019-10-03 14:34:59 -0700

    Added i18n for "about" modal.

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/about/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/about/template.vue
@@ -28,30 +28,24 @@
             <p class="left-align">
                 <img style="margin: 10px; width: 100px; max-width: 100px;" src="/etc/felibs/admin/images/peregrine-logo.svg" align="right"/>
                 <img style="width: 0px; height: 0px;" src="https://www.headwire.com/t/peregrine.png"/>
-                Peregrine CMS - a head optional content management system based on Apache Sling
-                and Vue.JS.
+                <span v-html="$i18n('aboutp1')"></span>
             </p>
             <p class="left-align browser-default">
-                For documentation about Peregrine CMS please visit our <a href="http://www.peregrine-cms.com">website</a>.
+                <span v-html="$i18n('aboutp2')"></span>
             </p>
             <p class="left-align browser-default">
-                Peregrine CMS is an open source project sponsored by
-                <a href="https://www.headwire.com" target="_blank">headwire.com, Inc</a>.
-                Commercial support is available upon request. We encourage contributions
-                to the project. Please let us know what you think about this product.
+                <span v-html="$i18n('aboutp3')"></span>
             </p>
             <p class="left-align browser-default">
-                <a href="https://github.com/headwirecom/peregrine-cms" target="_blank">github</a><br>
-                <a href="https://github.com/headwirecom/peregrine-cms/issues" target="_blank">issue tracker</a><br>
-                <a href="mailto:info@headwire.com?subject=invite me to peregrine-cms.slack.com">peregrine-cms.slack.com</a>
+                <span v-html="$i18n('aboutp4')"></span>
             </p>
         </div>
         <div class="modal-footer">
             <button 
                 type="button"
                 class="modal-action modal-close waves-effect waves-light btn-flat"
-                title="close">
-                close
+                v-bind:title="$i18n('close')">
+                {{$i18n('close')}}
             </button>
         </div>
     </div>
