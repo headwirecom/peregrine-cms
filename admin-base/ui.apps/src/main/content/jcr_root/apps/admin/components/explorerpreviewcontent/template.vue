6035dc626c4211273a0f2ebc5e154962b349f215
Felix Pütz <felix.puetz@metaproc.com>
2019-10-16 07:42:10 +0200

    METDEV-1908 merge asset-/objectview into explorerpreviewcontent

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/explorerpreviewcontent/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/explorerpreviewcontent/template.vue
@@ -1,87 +1,117 @@
 <template>
   <div :class="['explorer-preview-content', `preview-${nodeType}`]">
-    <template v-if="currentObject">
-      <ul class="explorer-preview-nav">
-        <li class="icon-list">
-          <div class="icons-left">
-            <a :title="`${nodeType}-info`"
-               class="icon-list-tab waves-effect waves-light"
-               :class="{'active': (tab==='info')}"
-               v-on:click.stop.prevent="onTabClick('info')">
-              <i class="editor-icon material-icons">settings</i>
-            </a>
-          </div>
 
-          <div class="icons-right">
-            <template v-if="allowOperations">
-              <a href="#!"
-                 :title="`rename ${nodeType}`"
-                 class="icon-list-btn waves-effect waves-light"
-                 v-on:click.stop.prevent="renameNode">
-                <i class="svg-icons svg-icon-rename"></i>
-              </a>
-              <a href="#!"
-                 :title="`move ${nodeType}`"
-                 class="icon-list-btn waves-effect waves-light"
-                 v-on:click.stop.prevent="moveNode">
-                <i class="material-icons">compare_arrows</i>
-              </a>
-              <a href="#!"
-                 :title="`delete ${nodeType}`"
-                 class="icon-list-btn waves-effect waves-light"
-                 v-on:click.stop.prevent="deleteNode">
-                <i class="material-icons">delete</i>
-              </a>
-            </template>
-            <template>
-              <a v-if="edit"
-                 title="cancel edit"
-                 class="icon-list-btn waves-effect waves-light"
-                 v-on:click.stop.prevent="onCancel">
-                <i class="editor-icon material-icons">info</i>
-              </a>
-              <a v-else
-                 :title="`edit ${nodeType} properties`"
-                 class="icon-list-btn waves-effect waves-light"
-                 v-on:click.stop.prevent="onEdit">
-                <i class="editor-icon material-icons">edit</i>
-              </a>
-            </template>
-          </div>
+    <div v-if="currentObject" class="explorer-preview-nav">
+      <ul class="nav-left">
+        <li>
+          <a title="og-tags"
+             class="waves-effect waves-light"
+             :class="{'active': isTab(Tab.OG_TAGS)}"
+             v-on:click.stop.prevent="onTabClick(Tab.OG_TAGS)">
+            <i class="editor-icon material-icons">label</i>
+          </a>
+        </li>
+        <li>
+          <a :title="`${nodeType}-info`"
+             class="waves-effect waves-light"
+             :class="{'active': isTab(Tab.INFO)}"
+             v-on:click.stop.prevent="onTabClick(Tab.INFO)">
+            <i class="editor-icon material-icons">settings</i>
+          </a>
         </li>
       </ul>
-      <div v-if="!edit" class="show-overflow">
-        <div>
-          <vue-form-generator
-              class="vfg-preview"
-              v-on:validated="onValidated"
-              v-bind:schema="readOnlySchema"
-              v-bind:model="node"
-              v-bind:options="options">
-          </vue-form-generator>
-        </div>
+
+      <ul class="nav-right">
+        <li v-if="allowOperations">
+          <a href="#!"
+             :title="`rename ${nodeType}`"
+             class="waves-effect waves-light"
+             v-on:click.stop.prevent="renameNode">
+            <i class="svg-icons svg-icon-rename"></i>
+          </a>
+        </li>
+        <li v-if="allowOperations">
+          <a href="#!"
+             :title="`move ${nodeType}`"
+             class="icon-list-btn waves-effect waves-light"
+             v-on:click.stop.prevent="moveNode">
+            <i class="material-icons">compare_arrows</i>
+          </a>
+        </li>
+        <li v-if="allowOperations">
+          <a href="#!"
+             :title="`delete ${nodeType}`"
+             class="icon-list-btn waves-effect waves-light"
+             v-on:click.stop.prevent="deleteNode">
+            <i class="material-icons">delete</i>
+          </a>
+        </li>
+        <li v-if="edit">
+          <a title="cancel edit"
+             class="icon-list-btn waves-effect waves-light"
+             v-on:click.stop.prevent="onCancel">
+            <i class="editor-icon material-icons">info</i>
+          </a>
+        </li>
+        <li v-if="!edit">
+          <a :title="`edit ${nodeType} properties`"
+             class="icon-list-btn waves-effect waves-light"
+             v-on:click.stop.prevent="onEdit">
+            <i class="editor-icon material-icons">edit</i>
+          </a>
+        </li>
+      </ul>
+    </div>
+
+    <div v-if="!edit" class="show-overflow">
+      <div v-if="isTab(Tab.OG_TAGS)">
+        <vue-form-generator
+            class="vfg-preview"
+            v-on:validated="onValidated"
+            v-bind:schema="readOnlyOgTagSchema"
+            v-bind:model="node"
+            v-bind:options="options">
+        </vue-form-generator>
       </div>
-      <template v-else>
-        <div class="show-overflow">
-          <vue-form-generator
-              v-bind:schema="schema"
-              v-bind:model="node"
-              v-bind:options="options">
-          </vue-form-generator>
-        </div>
-        <div class="explorer-confirm-dialog">
-          <button
-              type="button"
-              :title="`save ${nodeType} properties`"
-              v-bind:disabled="!valid"
-              class="btn btn-raised waves-effect waves-light right"
-              v-on:click.stop.prevent="onOk">
-            <i class="material-icons">check</i>
-          </button>
-        </div>
-      </template>
-    </template>
-    <div v-else class="explorer-preview-empty">
+      <div v-else-if="isTab(Tab.INFO)">
+        <vue-form-generator
+            class="vfg-preview"
+            v-on:validated="onValidated"
+            v-bind:schema="readOnlySchema"
+            v-bind:model="node"
+            v-bind:options="options">
+        </vue-form-generator>
+      </div>
+    </div>
+
+    <div v-if="edit" class="show-overflow">
+      <div v-if="isTab(Tab.OG_TAGS)" class="show-overflow">
+        <vue-form-generator
+            v-bind:schema="ogTagSchema"
+            v-bind:model="node"
+            v-bind:options="options">
+        </vue-form-generator>
+      </div>
+      <div v-else-if="isTab(Tab.INFO)" class="show-overflow">
+        <vue-form-generator
+            v-bind:schema="schema"
+            v-bind:model="node"
+            v-bind:options="options">
+        </vue-form-generator>
+      </div>
+      <div class="explorer-confirm-dialog">
+        <button
+            type="button"
+            :title="`save ${nodeType} properties`"
+            v-bind:disabled="!valid"
+            class="btn btn-raised waves-effect waves-light right"
+            v-on:click.stop.prevent="onOk">
+          <i class="material-icons">check</i>
+        </button>
+      </div>
+    </div>
+
+    <div v-if="!currentObject" class="explorer-preview-empty">
       <span>{{ $i18n(`no ${nodeType} selected`) }}</span>
       <i class="material-icons">info</i>
     </div>
@@ -98,11 +128,16 @@
         :onCancel="onMoveCancel"
         :onSelect="onMoveSelect">
     </admin-components-pathbrowser>
-  </div>
 
+  </div>
 </template>
 
 <script>
+  const Tab = {
+    INFO: 'info',
+    OG_TAGS: 'og-tags'
+  };
+
   export default {
     props: {
       model: {
@@ -124,7 +159,8 @@
     },
     data: function () {
       return {
-        tab: 'info',
+        Tab: Tab,
+        activeTab: Tab.INFO,
         valid: true,
         isOpen: false,
         selectedPath: null,
@@ -159,6 +195,23 @@
         return roSchema;
 
       },
+      readOnlyOgTagSchema() {
+        if (!this.ogTagSchema) {
+          return {};
+        }
+        const roSchema = JSON.parse(JSON.stringify(this.ogTagSchema));
+        roSchema.fields.forEach((field) => {
+          field.preview = true;
+          field.readonly = true;
+          if (field.fields) {
+            field.fields.forEach((field) => {
+              field.readonly = true;
+            })
+          }
+        });
+        return roSchema;
+
+      },
       currentObject() {
         return $perAdminApp.getNodeFromViewOrNull(`/state/tools/${this.nodeType}`);
       },
@@ -171,7 +224,14 @@
       schema() {
         const view = $perAdminApp.getView();
         const component = this.node.component;
-        return view.admin.componentDefinitions[component];
+        return view.admin.componentDefinitions[component].model;
+      },
+      ogTagSchema() {
+        const view = $perAdminApp.getView();
+        if (this.node) {
+          const component = this.node.component;
+          return view.admin.componentDefinitions[component].ogTags;
+        }
       }
     },
     methods: {
@@ -239,34 +299,15 @@
         $perAdminApp.getNodeFromView('/state/tools').edit = false;
       },
       onTabClick(clickedTab) {
-        this.tab = clickedTab;
+        this.activeTab = clickedTab;
+      },
+      isTab(tab) {
+        return this.activeTab === tab;
       }
     }
   }
 </script>
 <style scoped>
-  .icon-list {
-    width: 100%;
-  }
-
-  .icon-list-tab {
-    height: 44px;
-    margin-top: -2px;
-    padding: 6px 5px 5px 5px;
-  }
-
-  .icon-list-btn {
-    height: 44px;
-    width: 44px;
-    margin-top: -2px;
-    padding: 6px 5px 5px 5px;
-  }
-
-  .icons-left {
-    float: left;
-    height: 44px;
-    margin-left: 10px;
-  }
 
   .active {
     background-color: #37474f;
@@ -277,16 +318,56 @@
     overflow: auto;
   }
 
-  .icons-right {
-    float: right;
-    height: 44px;
-    margin-right: 10px;
-  }
-
   .editor-icon {
-    /*width: 44px;*/
     height: 44px;
     margin-right: 5px;
     margin-left: 5px;
+  }
+
+  .explorer .explorer-layout .row .explorer-preview .explorer-preview-nav .nav-right,
+  .explorer .explorer-layout .row .explorer-preview .explorer-preview-nav .nav-left {
+    margin: 0;
+    padding: 0;
+    display: flex;
+  }
+
+  .explorer .explorer-layout .row .explorer-preview .explorer-preview-nav .nav-left {
+    margin-right: auto;
+  }
+
+  .explorer .explorer-layout .row .explorer-preview .explorer-preview-nav .nav-right > li,
+  .explorer .explorer-layout .row .explorer-preview .explorer-preview-nav .nav-left > li {
+    border-left: 1px solid #eceff1;
+    border-right: 1px solid #b0bec5;
+    height: 100%;
+    line-height: 45px;
+  }
+
+  .explorer .explorer-layout .row .explorer-preview .explorer-preview-nav .nav-right > li:first-child,
+  .explorer .explorer-layout .row .explorer-preview .explorer-preview-nav .nav-left > li:first-child {
+    border-left: none;
+  }
+
+  .explorer .explorer-layout .row .explorer-preview .explorer-preview-nav .nav-right > li > a:hover,
+  .explorer .explorer-layout .row .explorer-preview .explorer-preview-nav .nav-right > li > a:focus,
+  .explorer .explorer-layout .row .explorer-preview .explorer-preview-nav .nav-right > li > a:active,
+  .explorer .explorer-layout .row .explorer-preview .explorer-preview-nav .nav-left > li > a:hover,
+  .explorer .explorer-layout .row .explorer-preview .explorer-preview-nav .nav-left > li > a:focus,
+  .explorer .explorer-layout .row .explorer-preview .explorer-preview-nav .nav-left > li > a:active {
+    background-color: #b0bec5;
+  }
+
+  .explorer .explorer-layout .row .explorer-preview .explorer-preview-nav .nav-right > li > a,
+  .explorer .explorer-layout .row .explorer-preview .explorer-preview-nav .nav-left > li > a {
+    padding: 0 0.75rem;
+    height: 100%;
+    display: inline-block;
+  }
+
+  .explorer .explorer-layout .row .explorer-preview .explorer-preview-nav .nav-right > li > a .material-icons,
+  .explorer .explorer-layout .row .explorer-preview .explorer-preview-nav .nav-left > li > a .material-icons,
+  .explorer .explorer-layout .row .explorer-preview .explorer-preview-nav .nav-right > li > a .svg-icon,
+  .explorer .explorer-layout .row .explorer-preview .explorer-preview-nav .nav-left > li > a .svg-icon {
+    line-height: 45px;
   }
 </style>
