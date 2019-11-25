bf9fd1614c08425e92dcc247476e5e8833391517
Radosław Wesołowski <radoslaw.wesolowski@gmail.com>
2019-11-25 15:46:52 +0100

    Adjust PrefixAndCutUrlExternalizer to selector based sitemap

--- platform/base/core/src/main/java/com/peregrine/sitemap/impl/PrefixAndCutUrlExternalizer.java
+++ platform/base/core/src/main/java/com/peregrine/sitemap/impl/PrefixAndCutUrlExternalizer.java
@@ -33,12 +33,16 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.metatype.annotations.Designate;
 
+import static com.peregrine.commons.util.PerConstants.DOT;
+import static com.peregrine.commons.util.PerConstants.SLASH;
+import static com.peregrine.sitemap.SiteMapConstants.SITE_MAP;
+import static com.peregrine.sitemap.SiteMapConstants.XML;
+
 @Component(service = UrlExternalizer.class, immediate = true)
 @Designate(ocd = PrefixAndCutUrlExternalizerConfig.class, factory = true)
 public final class PrefixAndCutUrlExternalizer extends UrlExternalizerBase {
 
-    private static final String SLASH = "/";
-
+    private static final String DOT_REGEX = "\\.";
     private PrefixAndCutUrlExternalizerConfig config;
     private int cutCount;
     private int siteMapPosition;
@@ -60,20 +64,41 @@ public final class PrefixAndCutUrlExternalizer extends UrlExternalizerBase {
     @Override
     public String map(final ResourceResolver resourceResolver, final String url) {
         final String[] split = StringUtils.split(url, SLASH);
-        final int length = split.length;
         int start = cutCount;
-        if (siteMapPosition < length && length <= siteMapPosition + 2 && split[siteMapPosition].endsWith(".sitemap.xml")) {
+        if (isSiteMap(split)) {
             start = siteMapPosition;
-            split[start] = "sitemap.xml";
+            final String[] nameSplit = split[siteMapPosition].split(DOT_REGEX);
+            final StringBuilder name = new StringBuilder(SITE_MAP);
+            for (int i = 2; i < nameSplit.length; i++) {
+                name.append(DOT);
+                name.append(nameSplit[i]);
+            }
+
+            split[start] = name.toString();
         }
 
-        StringBuilder result = new StringBuilder(prefix);
+        final StringBuilder result = new StringBuilder(prefix);
         for (int i = start; i < split.length; i++) {
             result.append(SLASH);
             result.append(split[i]);
         }
 
         return result.toString();
+    }
+
+    private boolean isSiteMap(final String[] url) {
+        int length = url.length;
+        if (siteMapPosition >= length || length > siteMapPosition + 2) {
+            return false;
+        }
+
+        final String[] siteMapPart = url[siteMapPosition].split(DOT_REGEX);
+        length = siteMapPart.length;
+        if (length >= 3 && siteMapPart[1].equals(SITE_MAP) && siteMapPart[length - 1].equals(XML)) {
+            return true;
+        }
+
+        return false;
     }
 
 }
