670d0261c0566b79b82f3e7e31c9606878fd4ede
Radosław Wesołowski <radoslaw.wesolowski@gmail.com>
2019-12-12 13:44:13 +0100

    Add Chars util class

---
+++ platform/commons/src/main/java/com/peregrine/commons/util/Chars.java
@@ -0,0 +1,12 @@
+package com.peregrine.commons.util;
+
+public final class Chars {
+
+    public static final char COLON = ':';
+    public static final char EQ = '=';
+
+    private Chars() {
+        throw new UnsupportedOperationException();
+    }
+
+}
