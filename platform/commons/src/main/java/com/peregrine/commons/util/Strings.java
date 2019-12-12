801fe6eeb37e6065189ed9c504f17236bd55a385
Radosław Wesołowski <radoslaw.wesolowski@gmail.com>
2019-12-12 13:44:42 +0100

    Test util class properties of Strings class

--- platform/commons/src/main/java/com/peregrine/commons/util/Strings.java
+++ platform/commons/src/main/java/com/peregrine/commons/util/Strings.java
@@ -4,6 +4,13 @@ import org.apache.commons.lang3.StringUtils;
 
 public final class Strings {
 
+    public static final String COLON = String.valueOf(Chars.COLON);
+    public static final String EQ = String.valueOf(Chars.EQ);
+
+    private Strings() {
+        throw new UnsupportedOperationException();
+    }
+
     public static String firstNotBlank(final String... strings) {
         for (final String s : strings) {
             if (StringUtils.isNotBlank(s)) {
