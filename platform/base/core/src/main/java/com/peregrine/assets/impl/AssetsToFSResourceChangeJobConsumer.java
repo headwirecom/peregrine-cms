a72e7718e3dd3ab92b9bd5c8c635d4fac73f005a
Radosław Wesołowski <radoslaw.wesolowski@gmail.com>
2020-01-07 13:24:38 +0100

    Delete folder when resource is removed

--- platform/base/core/src/main/java/com/peregrine/assets/impl/AssetsToFSResourceChangeJobConsumer.java
+++ platform/base/core/src/main/java/com/peregrine/assets/impl/AssetsToFSResourceChangeJobConsumer.java
@@ -26,6 +26,8 @@ package com.peregrine.assets.impl;
  */
 
 import com.peregrine.assets.ResourceResolverFactoryProxy;
+import com.peregrine.commons.ResourceUtils;
+import org.apache.commons.io.FileUtils;
 import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
@@ -35,10 +37,13 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.metatype.annotations.Designate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.util.Set;
+
+import static com.peregrine.commons.util.PerConstants.SLASH;
+import static java.util.Objects.isNull;
+import static org.apache.commons.lang3.StringUtils.*;
 
 @Component(service = JobConsumer.class, immediate = true, property = {
         JobConsumer.PROPERTY_TOPICS + "=" + AssetsToFSResourceChangeJobConsumer.TOPIC })
@@ -47,8 +52,6 @@ public final class AssetsToFSResourceChangeJobConsumer implements JobConsumer {
 
     public static final String TOPIC = "com/peregrine/assets/REFRESH_FS_FILES";
     public static final String PN_PATHS = "paths";
-
-    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Reference
     private ResourceResolverFactoryProxy resourceResolverFactory;
@@ -62,19 +65,57 @@ public final class AssetsToFSResourceChangeJobConsumer implements JobConsumer {
 
     @Override
     public JobResult process(final Job job) {
-        logger.error(rootPath);
         final Set<String> initialPaths = job.getProperty(PN_PATHS, Set.class);
         try (final ResourceResolver resourceResolver = resourceResolverFactory.getServiceResourceResolver()) {
             for (final String path : initialPaths) {
-                logger.error(path);
-                final Resource resource = resourceResolver.getResource(path);
-                logger.error(String.valueOf(resource));
+                updateFiles(resourceResolver, path);
             }
         } catch (final LoginException e) {
             return JobResult.CANCEL;
         }
 
         return JobResult.OK;
+    }
+
+    private void updateFiles(final ResourceResolver resourceResolver, final String path) {
+        final Resource resource = resourceResolver.getResource(path);
+        if (isNull(resource)) {
+            deleteMissingAncestorFolder(resourceResolver, path);
+        }
+    }
+
+    private void deleteMissingAncestorFolder(final ResourceResolver resourceResolver, final String path) {
+        final Resource ancestor = ResourceUtils.getFirstExistingAncestorOnPath(resourceResolver, path);
+        if (isNull(ancestor)) {
+            return;
+        }
+
+        final String ancestorPath = ancestor.getPath();
+        final String subPath = substringAfter(path, ancestorPath);
+        if (isBlank(subPath)) {
+            return;
+        }
+
+        final String name = substringBefore(subPath.substring(1), SLASH);
+        final String folderPath = localPath(ancestorPath, name);
+        final File folder = new File(folderPath);
+        if (folder.exists()) {
+            if (folder.isFile()) {
+                folder.delete();
+            } else {
+                FileUtils.deleteQuietly(folder);
+            }
+        }
+    }
+
+    private String localPath(final String... parts) {
+        final StringBuilder result = new StringBuilder(rootPath);
+        for (final String part : parts) {
+            result.append(SLASH);
+            result.append(part);
+        }
+
+        return result.toString();
     }
 
 }
