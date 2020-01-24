d8d240bfa045346c00bb011db4628920bf75ac29
Felix Pütz <felix.puetz@metaproc.com>
2020-01-24 22:33:52 +0100

    add NodeType import to objectview

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/objectview/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/objectview/template.vue
@@ -1,21 +1,23 @@
 <template>
-    <admin-components-explorerpreviewcontent
-        :model="model"
-        :nodeType="NodeType.OBJECT"
-        browserRoot="/content/objects"
-        currentPath="/content/objects">
-    </admin-components-explorerpreviewcontent>
+  <admin-components-explorerpreviewcontent
+      :model="model"
+      :nodeType="NodeType.OBJECT"
+      browserRoot="/content/objects"
+      currentPath="/content/objects">
+  </admin-components-explorerpreviewcontent>
 </template>
 
 <script>
+  import {NodeType} from '../../../../../../js/constants'
+
   export default {
-        props: ['model'],
-        data() {
-            return  {
-                NodeType: NodeType
-            };
-        }
-    };
+    props: ['model'],
+    data() {
+      return {
+        NodeType: NodeType
+      }
+    }
+  }
 </script>
 <style scoped>
 </style>
