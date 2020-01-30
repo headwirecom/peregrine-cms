65bb5d0eb0c2a839c45d599a3f2d76877ea62747
Felix Puetz <felix.puetz@metaproc.de>
2020-01-30 11:53:34 +0100

    fix "cannot read property 'parentElement' of null" in themeclean (#291)

--- themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/header/template.vue
+++ themes/themeclean/ui.apps/src/main/content/jcr_root/apps/themeclean/components/header/template.vue
@@ -1,18 +1,22 @@
 <template>
-  <themeclean-components-block v-bind:model="model">
-    <nav class="navbar align-items-center navbar-light w-100" v-bind:class="{'navbar-expand-lg': model.collapsed === 'false','navbar-light': model.colorscheme === 'light','navbar-dark': model.colorscheme === 'dark'}">
+  <themeclean-components-block v-bind:model="model" @mounted="onBlockMounted">
+    <nav class="navbar align-items-center navbar-light w-100"
+         ref="nav"
+         v-bind:class="{'navbar-expand-lg': model.collapsed === 'false','navbar-light': model.colorscheme === 'light','navbar-dark': model.colorscheme === 'dark'}">
       <h1 v-if="editAndEmpty">Configure Header</h1>
       <!-- Logo -->
       <span class="navbar-logo">
         <a v-if="model.logo" v-bind:href="$helper.pathToUrl(model.logourl)">
-          <img class="menu-logo" v-bind:src="$helper.pathToUrl(model.logo)" v-bind:alt="model.logoalttext"
-          v-bind:style="`height:${parseInt(model.logosize)}px;`">
+          <img class="menu-logo" v-bind:src="$helper.pathToUrl(model.logo)"
+               v-bind:alt="model.logoalttext"
+               v-bind:style="`height:${parseInt(model.logosize)}px;`">
         </a>
       </span>
       <!-- Hamburger toggle button -->
       <button class="navbar-toggler navbar-toggler-right" data-toggle="collapse"
-      data-target="#navbarSupportedContent" aria-expanded="false" aria-controls="navbarSupportedContent"
-      aria-label="navigation">
+              data-target="#navbarSupportedContent" aria-expanded="false"
+              aria-controls="navbarSupportedContent"
+              aria-label="navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <!-- Collapsible Menu -->
@@ -26,19 +30,43 @@
 </template>
 
 <script>
-    export default {
-        props: ['model'],
-        mounted() {
-          $('body').scrollspy({target:'.navbar'})
-          $('.nav-link').click( function(e) {
-            $('.navbar-collapse').collapse('hide')
-          })
-        },
-        computed: {
-        	isEditAndEmpty() {
-                if(!$peregrineApp.isAuthorMode()) return false
-                return this.$helper.areAllEmpty(this.model.logo, this.model.links,  this.model.buttons)
-            }
+  export default {
+    props: ['model'],
+    mounted() {
+      $('body').scrollspy({target: '.navbar'})
+      $('.nav-link').click(function (e) {
+        $('.navbar-collapse').collapse('hide')
+      })
+    },
+    computed: {
+      isEditAndEmpty() {
+        if (!$peregrineApp.isAuthorMode()) {
+          return false
         }
+        return this.$helper.areAllEmpty(this.model.logo, this.model.links, this.model.buttons)
+      }
+    },
+    created() {
+      this.$root.$on('block.mounted', this.onBlockMountede)
+    },
+    methods: {
+      onBlockMounted() {
+        // Add top margin to perApp to account for fixed header when sticky is true
+        if( this.model.sticky === 'true' && !$peregrineApp.isAuthorMode()) {
+          if( this.$refs.section.style.position === 'fixed' ){
+            const height = this.$refs.section.clientHeight
+            this.$refs.section.parentElement.style.marginTop = height + 'px';
+          }
+        }
+        const navSection = this.$refs.nav.parentElement.parentElement.parentElement
+        const navPosition = navSection.style.position
+        const navSticky = navPosition === "sticky" || navPosition === "fixed"
+        const navOffset = navSticky ? navSection.clientHeight : 0
+
+        navSection.style.top = `0px`
+        navSection.style.marginTop = `-${navOffset}px`
+        navSection.style.paddingTop = `${navOffset}px`
+      }
     }
+  }
 </script>
