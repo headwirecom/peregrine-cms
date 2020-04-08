5324f29f5bb045a94ad24e47ae30449708c4debb
Ruben Reusser <rr@headwire.com>
2020-04-08 09:33:56 -0700

    position pages dropdown correctly in subnav

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/subnav/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/subnav/template.vue
@@ -39,7 +39,7 @@
             </ul>
         </div>
         <template v-for="child in model.children">
-            <div v-bind:is="child.component" v-bind:model="child"></div>
+            <div v-bind:is="child.component" v-bind:model="child" v-bind:key="child.path"></div>
         </template>
         <span v-if="isEditPage" class="center-keeper"></span>
     </div>
@@ -60,17 +60,18 @@ export default {
         },
     },
     mounted() {
-        $('.dropdown-button').dropdown({
-                inDuration: 300,
-                outDuration: 225,
-                constrainWidth: false, // Does not change width of dropdown to that of the activator
-                hover: false, // Activate on hover
-                gutter: 0, // Spacing from edge
-                belowOrigin: false, // Displays dropdown below the button
-                alignment: 'left', // Displays dropdown with edge aligned to the left of button
-                stopPropagation: false // Stops event propagation
-            }
-        );
+        $('.dropdown-button').dropdown( { belowOrigin: true } )
+        // {
+        //         inDuration: 300,
+        //         outDuration: 225,
+        //         constrainWidth: false, // Does not change width of dropdown to that of the activator
+        //         hover: false, // Activate on hover
+        //         gutter: 0, // Spacing from edge
+        //         belowOrigin: true, // Displays dropdown below the button
+        //         alignment: 'left', // Displays dropdown with edge aligned to the left of button
+        //         stopPropagation: false // Stops event propagation
+        //     }
+        // );
     },
     methods: {
         isEditor: function() {
