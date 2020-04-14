3c7672e179e063bedda11983099aa935bb2ffa07
reusr1 <rr@headwire.com>
2020-04-14 10:09:20 -0700

    reverted back to the old version as the rewrite seemed to have issues with returning the assetsview dialog

--- admin-base/core/src/main/java/com/peregrine/admin/servlets/ComponentDefinitionServlet.java
+++ admin-base/core/src/main/java/com/peregrine/admin/servlets/ComponentDefinitionServlet.java
@@ -25,150 +25,133 @@ package com.peregrine.admin.servlets;
  * #L%
  */
 
-import com.peregrine.commons.servlets.AbstractBaseServlet;
-import com.peregrine.commons.servlets.ServletHelper;
-import com.peregrine.commons.util.PerConstants;
-import org.apache.sling.api.resource.Resource;
-import org.osgi.service.component.annotations.Component;
-
-import javax.servlet.Servlet;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Optional;
-import java.util.function.Function;
-
 import static com.peregrine.admin.servlets.AdminPaths.RESOURCE_TYPE_COMPONENT_DEFINITION;
-import static com.peregrine.commons.Strings.COLON;
-import static com.peregrine.commons.Strings.SLASH;
-import static com.peregrine.commons.util.PerConstants.*;
-import static com.peregrine.commons.util.PerUtil.*;
-import static java.util.Objects.nonNull;
-import static org.apache.commons.lang3.StringUtils.*;
+import static com.peregrine.commons.util.PerConstants.APPS_ROOT;
+import static com.peregrine.commons.util.PerConstants.MODEL;
+import static com.peregrine.commons.util.PerConstants.NAME;
+import static com.peregrine.commons.util.PerConstants.OG_TAGS;
+import static com.peregrine.commons.util.PerConstants.PATH;
+import static com.peregrine.commons.util.PerConstants.SLASH;
+import static com.peregrine.commons.util.PerConstants.SLING_RESOURCE_SUPER_TYPE;
+import static com.peregrine.commons.util.PerConstants.SLING_RESOURCE_TYPE;
+import static com.peregrine.commons.util.PerUtil.EQUALS;
+import static com.peregrine.commons.util.PerUtil.GET;
+import static com.peregrine.commons.util.PerUtil.PER_PREFIX;
+import static com.peregrine.commons.util.PerUtil.PER_VENDOR;
 import static org.apache.sling.api.servlets.ServletResolverConstants.SLING_SERVLET_METHODS;
 import static org.apache.sling.api.servlets.ServletResolverConstants.SLING_SERVLET_RESOURCE_TYPES;
 import static org.osgi.framework.Constants.SERVICE_DESCRIPTION;
 import static org.osgi.framework.Constants.SERVICE_VENDOR;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+import com.peregrine.commons.servlets.AbstractBaseServlet;
+import com.peregrine.commons.servlets.ServletHelper;
+import com.peregrine.commons.util.PerConstants;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+
+import javax.servlet.Servlet;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.osgi.service.component.annotations.Component;
+
 /**
  * Provides the Component Definition of a Resource
  *
  * The API Definition can be found in the Swagger Editor configuration:
- *    ui.apps/src/main/content/jcr_root/perapi/definitions/admin.yaml
+ * ui.apps/src/main/content/jcr_root/perapi/definitions/admin.yaml
  */
-@Component(
-        service = Servlet.class,
-        property = {
-                SERVICE_DESCRIPTION + EQUALS + PER_PREFIX + "Component Definition Servlet",
-                SERVICE_VENDOR + EQUALS + PER_VENDOR,
-                SLING_SERVLET_METHODS + EQUALS + GET,
-                SLING_SERVLET_RESOURCE_TYPES + EQUALS + RESOURCE_TYPE_COMPONENT_DEFINITION
-        }
-)
+@Component(service = Servlet.class, property = {
+        SERVICE_DESCRIPTION + EQUALS + PER_PREFIX + "Component Definition Servlet",
+        SERVICE_VENDOR + EQUALS + PER_VENDOR, SLING_SERVLET_METHODS + EQUALS + GET,
+        SLING_SERVLET_RESOURCE_TYPES + EQUALS + RESOURCE_TYPE_COMPONENT_DEFINITION })
 @SuppressWarnings("serial")
-public final class ComponentDefinitionServlet extends AbstractBaseServlet {
+public class ComponentDefinitionServlet extends AbstractBaseServlet {
 
     private static final String EXPLORER_DIALOG_JSON = "explorer_dialog.json";
     private static final String DIALOG_JSON = "dialog.json";
     private static final String OG_TAG_DIALOG_JSON = "og_tag_dialog.json";
-    private static final String APPS_PREFIX = APPS_ROOT + SLASH;
-    private static final String _CONTENT_ = "/content/";
 
     @Override
-    protected Response handleRequest(final Request request) throws IOException {
-        final String path = request.getParameter(PATH);
+    protected Response handleRequest(Request request) throws IOException {
+        String path = request.getParameter(PATH);
         Resource resource = request.getResourceByPath(path);
-        boolean isPage = resource.isResourceType(PerConstants.PAGE_PRIMARY_TYPE);
-        if (isPage) {
+        boolean page = false;
+        if (resource.getResourceType().equals(PerConstants.PAGE_PRIMARY_TYPE)) {
+            page = true;
             resource = resource.getChild(PerConstants.JCR_CONTENT);
-        } else if ("/apps/admin/components/assetview".equals(path)) {
-            isPage = true;
         }
-
-        final JsonResponse answer = new JsonResponse();
-        final Resource typedResource = findTypedResource(resource);
-        final Resource component = extractComponent(typedResource);
-        final String componentPath = component.getPath();
+        String componentPath = "";
+        if (path.startsWith(APPS_ROOT + SLASH)) {
+            componentPath = path;
+        } else {
+            componentPath = APPS_ROOT + SLASH + resource.getValueMap().get(SLING_RESOURCE_TYPE, String.class);
+        }
+        Resource component = request.getResourceByPath(componentPath);
+        logger.debug("Component Path: '{}', Component: '{}'", componentPath, component);
+        if ("/apps/admin/components/assetview".equals(path)) {
+            page = true;
+        }
+        Resource dialog = component.getChild(page ? EXPLORER_DIALOG_JSON : DIALOG_JSON);
+        if (dialog == null) {
+            dialog = getDialogFromSuperType(component, page, false);
+        }
+        Resource ogTags = component.getChild(OG_TAG_DIALOG_JSON);
+        if (ogTags == null) {
+            ogTags = getDialogFromSuperType(component, page, true);
+        }
+        JsonResponse answer = new JsonResponse();
         answer.writeAttribute(PATH, componentPath);
         answer.writeAttribute(NAME, ServletHelper.componentPathToName(componentPath));
-
-        String value = getInheritedDialogRaw(path, component, isPage ? EXPLORER_DIALOG_JSON : DIALOG_JSON);
-        if (isNotBlank(value)) {
-            answer.writeAttributeRaw(MODEL, value);
+        if (dialog != null) {
+            answer.writeAttributeRaw(MODEL, rewriteDialogToTenant(path, dialog));
         }
-
-        value = getInheritedDialogRaw(path, component, OG_TAG_DIALOG_JSON);
-        if (isNotBlank(value)) {
-            answer.writeAttributeRaw(OG_TAGS, value);
+        if (ogTags != null) {
+            answer.writeAttributeRaw(OG_TAGS, rewriteDialogToTenant(path, ogTags));
         }
-
         return answer;
     }
 
-    private String getInheritedDialogRaw(final String path, final Resource component, final String relPath) {
-        return Optional.ofNullable(component)
-                .map(c -> getInheritedChild(c, relPath))
-                .map(r -> rewriteDialogToTenant(path, r))
-                .orElse(null);
+    /* quick method to serialize the dialog and convert all template specific paths to tenant paths */
+    private String rewriteDialogToTenant(String path, Resource dialog) throws IOException {
+        InputStream is = dialog.adaptTo(InputStream.class);
+        String answer = ServletHelper.asString(is).toString();
+        if(path != null && path.startsWith("/content/")) {
+            String tenantPath = path.substring(0, path.indexOf('/', 10));
+            return answer.replaceAll("\"/content/[^/]*/", "\"" + tenantPath + "/");
+        } else {
+            return answer;
+        }
     }
 
-    /* quick method to serialize the dialog and convert all template specific paths to tenant paths */
-    private String rewriteDialogToTenant(final String path, final Resource dialog) {
-        final InputStream is = dialog.adaptTo(InputStream.class);
-        try {
-            final String answer = ServletHelper.asString(is).toString();
-            if (startsWith(path, _CONTENT_)) {
-                String tenantPath = substringAfter(path, _CONTENT_);
-                tenantPath = _CONTENT_ + substringBefore(tenantPath, SLASH);
-                return answer.replaceAll("\"/content/[^/]*/", "\"" + tenantPath + SLASH);
+    private Resource getDialogFromSuperType(Resource resource, boolean page, boolean isMetaTag) {
+        String componentPath = resource.getValueMap().get(SLING_RESOURCE_SUPER_TYPE, String.class);
+        if(componentPath != null) {
+            if (!componentPath.startsWith(APPS_ROOT + SLASH)) {
+                componentPath = APPS_ROOT + SLASH + componentPath;
             }
-
-            return answer;
-        } catch (final IOException e) {
+            ResourceResolver resourceResolver = resource.getResourceResolver();
+            Resource component = resourceResolver.getResource(componentPath);
+            Resource dialog;
+            if(isMetaTag){
+              dialog = component.getChild(OG_TAG_DIALOG_JSON);
+            } else {
+              dialog = component.getChild(page ? EXPLORER_DIALOG_JSON : DIALOG_JSON);
+            }
+            if (dialog == null) {
+              if(isMetaTag){
+                return getDialogFromSuperType(component, page, true);
+              }
+              return getDialogFromSuperType(component, page, false);
+            } else {
+                return dialog;
+            }
+        } else {
             return null;
         }
     }
-
-    private Resource findTypedResource(final Resource resource) {
-        if (resource.getValueMap().containsKey(SLING_RESOURCE_TYPE)) {
-            return resource;
-        }
-
-        return findTypedResource(resource.getParent());
-    }
-
-    private Resource extractComponent(final Resource resource) {
-        if (resource.getPath().startsWith(APPS_PREFIX)) {
-            return resource;
-        }
-
-        return extractComponent(resource, Resource::getResourceType);
-    }
-
-    private Resource extractComponent(final Resource resource, final Function<Resource, String> getType) {
-        return Optional.of(resource)
-                .map(getType)
-                .map(s -> s.replaceAll(COLON, SLASH))
-                .map(t -> t.startsWith(APPS_PREFIX) ? t : APPS_PREFIX + t)
-                .map(resource::getChild)
-                .orElse(null);
-    }
-
-    private Resource getInheritedChild(final Resource component, final String relPath) {
-        final Resource answer = component.getChild(relPath);
-        if (nonNull(answer)) {
-            return answer;
-        }
-
-        final Resource superComponent = extractSuperComponent(component);
-        if (nonNull(superComponent)) {
-            return getInheritedChild(superComponent, relPath);
-        }
-
-        return null;
-    }
-
-    private Resource extractSuperComponent(final Resource resource) {
-        return extractComponent(resource,  Resource::getResourceSuperType);
-    }
-
 }
