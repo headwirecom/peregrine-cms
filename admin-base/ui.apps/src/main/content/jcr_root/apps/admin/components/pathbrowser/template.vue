97897cfaa28aa10faddbfea4f85571e99a43a60e
Felix Puetz <felix.puetz@metaproc.de>
2020-06-16 13:07:02 +0200

    fix focus loss issues when inserting img/link

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/pathbrowser/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/pathbrowser/template.vue
@@ -26,26 +26,25 @@
 <transition name="modal">
     <div class="modal-mask" v-on:click.stop.prevent="onCancel">
         <div class="modal-wrapper">
-        <div class="pathbrowser modal-container" v-on:click.stop.prevent="onPrevent">
+        <div class="pathbrowser modal-container"
+             @click.stop.prevent="onPrevent"
+             @mousedown.prevent="() => {}">
             <div class="modal-header" v-if="">
                 {{ header }}
             </div>
             <ul class="pathbrowser-tabs">
                 <li class="tab">
-                    <a href="#" :class="tab === 'browse' ? 'active' : ''" v-on:click="select('browse')"
-                       @mousedown.prevent="() => {}">
+                    <a href="#" :class="tab === 'browse' ? 'active' : ''" v-on:click="select('browse')">
                         <i class="material-icons">list</i>
                     </a>
                 </li>
                 <li class="tab">
-                    <a href="#" :class="tab === 'cards' ? 'active' : ''" v-on:click="select('cards')"
-                       @mousedown.prevent="() => {}">
+                    <a href="#" :class="tab === 'cards' ? 'active' : ''" v-on:click="select('cards')">
                         <i class="material-icons">view_module</i>
                     </a>
                 </li>
                 <li v-if="withLinkTab" class="tab">
-                    <a href="#" :class="tab === 'link' ? 'active' : ''" v-on:click="select('link')"
-                       @mousedown.prevent="() => {}">
+                    <a href="#" :class="tab === 'link' ? 'active' : ''" v-on:click="select('link')">
                         <i class="material-icons">link</i>
                     </a>
                 </li>
@@ -89,8 +88,7 @@
                                     <template v-if="!isRoot">
                                         <a
                                             href="#!"
-                                            v-on:click.stop.prevent="selectParent"
-                                            @mousedown.prevent="() => {}">
+                                            v-on:click.stop.prevent="selectParent">
                                             <i class="material-icons">keyboard_arrow_left</i>
                                         </a>
                                         {{currentPath}} ({{list.length}})
@@ -110,23 +108,20 @@
                             <template v-for="item in list">
                                 <li v-if="isFolder(item)"
                                     v-on:click.stop.prevent="navigateFolder(item)"
-                                    @mousedown.prevent="() => {}"
                                     :class="isSelected(item.path) ? 'selected' : ''">
                                     <template v-if="!isBrowserTypeImage">
                                         <input name="selectedItem" type="radio" class="with-gap" :checked="isSelected(item.path)" />
-                                        <label v-on:click.stop.prevent="selectItem(item)"
-                                               @mousedown.prevent="() => {}"></label>
+                                        <label v-on:click.stop.prevent="selectItem(item)"></label>
                                     </template>
                                     <i class="material-icons">{{getFolderIcon()}}</i>
                                     <span>{{item.name}}</span>
                                 </li>
                                 <li v-if="isFile(item) && isFileAllowed()"
                                     v-on:click.stop.prevent="selectItem(item)"
-                                    @mousedown.prevent="() => {}"
                                     :class="isSelected(item.path) ? 'selected' : ''">
                                     <template v-if="isSelectable(item)">
                                         <input name="selectedItem" type="radio" class="with-gap" :checked="isSelected(item.path)" />
-                                        <label @mousedown.prevent="() => {}"></label>
+                                        <label></label>
                                     </template>
                                     <i class="material-icons">image</i>
                                     <span>{{item.name}}</span>
@@ -355,12 +350,10 @@
                 </div>
                     <div class="pathbrowser-buttons">
                         <button v-on:click="onCancel"
-                                @mousedown.prevent="() => {}"
                                 class="modal-action waves-effect waves-light btn-flat">
                                 cancel
                         </button>
                         <button v-on:click="onSelect"
-                                @mousedown.prevent="() => {}"
                                 class="modal-action waves-effect waves-light btn-flat">
                                 select
                         </button>
