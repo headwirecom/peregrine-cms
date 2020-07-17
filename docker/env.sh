83cf0cfc36bdaedec07ab3e5870ec314196fbd97
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2020-07-17 11:38:25 -0700

    Initial support for adding Apache HTTPD to the Docker container.

--- docker/env.sh
+++ docker/env.sh
@@ -3,3 +3,7 @@
 if [ -z ${DOCKER_IMAGE+x} ]; then
   export DOCKER_IMAGE=peregrinecms/peregrine-cms:latest
 fi
+
+export PEREGRINE_SITE=themecleanflex
+export APACHE_DOMAIN=localhost
+export APACHE_PROXY_URL=http://localhost:8080/
--- docker/run.sh
+++ docker/run.sh
@@ -4,5 +4,8 @@
 
 echo "Using Docker image: ${DOCKER_IMAGE}"
  
-docker run -it -p 8080:8080 \
+docker run -it -p 8888:80 -p 8080:8080 \
+    -e APACHE_DOMAIN=${APACHE_DOMAIN} \
+    -e APACHE_PROXY_URL=${APACHE_PROXY_URL} \
+    -e PEREGRINE_SITE=${PEREGRINE_SITE} \
     ${DOCKER_IMAGE}
