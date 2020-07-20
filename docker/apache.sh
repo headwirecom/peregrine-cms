e7032fd3b77c08daff4fd7bd71b6536764550d03
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2020-07-20 09:20:54 -0700

    Re-added Sling tail log monitoring

--- docker/apache.sh
+++ docker/apache.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+echo "Giving time for Peregrine to startup..."
 sleep 15
 
 # Replicate site to file system
+echo "Replicating site content to file system..."
 curl -v -u admin:admin -d "withSite=themcleanflex" -X POST 'http://localhost:8080/perapi/admin/tenantSetupReplication.json/content/themecleanflex'
 
-apachectl -D FOREGROUND
+apachectl start
