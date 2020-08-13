4943421daa289fdb2611bfe841f7e94d8563b7f6
Felix Puetz <felix.puetz@metaproc.de>
2020-08-13 14:24:27 +0200

    improve export design of richtoolbar/groups/index.js

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/richtoolbar/groups/index.js
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/richtoolbar/groups/index.js
@@ -1,23 +1,11 @@
-import alwaysActive from './alwaysActive'
-import actions from './actions'
-import textFormat from './textFormat'
-import boldItalic from './boldItalic'
-import superSubScript from './superSubScript'
-import link from './link'
-import image from './image'
-import align from './align'
-import list from './list'
-import removeFormat from './removeFormat'
-import responsiveMenu from './responsiveMenu'
-
-export const alwaysActiveGroup = alwaysActive
-export const actionsGroup = actions
-export const textFormatGroup = textFormat
-export const boldItalicGroup = boldItalic
-export const superSubScriptGroup = superSubScript
-export const linkGroup = link
-export const imageGroup = image
-export const alignGroup = align
-export const listGroup = list
-export const removeFormatGroup = removeFormat
-export const responsiveMenuGroup = responsiveMenu
+export {default as alwaysActiveGroup} from './alwaysActive'
+export {default as actionsGroup} from './actions'
+export {default as textFormatGroup} from './textFormat'
+export {default as boldItalicGroup} from './boldItalic'
+export {default as superSubScriptGroup} from './superSubScript'
+export {default as linkGroup} from './link'
+export {default as imageGroup} from './image'
+export {default as alignGroup} from './align'
+export {default as listGroup} from './list'
+export {default as removeFormatGroup} from './removeFormat'
+export {default as responsiveMenuGroup} from './responsiveMenu'
