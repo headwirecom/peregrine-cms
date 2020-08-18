06797f4d497213a4b4c723bb90fe2d01cc3abf2f
Felix Puetz <felix.puetz@metaproc.de>
2020-08-18 14:05:26 +0200

    transfer from single-button to drop-down-button in richtoolbar no more changes width

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/richtoolbargroup/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/richtoolbargroup/template.vue
@@ -1,43 +1,45 @@
 <template>
-  <admin-components-materializedropdown
-      v-if="collapse && items && items.length > 1"
-      tag="button"
-      class="btn"
-      :class="{'active': active}"
-      :title="vLabel"
-      :below-origin="true"
-      :items="vItems"
-      @mousedown.native.prevent="() => {}"
-      @item-click="$emit('click', {btn: $event})">
-    <admin-components-icon v-if="icon" :icon="vIcon" :lib="iconLib"/>
-    <span class="caret-down"></span>
-  </admin-components-materializedropdown>
+  <div class="btn-group" :class="[`group-${vLabel}`]">
+    <admin-components-materializedropdown
+        v-if="collapse && items && items.length > 1"
+        tag="button"
+        class="btn"
+        :class="[{'active': active}]"
+        :title="vLabel"
+        :below-origin="true"
+        :items="vItems"
+        @mousedown.native.prevent="() => {}"
+        @item-click="$emit('click', {btn: $event})">
+      <admin-components-icon v-if="icon" :icon="vIcon" :lib="iconLib"/>
+      <span class="caret-down"></span>
+    </admin-components-materializedropdown>
 
-  <div v-else :class="['btn-group', `group-${vLabel}`]">
-    <template v-for="(btn, i) in items">
-      <admin-components-richtoolbargroup
-          v-if="btn.items && btn.items.length > 1"
-          :key="`rich-toolbar-sub-group-${i}`"
-          :icon="btn.icon"
-          :iconLib="btn.iconLib"
-          :collapse="btn.collapse"
-          :label="btn.label"
-          :title="btn.title"
-          :active="btn.active"
-          :items="btn.items"
-          :class="btn.class"
-          @click="$emit('click', $event)"/>
-      <admin-components-richtoolbarbtn
-          v-else
-          :key="getButtonKey(btn, i)"
-          :items="btn.items"
-          :icon="btn.icon"
-          :icon-lib="btn.iconLib"
-          :label="btn.label"
-          :class="btn.class"
-          :title="$i18n(btn.title)"
-          :active="btn.isActive? btn.isActive() : false"
-          @click="$emit('click', {btn})"/>
+    <template v-else>
+      <template v-for="(btn, i) in items">
+        <admin-components-richtoolbargroup
+            v-if="btn.items && btn.items.length > 1"
+            :key="`rich-toolbar-sub-group-${i}`"
+            :icon="btn.icon"
+            :iconLib="btn.iconLib"
+            :collapse="btn.collapse"
+            :label="btn.label"
+            :title="btn.title"
+            :active="btn.active"
+            :items="btn.items"
+            :class="btn.class"
+            @click="$emit('click', $event)"/>
+        <admin-components-richtoolbarbtn
+            v-else
+            :key="getButtonKey(btn, i)"
+            :items="btn.items"
+            :icon="btn.icon"
+            :icon-lib="btn.iconLib"
+            :label="btn.label"
+            :class="btn.class"
+            :title="$i18n(btn.title)"
+            :active="btn.isActive? btn.isActive() : false"
+            @click="$emit('click', {btn})"/>
+      </template>
     </template>
   </div>
 </template>
