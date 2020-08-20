069e19dab63018e21bd50436cf60ceecc2785670
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2020-08-20 10:16:15 -0700

    Docker: Added support for starting Sling for the first and as an entry point.

--- docker/scripts/install-sling.sh
+++ docker/scripts/install-sling.sh
@@ -29,3 +29,20 @@ mv -v target/slingfeature-tmp/feature-oak_tar.json /app/sling
 cd ..
 
 rm -rf ${DIR}
+
+echo "Starting Sling for the first time..."
+cd /app/sling && java -jar /app/sling/org.apache.sling.feature.launcher.jar \
+    -f /app/sling/feature-oak_tar.json \
+    -p /app/sling \
+    -c /app/sling/launcher/cache &
+
+# Wait for Sling to fully start up
+STATUS=$(curl -u admin:admin -s --fail  http://localhost:8080/system/console/bundles.json | jq '.s[3:5]' -c)
+if [ "$STATUS" != "[0,0]" ]; then
+  while [ "$STATUS" != "[0,0]" ]
+  do
+    echo "Sling still starting. Waiting for all bundles to be ready.."
+    STATUS=$(curl -u admin:admin -s --fail  http://localhost:8080/system/console/bundles.json | jq '.s[3:5]' -c)
+    sleep 5
+  done
+fi
