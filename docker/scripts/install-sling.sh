64f2f1555faac001cb1d994e2556308bc1d12c53
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2020-08-20 08:19:51 -0700

    Docker: Added Sling12 build. Remaining Dockerfile commented out... work in progress.

--- docker/scripts/install-sling.sh
+++ docker/scripts/install-sling.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-echo "Fetching Sling Git repostory..."
+usage () {
+  echo "Usage: `basename $0` <sling repo> <sling branch>"
+}
+
+if [ $# -ne 2 ]; then
+  usage
+  exit 1
+fi
+
+REPO=$1
+BRANCH=$2
+DIR=sling-org-apache-sling-starter
+
+echo "Fetching Sling from repo: ${REPO}"
+echo "Building Sling using branch: ${BRANCH}"
+
+# Build Sling
+git clone ${REPO}
+cd ${DIR}
+git checkout ${BRANCH}
+mvn clean install
+
+# Move Sling assets to target installation dir
+mkdir -v -p /app/sling
+mv -v target/dependency/org.apache.sling.feature.launcher.jar /app/sling
+mv -v target/slingfeature-tmp/feature-oak_tar.json /app/sling
+cd ..
+
+rm -rf ${DIR}
