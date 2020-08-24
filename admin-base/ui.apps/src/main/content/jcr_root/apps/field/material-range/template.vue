d52a64e83680cb09c788bd0ea08dfc34997bcc87
Felix Puetz <felix.puetz@metaproc.de>
2020-08-24 13:05:21 +0200

    refine material-range slider by adding input and min/max on the range

--- admin-base/ui.apps/src/main/content/jcr_root/apps/field/material-range/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/field/material-range/template.vue
@@ -1,34 +1,72 @@
 <template>
-	<p v-if="!schema.preview" class="range-field">
-		<input
-			type="range"
-			v-model="value"
-			:id="getFieldID(schema)"
-			:class="schema.fieldClasses"
-			:disabled="schema.disabled || schema.preview"
-			:alt="schema.alt"
-			:max="schema.max"
-			:min="schema.min"
-			:name="schema.inputName"
-			:required="schema.required"
-			:step="schema.step"
-		/>
-		<input 
-			type="checkbox" 
-			:disabled="schema.disabled || schema.preview"
-			:id="getFieldID(schema)+'-enabled'" />
-		<label :for="getFieldID(schema)+'-enabled'">enabled</label>
-		<input 	
-			type="text" 
-			v-model="value" 
-			:disabled="schema.disabled || schema.preview"
-		/>
-	</p>
-	<div v-else>{{value}}</div>
+  <div v-if="!schema.preview" class="range-field" :class="{'is-empty': !value}">
+    <input
+        v-if="value"
+        ref="range"
+        type="range"
+        class="range"
+        v-model="value"
+        :id="getFieldID(schema)"
+        :class="[schema.fieldClasses]"
+        :disabled="schema.disabled || schema.preview"
+        :alt="schema.alt"
+        :max="schema.max"
+        :min="schema.min"
+        :name="schema.inputName"
+        :required="schema.required"
+        :step="schema.step"/>
+    <div v-else class="empty-range">
+      <div class="rail"></div>
+    </div>
+    <input
+        type="number"
+        class="range-value"
+        v-model="value"
+        :id="`${getFieldID(schema)}-input`"
+        :disabled="schema.disabled || schema.preview"
+        :alt="schema.alt"
+        :max="schema.max"
+        :min="schema.min"
+        :step="schema.step"
+        :name="`${getFieldID(schema)}-input`"
+        :required="schema.required"
+        placeholder="null"
+        @keypress="onKeyPress"/>
+    <div class="range-numbers">
+      <div class="min">{{ min }}</div>
+      <div class="max">{{ max }}</div>
+    </div>
+  </div>
+  <div v-else>{{ value }}</div>
 </template>
 
-<script>	
-	export default {
-		mixins: [ VueFormGenerator.abstractField ],
-	}
+<script>
+export default {
+  mixins: [VueFormGenerator.abstractField],
+  data() {
+    return {
+      delay: 10,
+      timeout: null
+    }
+  },
+  computed: {
+    min() {
+      return this.schema.min || 0
+    },
+    max() {
+      return this.schema.max || 100
+    }
+  },
+  methods: {
+    onKeyPress(event) {
+      const oldVal = event.target.value
+
+      if (!oldVal) return
+
+      if (oldVal >= this.max) {
+        event.preventDefault()
+      }
+    }
+  }
+}
 </script>
