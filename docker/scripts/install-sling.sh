b607330ad90ee6f96edd913a6e7b847a1e887ea1
Cris Rockwell <cmrockwe@umich.edu>
2020-08-27 18:06:28 -0400

    WIP docker script changes to build Sling 12 from peregrine-builder-sling12 directory

--- docker/scripts/install-sling.sh
+++ docker/scripts/install-sling.sh
@@ -1,38 +1,43 @@
 #!/bin/bash
 
 usage () {
-  echo "Usage: `basename $0` <sling repo> <sling branch>"
+  echo "Usage: `basename $0` <sling repo> <sling branch> <directory>"
 }
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 3 ]; then
   usage
   exit 1
 fi
 
 REPO=$1
 BRANCH=$2
-DIR=sling-org-apache-sling-starter
+DIR1=$3
+DIR2=peregrine-builder-sling-12
+#DIR=sling-org-apache-sling-starter
 
 echo "Fetching Sling from repo: ${REPO}"
+echo "Building Sling using folder: ${DIR1}"
 echo "Building Sling using branch: ${BRANCH}"
+
 
 # Build Sling
 git clone ${REPO}
-cd ${DIR}
+cd ${DIR1}
 git checkout ${BRANCH}
+cd ${DIR2}
 mvn clean install
 
 # Move Sling assets to target installation dir
 mkdir -v -p /app/sling
 mv -v target/dependency/org.apache.sling.feature.launcher.jar /app/sling
-mv -v target/slingfeature-tmp/feature-oak_tar.json /app/sling
+mv -v target/slingfeature-tmp/feature-oak_tar_fds.json /app/sling
 cd ..
 
-rm -rf ${DIR}
+rm -rf ${DIR1}
 
 echo "Starting Sling for the first time..."
 cd /app/sling && java -jar /app/sling/org.apache.sling.feature.launcher.jar \
-    -f /app/sling/feature-oak_tar.json \
+    -f /app/sling/feature-oak_tar_fds.json \
     -p /app/sling \
     -c /app/sling/launcher/cache &
 
