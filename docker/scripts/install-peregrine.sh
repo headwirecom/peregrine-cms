d1885c760b1ed3b40a6c60db24a46d18a9011c4e
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2020-09-21 12:02:03 -0700

    feature/sling12 docker (#567)
    * Refactored Docker build for Sling12
    
    * Added themeclean-flex to the build

--- docker/scripts/install-peregrine.sh
+++ docker/scripts/install-peregrine.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
-usage () {
-  echo "Usage: `basename $0` <peregrine repo> <peregrine branch>"
-}
+echo "Building peregrine..."
 
-if [ $# -ne 2 ]; then
-  usage
-  exit 1
-fi
+SAVE_PWD=`pwd`
 
-REPO=$1
-BRANCH=$2
-DIR=peregrine-cms
-
-echo "Fetching peregrine from repo: ${REPO}"
-echo "Building peregrine using branch: ${BRANCH}"
-
-# Build Peregrine
-git clone ${REPO}
-cd ${DIR}
-git checkout ${BRANCH}
-
+echo "Starting Sling for the first time..."
 /app/scripts/start.sh
 
-mvn clean install -PautoInstallPackage --quiet
+cd peregrine-cms
+mvn clean install -PautoInstallPackage
+cd ../themeclean-flex
+mvn clean install -PautoInstallPackage
+
+cd ${SAVE_PWD}
--- docker/scripts/install-sling.sh
+++ docker/scripts/install-sling.sh
@@ -1,40 +1,16 @@
 #!/bin/bash
 
-usage () {
-  echo "Usage: `basename $0` <sling repo> <sling branch> <directory>"
-}
+echo "Building Sling"
 
-if [ $# -ne 3 ]; then
-  usage
-  exit 1
-fi
-
-REPO=$1
-BRANCH=$2
-DIR1=$3
-DIR2=peregrine-builder-sling-12
-#DIR=sling-org-apache-sling-starter
-
-echo "Fetching Sling from repo: ${REPO}"
-echo "Building Sling using folder: ${DIR1}"
-echo "Building Sling using branch: ${BRANCH}"
-
+SAVE_PWD=`pwd`
 
 # Build Sling
-git clone ${REPO}
-cd ${DIR1}
-git checkout ${BRANCH}
-cd ${DIR2}
+cd peregrine-cms/sling/peregrine-builder-sling-12
 mvn clean install --quiet
 
 # Move Sling assets to target installation dir
 mkdir -v -p /app/sling
 mv -v target/dependency/org.apache.sling.feature.launcher.jar /app/sling
 mv -v target/slingfeature-tmp/feature-oak_tar.json /app/sling
-cd ../..
 
-rm -rf ${DIR1}
-
-echo "Starting Sling for the first time..."
-
-/app/scripts/start.sh
+cd ${SAVE_PWD}
