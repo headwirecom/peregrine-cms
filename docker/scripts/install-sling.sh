3f3d851cfc51173fa28a1a89e556f3a21d2ae856
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2020-09-25 11:12:38 -0700

    Updated Sling startup to use a Feature Archive

--- docker/scripts/install-sling.sh
+++ docker/scripts/install-sling.sh
@@ -6,11 +6,13 @@ SAVE_PWD=`pwd`
 
 # Build Sling
 cd peregrine-cms/sling/peregrine-builder-sling-12
-mvn clean install --quiet
+mvn clean install
 
 # Move Sling assets to target installation dir
 mkdir -v -p /app/sling
 mv -v target/dependency/org.apache.sling.feature.launcher.jar /app/sling
 mv -v target/slingfeature-tmp/feature-oak_tar.json /app/sling
+mv -v target/com.peregrine-cms.sling.launchpad-12-SNAPSHOT-oak_tar_far.far /app/sling
+
 
 cd ${SAVE_PWD}
--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 cd /app/sling && java -jar /app/sling/org.apache.sling.feature.launcher.jar \
-    -f /app/sling/feature-oak_tar.json \
-    -p /app/sling  \
+    -D sling.runmodes=author,notshared,oak_tar_fds \
+    -f /app/sling/com.peregrine-cms.sling.launchpad-12-SNAPSHOT-oak_tar_far.far \
+    -p /app/sling \
     -c /app/sling/launcher/cache &
 
 # Wait for Sling to fully start up
