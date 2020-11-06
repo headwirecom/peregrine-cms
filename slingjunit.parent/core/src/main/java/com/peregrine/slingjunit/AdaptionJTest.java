ccc3a5db3c6e7847c70f744d8c2e8aff94c7b79e
Cris Rockwell <cmrockwe@umich.edu>
2020-11-06 09:55:39 -0500

    618 added ssrPageMergeScenario1 for PageMerge::getMergedResources. Test whether getMergedResources properly merges template content with page content for HTL rendering

--- slingjunit.parent/core/src/main/java/com/peregrine/slingjunit/AdaptionJTest.java
+++ slingjunit.parent/core/src/main/java/com/peregrine/slingjunit/AdaptionJTest.java
@@ -2,12 +2,15 @@ package com.peregrine.slingjunit;
 
 import com.peregrine.adaption.PerAsset;
 import com.peregrine.adaption.PerPage;
+import com.peregrine.nodetypes.merge.PageMerge;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.resource.ResourceUtil;
+import org.apache.sling.models.factory.ModelFactory;
 import org.apache.sling.junit.annotations.SlingAnnotationsTestRunner;
 import org.apache.sling.junit.annotations.TestReference;
+import org.apache.sling.servlethelpers.MockSlingHttpServletRequest;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,28 +18,38 @@ import org.junit.runner.RunWith;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.nodetype.NodeType;
+import javax.script.SimpleBindings;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.*;
 
-
+/**
+ * Run test using this URL
+ * http://localhost:8080/system/sling/junit/com.peregrine.slingjunit.AdaptionJTest.html
+ *
+ */
 @RunWith(SlingAnnotationsTestRunner.class)
 public class AdaptionJTest {
-    private static String PAGE_EXAMPLE = "/content/example/pages/contact";
+    private static String CONTACT_PAGE_PATH = "/content/example/pages/contact";
     private static String ASSET_EXAMPLE = "/content/example/assets/images/logo.png";
     private static String OBJECT_EXAMPLE = "/content/example/objects/sample";
     private static String TEMPLATE_EXAMPLE = "/content/example/templates/base";
 
-
+    private MockSlingHttpServletRequest request;
     private Resource resource;
     @TestReference
     private ResourceResolverFactory resolverFactory;
     private ResourceResolver resourceResolver;
 
+    @TestReference
+    private ModelFactory modelFactory;
 
     @Test
     public void getSiteFromPage(){
         assertNotNull(resourceResolver);
-        resource = resourceResolver.getResource(PAGE_EXAMPLE);
+        resource = resourceResolver.getResource(CONTACT_PAGE_PATH);
         assertFalse( ResourceUtil.isNonExistingResource(resource));
         PerPage page = resource.adaptTo(PerPage.class);
         assertNotNull(page);
@@ -73,14 +86,60 @@ public class AdaptionJTest {
         assertSiteResource(template.getSiteResource());
     }
 
-//    TODO: Object needs Adaption implemented
-//    @Test
-//    public void getSiteFromObject(){
-//    }
+    /**
+     * Server Side Rendering SSR Page Merge Scenario 1
+     *
+     * Template has structural components (nav and footer) no containers
+     * Page has content container with children
+     *
+     * Template: /content/example/templates/base/
+     *      _jcr_content/nav
+     *      _jcr_content/footer
+     *
+     * Page: /content/example/pages/contact/
+     *      _jcr_content/nfceefd40-b802-4203-8147-9cb2b2c78c6b
+     *      _jcr_content/n3736dc36-9cc3-49d7-a7d4-bf4d94e0ea2f
+     *          n8680c077-cc22-40d3-8989-d86d832a85d1
+     *
+     * Resulting Page Merge
+     *  /content/example/templates/base/_jcr_content/nav
+     *  /content/example/templates/base/_jcr_content/footer
+     *  /content/example/pages/contact/_jcr_content/nfceefd40-b802-4203-8147-9cb2b2c78c6b
+     *  /content/example/pages/contact/_jcr_content/n3736dc36-9cc3-49d7-a7d4-bf4d94e0ea2f
+     *  /content/example/pages/contact/_jcr_content/n3736dc36-9cc3-49d7-a7d4-bf4d94e0ea2f/n8680c077-cc22-40d3-8989-d86d832a85d1
+     *
+     */
+    @Test
+    public void ssrPageMergeScenario1(){
+        resource = resourceResolver.getResource(CONTACT_PAGE_PATH);
+        List<String> resourcePaths = new ArrayList<>();
+        resourcePaths.add("/content/example/templates/base/jcr:content/nav");
+        resourcePaths.add("/content/example/templates/base/jcr:content/footer");
+        resourcePaths.add("/content/example/pages/contact/jcr:content/nfceefd40-b802-4203-8147-9cb2b2c78c6b");
+        resourcePaths.add("/content/example/pages/contact/jcr:content/n3736dc36-9cc3-49d7-a7d4-bf4d94e0ea2f");
+// Note: children of containers are not included in the resource list.
+// /content/example/pages/contact/jcr:content/n3736dc36-9cc3-49d7-a7d4-bf4d94e0ea2f/n8680c077-cc22-40d3-8989-d86d832a85d1
+// resources inside containers are rendered when the container is rendered, when it lists all children
+        SimpleBindings bindings = new SimpleBindings();
+        bindings.put("resource", resource);
+        bindings.put("request", request);
+        bindings.put("modelFactory", modelFactory);
+
+        PageMerge pageMerge = new PageMerge();
+        pageMerge.init(bindings);
+        List<Resource> actualResources = pageMerge.getMergedResources();
+        assertEquals(resourcePaths.size(), actualResources.size());
+        List<String> actualResourcePaths = actualResources.stream()
+            .map(Resource::getPath)
+            .collect(Collectors.toList());
+        assertTrue(actualResourcePaths.containsAll(resourcePaths));
+        assertTrue(resourcePaths.containsAll(actualResourcePaths));
+    }
 
     @Before
     public void setUp() throws Exception {
         resourceResolver = resolverFactory.getAdministrativeResourceResolver(null);
+        request = new MockSlingHttpServletRequest(resourceResolver);
     }
 
     @After
