b5e78c82e30c7513288da0d725c3f2bf1b95fa7d
Cris Rockwell <cmrockwe@umich.edu>
2020-11-06 16:22:09 -0500

    refactored unit tests moving common binding to setup. added test for SSR scenario 2: sub-template inheritance

--- slingjunit.parent/core/src/main/java/com/peregrine/slingjunit/AdaptionJTest.java
+++ slingjunit.parent/core/src/main/java/com/peregrine/slingjunit/AdaptionJTest.java
@@ -39,6 +39,7 @@ public class AdaptionJTest {
 
     private MockSlingHttpServletRequest request;
     private Resource resource;
+    private SimpleBindings bindings;
     @TestReference
     private ResourceResolverFactory resolverFactory;
     private ResourceResolver resourceResolver;
@@ -120,11 +121,7 @@ public class AdaptionJTest {
 // Note: children of containers are not included in the resource list.
 // /content/example/pages/contact/jcr:content/n3736dc36-9cc3-49d7-a7d4-bf4d94e0ea2f/n8680c077-cc22-40d3-8989-d86d832a85d1
 // resources inside containers are rendered when the container is rendered, when it lists all children
-        SimpleBindings bindings = new SimpleBindings();
         bindings.put("resource", resource);
-        bindings.put("request", request);
-        bindings.put("modelFactory", modelFactory);
-
         PageMerge pageMerge = new PageMerge();
         pageMerge.init(bindings);
         List<Resource> actualResources = pageMerge.getMergedResources();
@@ -136,10 +133,41 @@ public class AdaptionJTest {
         assertTrue(resourcePaths.containsAll(actualResourcePaths));
     }
 
+    /**
+     ssrPageMergeScenario2 test pages based on a sub-template
+     sub-template: /content/example/templates/base/sub-template
+     page:
+     */
+    @Test
+    public void ssrPageMergeScenario2(){
+        resource = resourceResolver.getResource("/content/example/pages/sub-template-page");
+        // set up expected resource list
+        List<String> resourcePaths = new ArrayList<>();
+        resourcePaths.add("/content/example/templates/base/jcr:content/nav");
+        resourcePaths.add("/content/example/templates/base/jcr:content/footer");
+        resourcePaths.add("/content/example/pages/sub-template-page/jcr:content/container");
+        bindings.put("resource", resource);
+        PageMerge pageMerge = new PageMerge();
+        pageMerge.init(bindings);
+        // run test
+        List<Resource> actualResources = pageMerge.getMergedResources();
+        assertEquals(resourcePaths.size(), actualResources.size());
+        List<String> actualResourcePaths = actualResources.stream()
+                .map(Resource::getPath)
+                .collect(Collectors.toList());
+        assertTrue(actualResourcePaths.containsAll(resourcePaths));
+        assertTrue(resourcePaths.containsAll(actualResourcePaths));
+    }
+
+
     @Before
     public void setUp() throws Exception {
         resourceResolver = resolverFactory.getAdministrativeResourceResolver(null);
         request = new MockSlingHttpServletRequest(resourceResolver);
+        bindings = new SimpleBindings();
+        bindings.put("request", request);
+        bindings.put("modelFactory", modelFactory);
+        bindings.put("resolver", resourceResolver);
     }
 
     @After
