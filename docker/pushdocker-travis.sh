412a42b45fc8e687dca82c6bff52b06e9b523312
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2020-11-10 02:27:22 -0700

    Add branch naming convention to PR branch

--- docker/pushdocker-travis.sh
+++ docker/pushdocker-travis.sh
@@ -6,7 +6,8 @@ env | grep "PULL_REQUEST"
 
 if [ ${TRAVIS_PULL_REQUEST} != "false" ]; then
   echo "Pull request build detected"
+  BRANCH="$(echo ${TRAVIS_PULL_REQUEST_BRANCH} | tr '/' '-' | tr '[:upper:]' '[:lower:]')"
   docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-  docker tag "$DOCKER_IMAGE" "${DOCKER_IMAGE}:${TRAVIS_PULL_REQUEST_BRANCH}"
-  docker push ${DOCKER_IMAGE}:${TRAVIS_PULL_REQUEST_BRANCH}
+  docker tag "$DOCKER_IMAGE" "${DOCKER_IMAGE}:${BRANCH}"
+  docker push ${DOCKER_IMAGE}:${BRANCH}
 fi
