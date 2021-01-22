dc567cabb2115a77ac6b58393f259ec50e64b4e8
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2021-01-22 16:06:57 -0700

    Added support for uploading Peregrine packages. Package installation pending.

--- docker/scripts/install-peregrine.sh
+++ docker/scripts/install-peregrine.sh
@@ -2,16 +2,28 @@
 # exit if any command fails
 set -e
 
-echo "Building peregrine..."
+PACKAGE_DIR=/app/binaries
 
 SAVE_PWD=`pwd`
 
 echo "Starting Sling for the first time..."
 /app/scripts/start.sh
 
-cd peregrine-cms
-mvn clean install -PautoInstallPackage
-cd ../themeclean-flex
-mvn clean install -PautoInstallPackage
+echo "Installing Sling Packager"
+npm install @peregrinecms/slingpackager -g
+
+ls ${PACKAGE_DIR}/*.zip | while read package;
+do
+  echo "Uploading package: ${package}..."
+  slingpackager -v upload ${package}
+done
+
+slingpackager list
+
+slingpackager list | awk '{print $1}' | cut -d"=" -f2 | while read package;
+do
+  echo "Installing package: ${package}..."
+#  slingpackager -v install ${package}
+done
 
 cd ${SAVE_PWD}
