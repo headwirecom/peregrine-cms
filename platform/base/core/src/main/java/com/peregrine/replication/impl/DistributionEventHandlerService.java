446edea3c5a0dd5d0ee122617cb2901ede92c3a6
Radosław Wesołowski <rw@headwire.com>
2021-01-29 12:48:31 +0100

    Simplify DistributionEventHandlerService

--- platform/base/core/src/main/java/com/peregrine/replication/impl/DistributionEventHandlerService.java
+++ platform/base/core/src/main/java/com/peregrine/replication/impl/DistributionEventHandlerService.java
@@ -1,6 +1,8 @@
 package com.peregrine.replication.impl;
 
+import com.peregrine.commons.util.PerUtil;
 import com.peregrine.replication.DistributionEventPojo;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
@@ -12,14 +14,13 @@ import org.osgi.service.event.EventHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.peregrine.replication.ReplicationUtil.updateReplicationProperties;
 import static com.peregrine.commons.util.PerConstants.*;
 import static com.peregrine.commons.util.PerUtil.EQUALS;
 import static com.peregrine.commons.util.PerUtil.PER_PREFIX;
 import static com.peregrine.commons.util.PerUtil.loginService;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.apache.sling.distribution.event.DistributionEventTopics.AGENT_PACKAGE_DISTRIBUTED;
 import static org.apache.sling.distribution.event.DistributionEventTopics.IMPORTER_PACKAGE_IMPORTED;
 import static org.osgi.framework.Constants.SERVICE_VENDOR;
@@ -44,8 +45,6 @@ import static org.osgi.service.event.EventConstants.EVENT_TOPIC;
 public class DistributionEventHandlerService implements EventHandler {
 
     private static final String DISTRIBUTION_TYPE_ADD = "ADD";
-    private static final String DISTRIBUTION_TYPE_DELETE = "DELETE";
-
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
@@ -54,34 +53,26 @@ public class DistributionEventHandlerService implements EventHandler {
     private ResourceResolverFactory resourceResolverFactory;
 
     @Override
-    public void handleEvent(Event event) {
-        final String topic = event.getTopic();
-
-        if (AGENT_PACKAGE_DISTRIBUTED.equals(topic) || IMPORTER_PACKAGE_IMPORTED.equals(topic)) {
-            final DistributionEventPojo distributionEventPojo = new DistributionEventPojo(event);
-            setReplicationProperties(distributionEventPojo);
+    public void handleEvent(final Event event) {
+        if (StringUtils.equalsAny(event.getTopic(), AGENT_PACKAGE_DISTRIBUTED, IMPORTER_PACKAGE_IMPORTED)) {
+            setReplicationProperties(new DistributionEventPojo(event));
         }
     }
 
-    private void setReplicationProperties(DistributionEventPojo distributionEventPojo) {
-        try {
-            ResourceResolver finalResourceResolver = loginService(resourceResolverFactory, DISTRIBUTION_SUB_SERVICE);
-            List<String> paths = Arrays.stream(distributionEventPojo.getPaths())
-                    .filter( path -> path.endsWith(JCR_CONTENT) || !path.contains(JCR_CONTENT))
-                    .collect(Collectors.toList());
+    private void setReplicationProperties(final DistributionEventPojo data) {
+        try (final ResourceResolver resourceResolver = loginService(resourceResolverFactory, DISTRIBUTION_SUB_SERVICE)) {
+            Arrays.stream(data.getPaths())
+                    .filter(path -> PerUtil.isJcrContent(path) || !path.contains(JCR_CONTENT))
+                    .forEach(path -> {
+                        String replicationRef = EMPTY;
+                        if (DISTRIBUTION_TYPE_ADD.equals(data.getDistributionType().name())) {
+                            replicationRef = data.getDistributionComponentKind() + "://" + path;
+                        }
 
-            for (String path : paths) {
-                String replicationRef = "";
-                Resource resource = finalResourceResolver.getResource(path);
-                if (DISTRIBUTION_TYPE_ADD.equals( distributionEventPojo.getDistributionType().name())) {
-                    replicationRef = distributionEventPojo.getDistributionComponentKind() + "://" + path;
-
-                } else if (DISTRIBUTION_TYPE_DELETE.equals( distributionEventPojo.getDistributionType().name())) {
-                    replicationRef = null;
-                }
-                log.info("properties for {} were updated by dist event handler.",path);
-                updateReplicationProperties(resource, replicationRef, null);
-            }
+                        log.info("properties for {} were updated by dist event handler.", path);
+                        final Resource resource = resourceResolver.getResource(path);
+                        updateReplicationProperties(resource, replicationRef, null);
+                    });
         } catch (LoginException e) {
             log.error("Failed to update per:Replication properties", e);
         }
