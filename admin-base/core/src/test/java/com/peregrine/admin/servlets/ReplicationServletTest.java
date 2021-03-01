994f11784c3db42c3e4d6b6635d24b8943b62b85
Radosław Wesołowski <radoslaw.wesolowski@gmail.com>
2021-03-01 11:45:25 +0100

    Extract ReplicationServletTestBase

--- admin-base/core/src/test/java/com/peregrine/admin/servlets/ReplicationServletTest.java
+++ admin-base/core/src/test/java/com/peregrine/admin/servlets/ReplicationServletTest.java
@@ -1,54 +1,27 @@
 package com.peregrine.admin.servlets;
 
-import com.peregrine.SlingServletTest;
-import com.peregrine.admin.resource.AdminResourceHandler;
-import com.peregrine.commons.servlets.AbstractBaseServlet;
-import com.peregrine.replication.PerReplicable;
 import com.peregrine.replication.Replication;
-import junitx.util.PrivateAccessor;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.Collections;
 
 import static com.peregrine.admin.servlets.ReplicationServlet.DEACTIVATE;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-public final class ReplicationServletTest extends SlingServletTest {
+public final class ReplicationServletTest extends ReplicationServletTestBase {
 
-    private final ReplicationServlet servlet = new ReplicationServlet();
-
-    private final AdminResourceHandler resourceManagement = mock(AdminResourceHandler.class);
-    private final Replication replication = mock(Replication.class);
-    private final PerReplicable replicable = mock(PerReplicable.class);
-
-    @Before
-    public void setUp() throws NoSuchFieldException, Replication.ReplicationException {
-        PrivateAccessor.setField(servlet, "resourceManagement", resourceManagement);
-        jcrContent.addAdapter(replicable);
-        when(replicable.getMainResource()).thenReturn(jcrContent);
+    public ReplicationServletTest() throws NoSuchFieldException, Replication.ReplicationException {
+        super(new ReplicationServlet());
     }
 
     @Test
     public void performDeactivation() throws IOException, Replication.ReplicationException {
         request.putParameter(DEACTIVATE, true);
-        final AbstractBaseServlet.Request request = new AbstractBaseServlet.Request(this.request, response);
-        when(replication.deactivate(page)).thenReturn(Collections.singletonList(jcrContent));
-        final String response = servlet.performReplication(replication, request, page, resourceResolver).getContent();
-        assertTrue(response.contains(jcrContent.getPath()));
+        performReplicationResponseContains(jcrContent);
     }
 
     @Test
     public void performActivation() throws IOException, Replication.ReplicationException {
-        when(replication.prepare(any())).thenAnswer(i -> i.getArguments()[0]);
-        when(replication.replicate(any())).thenAnswer(i -> i.getArguments()[0]);
-        final AbstractBaseServlet.Request request = new AbstractBaseServlet.Request(this.request, response);
-        final String response = servlet.performReplication(replication, request, page, resourceResolver).getContent();
-        assertTrue(response.contains(jcrContent.getPath()));
+        performReplicationResponseContains(jcrContent);
     }
 
 }
---
+++ admin-base/core/src/test/java/com/peregrine/admin/servlets/ReplicationServletTestBase.java
@@ -0,0 +1,72 @@
+package com.peregrine.admin.servlets;
+
+import com.peregrine.SlingServletTest;
+import com.peregrine.admin.resource.AdminResourceHandler;
+import com.peregrine.commons.servlets.AbstractBaseServlet;
+import com.peregrine.mock.PageMock;
+import com.peregrine.replication.PerReplicable;
+import com.peregrine.replication.Replication;
+import com.peregrine.replication.ReplicationsContainerWithDefault;
+import junitx.util.PrivateAccessor;
+import org.apache.sling.api.resource.Resource;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.StreamSupport;
+
+import static com.peregrine.admin.servlets.ReplicationServlet.DEACTIVATE;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ReplicationServletTestBase extends SlingServletTest {
+
+    private final ReplicationServletBase servlet;
+
+    private final AdminResourceHandler resourceManagement = mock(AdminResourceHandler.class);
+    private final ReplicationsContainerWithDefault replications = mock(ReplicationsContainerWithDefault.class);
+
+    private final Replication replication = mock(Replication.class);
+    private final PerReplicable replicable = mock(PerReplicable.class);
+
+    public ReplicationServletTestBase(final ReplicationServletBase servlet)
+            throws NoSuchFieldException, Replication.ReplicationException
+    {
+        this.servlet = servlet;
+        PrivateAccessor.setField(servlet, "replications", replications);
+        PrivateAccessor.setField(servlet, "resourceManagement", resourceManagement);
+
+        when(replication.prepare(any())).thenAnswer(i -> i.getArguments()[0]);
+        when(replication.replicate(any())).thenAnswer(i -> i.getArguments()[0]);
+        when(replication.deactivate(any(PageMock.class))).thenAnswer(
+                i -> Collections.singletonList(
+                        ((PageMock)i.getArguments()[0]).getContent()
+                )
+        );
+
+        page.addAdapter(replicable);
+        jcrContent.addAdapter(replicable);
+        when(replicable.getMainResource()).thenReturn(jcrContent);
+    }
+
+    protected void performReplicationResponseContains(final String... substrings) throws IOException, Replication.ReplicationException {
+        final AbstractBaseServlet.Request request = new AbstractBaseServlet.Request(this.request, response);
+        final String response = servlet.performReplication(replication, request, page, resourceResolver).getContent();
+        Arrays.stream(substrings)
+                .map(response::contains)
+                .forEach(Assert::assertTrue);
+    }
+
+    protected void performReplicationResponseContains(final Resource... resources) throws IOException, Replication.ReplicationException {
+        performReplicationResponseContains(Arrays.stream(resources)
+                .map(Resource::getPath)
+                .toArray(String[]::new)
+        );
+    }
+
+}
