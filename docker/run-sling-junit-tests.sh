ed5b813ba0fafa1a01bfa86ab815b7c2d4b387e2
Ruben Reusser <rr@headwire.com>
2021-04-04 07:48:50 -0700

    added pause for sling junit to be ready

--- docker/run-sling-junit-tests.sh
+++ docker/run-sling-junit-tests.sh
@@ -3,37 +3,16 @@
 # run the docker instance we just built
 docker run -d --rm -p 8080:8080 --name peregrine peregrinecms/peregrine-cms:latest
 
-# Wait for Sling to fully start up
-STATUS=$(curl -u admin:admin -s --fail  http://localhost:8080/system/console/bundles.json | jq '.s[3:5]' -c)
-if [ "$STATUS" != "[0,0]" ]; then
-  while [ "$STATUS" != "[0,0]" ]
-  do    
-    echo "Sling still starting. Waiting for all bundles to be ready.."
-    STATUS=$(curl -u admin:admin -s --fail  http://localhost:8080/system/console/bundles.json | jq '.s[3:5]' -c)
-    sleep 5
-  done
-fi
-
 # install the sling junit test suite into the docker image and execute it
 
 cd ..
 cd slingjunit.parent
 mvn install -PautoInstallPackage
 
-# Wait for all bundles to be ready
-STATUS=$(curl -u admin:admin -s --fail  http://localhost:8080/system/console/bundles.json | jq '.s[3:5]' -c)
-if [ "$STATUS" != "[0,0]" ]; then
-  while [ "$STATUS" != "[0,0]" ]
-  do    
-    echo "Sling still starting. Waiting for all bundles to be ready.."
-    STATUS=$(curl -u admin:admin -s --fail  http://localhost:8080/system/console/bundles.json | jq '.s[3:5]' -c)
-    sleep 5
-  done
-fi
+sleep 15
 
-sleep 1
-
-curl -s -X POST http://admin:admin@localhost:8080/system/sling/junit/.json
+curl -s "http://admin:admin@localhost:8080/system/sling/junit/"
+curl -s -X POST "http://admin:admin@localhost:8080/system/sling/junit/.json" | jq . | grep "\"failure\":"
 cd ../docker
 
 # cleanup docker
