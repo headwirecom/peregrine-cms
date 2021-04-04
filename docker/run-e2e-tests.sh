d289935bd2a648ac91b46b566d7a7ec90a6a24ed
Ruben Reusser <rr@headwire.com>
2021-04-03 21:28:43 -0700

    make sure instance is running before sling junit tests

--- docker/run-e2e-tests.sh
+++ docker/run-e2e-tests.sh
@@ -3,11 +3,6 @@
 # run the docker instance we just built
 docker run -d --rm -p 8080:8080 --name peregrine peregrinecms/peregrine-cms:latest
 
-# clone and install the e2e test repository
-git clone https://github.com/peregrine-cms/peregrine-cms-e2e-testing.git
-cd peregrine-cms-e2e-testing
-npm ci
-
 # Wait for Sling to fully start up
 STATUS=$(curl -u admin:admin -s --fail  http://localhost:8080/system/console/bundles.json | jq '.s[3:5]' -c)
 if [ "$STATUS" != "[0,0]" ]; then
@@ -18,6 +13,11 @@ if [ "$STATUS" != "[0,0]" ]; then
     sleep 5
   done
 fi
+
+# clone and install the e2e test repository
+git clone https://github.com/peregrine-cms/peregrine-cms-e2e-testing.git
+cd peregrine-cms-e2e-testing
+npm ci
 
 # accept terms and conditions
 curl -X POST http://admin:admin@localhost:8080/perapi/admin/acceptTermsAndConditions.json
--- docker/run-sling-junit-tests.sh
+++ docker/run-sling-junit-tests.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+# run the docker instance we just built
+docker run -d --rm -p 8080:8080 --name peregrine peregrinecms/peregrine-cms:latest
+
+# Wait for Sling to fully start up
+STATUS=$(curl -u admin:admin -s --fail  http://localhost:8080/system/console/bundles.json | jq '.s[3:5]' -c)
+if [ "$STATUS" != "[0,0]" ]; then
+  while [ "$STATUS" != "[0,0]" ]
+  do    
+    echo "Sling still starting. Waiting for all bundles to be ready.."
+    STATUS=$(curl -u admin:admin -s --fail  http://localhost:8080/system/console/bundles.json | jq '.s[3:5]' -c)
+    sleep 5
+  done
+fi
+
 # install the sling junit test suite into the docker image and execute it
 
 cd ..
@@ -5,3 +21,6 @@ cd slingjunit.parent
 mvn install -PautoInstallPackage
 curl -s -X POST http://localhost:8080/system/sling/junit/.json | jq . | grep "\"failure\":"
 cd docker
+
+# cleanup docker
+docker kill peregrine
