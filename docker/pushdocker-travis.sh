aa8033b16f4e099dee25922a6f3a96510a20149f
Ruben Reusser <rr@headwire.com>
2021-04-05 13:56:36 -0700

    add build of author and publish to travis

--- docker/pushdocker-travis.sh
+++ docker/pushdocker-travis.sh
@@ -10,5 +10,9 @@ if [ ${TRAVIS_PULL_REQUEST} != "false" ]; then
   TAG="pr-${TRAVIS_PULL_REQUEST}"
   docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
   docker tag "$DOCKER_IMAGE" "${DOCKER_IMAGE}:${TAG}"
+  docker tag "$DOCKER_IMAGE-author" "${DOCKER_IMAGE}:${TAG}-author"
+  docker tag "$DOCKER_IMAGE-publish" "${DOCKER_IMAGE}:${TAG}-publish"
   docker push ${DOCKER_IMAGE}:${TAG}
+  docker push ${DOCKER_IMAGE}-author:${TAG}-author
+  docker push ${DOCKER_IMAGE}-publish:${TAG}-publish
 fi
