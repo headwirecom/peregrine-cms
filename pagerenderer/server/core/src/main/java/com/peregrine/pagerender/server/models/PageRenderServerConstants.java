e3908a44005a27cd0a658f9634edf226c2ff2e28
Cris Rockwell <cmrockwe@umich.edu>
2021-04-19 18:02:13 -0400

    updated footer component adding model

--- pagerenderer/server/core/src/main/java/com/peregrine/pagerender/server/models/PageRenderServerConstants.java
+++ pagerenderer/server/core/src/main/java/com/peregrine/pagerender/server/models/PageRenderServerConstants.java
@@ -4,4 +4,6 @@ public class PageRenderServerConstants {
     public static final String PR_SERVER_COMPONENT_BASE_TYPE = "pagerenderserver/components/base";
     public static final String PR_SERVER_COMPONENT_CONTAINER_TYPE = "pagerenderserver/components/container";
     public static final String PR_SERVER_COMPONENT_PAGE_TYPE =  "pagerenderserver/components/page";
+    public static final String PR_SERVER_COMPONENT_NAV_TYPE =  "pagerenderserver/components/nav";
+    public static final String PR_SERVER_COMPONENT_FOOTER_TYPE =  "pagerenderserver/components/footer";
 }
