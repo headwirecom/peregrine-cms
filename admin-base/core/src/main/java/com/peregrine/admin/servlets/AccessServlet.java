8b2f211a93de48f7129bf0bcbde7a66d67d46868
Cris Rockwell <cmrockwe@umich.edu>
2021-04-22 16:00:52 -0400

    added flag preferences/tildaPage in the access servlet

--- admin-base/core/src/main/java/com/peregrine/admin/servlets/AccessServlet.java
+++ admin-base/core/src/main/java/com/peregrine/admin/servlets/AccessServlet.java
@@ -84,7 +84,11 @@ public class AccessServlet extends AbstractBaseServlet {
                 description = "A list of allowed JCR property paths relative to a user's home directory that will be included in the response",
                 required = true
         )
-        String[] profile_include_list() default { "preferences/firstLogin", "profile/email" };
+        String[] profile_include_list() default { 
+            "preferences/firstLogin",
+            "preferences/tildaPage", 
+            "profile/email" 
+        };
     }
 
     public static final String USER_ID = "userID";
