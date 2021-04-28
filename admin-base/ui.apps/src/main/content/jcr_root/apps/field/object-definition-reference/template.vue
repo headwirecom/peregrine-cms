d17767cbf38d1b01221602b5c3a160271eed5846
Felix Puetz <felix.puetz@metaproc.de>
2021-04-28 14:44:43 +0200

    add: new field "object-definition-reference"

---
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/field/object-definition-reference/template.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- Switch -->
+  <div class="wrap">
+    <div v-if="!schema.preview" class="switch">
+      <path-browser :model="model" :schema="schema"/>
+    </div>
+    <p v-else>{{ value }}</p>
+  </div>
+</template>
+
+<script>
+import PathBrowser from '../pathbrowser/template.vue';
+
+export default {
+  name: 'ObjectDefinitionReference',
+  components: {PathBrowser},
+  mixins: [VueFormGenerator.abstractField],
+  props: ['model'],
+}
+</script>
