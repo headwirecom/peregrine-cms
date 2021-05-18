87196a422f797003ecd84f9f41590dd576a81b4b
Felix Puetz <felix.puetz@metaproc.de>
2021-05-18 16:22:56 +0200

    feature/peregrine-icon
    - insertIcon now takes img-path as parameter
    - insert icon now inserts a "peregrine-icon" instead of a placeholder

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/richtoolbar/groups/icons/icons.js
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/richtoolbar/groups/icons/icons.js
@@ -1,39 +1,39 @@
-import {IconLib} from '../../../../../../../../js/constants'
-import {LoggerFactory} from '../../../../../../../../js/logger'
-import {get} from '../../../../../../../../js/utils'
+import { IconLib } from '../../../../../../../../js/constants';
+import { LoggerFactory } from '../../../../../../../../js/logger';
+import { get } from '../../../../../../../../js/utils';
 
-const FILE_NAME = 'richtoolbar.groups.icons.icon'
-const log = LoggerFactory.logger(FILE_NAME).setLevelDebug()
+const FILE_NAME = 'richtoolbar.groups.icons.icon';
+const log = LoggerFactory.logger(FILE_NAME).setLevelDebug();
 
 function getChildName(child) {
-  let arr = child.name.split('.')
+  let arr = child.name.split('.');
 
-  arr.pop()
+  arr.pop();
 
-  return arr.join('.')
+  return arr.join('.');
 }
 
 function getChildImage(child) {
-  return `<img src="${child.path}" alt="${child.name}"/>`
+  return `<img src="${child.path}" alt="${child.name}"/>`;
 }
 
 export default (vm) => {
-  const tenantIcons = get($perAdminApp.getView(), '/admin/icons', [])
-  const icons = []
+  const tenantIcons = get($perAdminApp.getView(), '/admin/icons', []);
+  const icons = [];
 
   tenantIcons.forEach((child) => {
-    const name = getChildName(child)
-    const img = getChildImage(child)
+    const name = getChildName(child);
+    const img = getChildImage(child);
 
     icons.push({
       label: name,
       icon: img,
       iconLib: IconLib.PLAIN_TEXT,
       click: () => {
-        vm.insertIcon(name)
+        vm.insertIcon(child.path);
       },
-    })
-  })
+    });
+  });
 
-  return icons
-}
+  return icons;
+};
