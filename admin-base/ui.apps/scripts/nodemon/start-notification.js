bc0a120379c9160bb02aa66456486b3330c3b27d
Felix Puetz <felix.puetz@metaproc.de>
2021-07-08 12:40:47 +0200

    notifications !

--- admin-base/ui.apps/scripts/nodemon/start-notification.js
+++ admin-base/ui.apps/scripts/nodemon/start-notification.js
@@ -2,11 +2,12 @@ const path = require('path');
 const notifier = require('node-notifier');
 
 notifier.notify({
+  id: 1337,
   title: 'peregrine-cms (STARTED)',
   message: 'started building & deploying /ui.apps',
   icon: path.join(
     __dirname,
     '../../../../pagerenderer/server/ui.apps/src/main/content/jcr_root/content/pagerenderserver/assets/peregrine-logo.png'
   ),
-  timeout: 2,
+  timeout: 1,
 });
