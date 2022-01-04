d7afc4637e51418c12fa3053536a4d999bba8d24
Ruben <rr@headwire.com>
2022-01-04 14:11:29 +0100

    added editor page

---
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/jsonformeditor/template.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="file-editor">
+    <iframe src="/content/admin/jsonformeditor/index.html" width="100%" height="100%" border="0"></iframe>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['model']
+};
+</script>
+
+<style scoped>
+.file-editor {
+  width: calc(100% - 340px) !important;
+  margin: 0;
+  border: 1px solid #607d8b;
+  padding: 0;
+  border-radius: 3px;
+  max-height: 100%;
+  overflow: auto;
+}
+</style>
