55a28bf15d40496c262f71c7f76e34494f8a722b
Andreas Schaefer <as@headwire.com>
2022-01-04 09:19:29 -0800

    Handed over the Site and Object Name to the JSon Form Editor

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/jsonformeditor/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/jsonformeditor/template.vue
@@ -1,13 +1,47 @@
 <template>
   <div class="file-editor">
-    <iframe src="/content/admin/jsonformeditor/index.html" width="100%" height="100%" border="0"></iframe>
+    <iframe
+        v-bind:src="`/content/admin/jsonformeditor/index.html?sitename=${tenantName}&objectname=${objectDefinitionName}`"
+        width="100%"
+        height="100%"
+        border="0">
+    </iframe>
   </div>
 </template>
 
 <script>
 
 export default {
-  props: ['model']
+  props: ['model'],
+  data() {
+    return {
+      tenantName: '',
+      objectDefinitionName: ''
+    }
+  },
+  mounted() {
+    const path = this.getPath()
+    const segments = path.split('/')
+    console.log(`# of segments: ${segments.length}, segments: ${segments}`)
+    if(segments.length >= 5) {
+      this.tenantName = segments[2]
+      this.objectDefinitionName = segments[4];
+    }
+    console.log(`Tenant Name: ${this.tenantName}, Object Definition Name: ${this.objectDefinitionName}`)
+  },
+  methods: {
+    getPath(){
+      if( this.$root.$data.pageView){
+        if( this.$root.$data.pageView.path ){
+          return this.$root.$data.pageView.path;
+        } else {
+          return '';
+        }
+      } else {
+        return '';
+      }
+    },
+  }
 };
 </script>
 
