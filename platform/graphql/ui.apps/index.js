7f251ec99c43c6b2e785c3486ec04cea4cf1e722
Andreas Schaefer <as@headwire.com>
2022-01-20 14:43:23 -0800

    Fixed an issue with a weird Angstrom character in the GraphiQL ui

--- platform/graphql/ui.apps/index.js
+++ platform/graphql/ui.apps/index.js
@@ -1,16 +1,28 @@
-var fs = require('fs-extra');
+// var fs = require('fs-extra');
+var fs = require('fs');
 
-fs.mkdirs('./target/classes/etc/graphiql')
-fs.copySync('node_modules/graphiql/graphiql.min.js', 'target/classes/etc/graphiql/graphiql.min.js')
-fs.copySync('node_modules/graphiql/graphiql.min.js.map', 'target/classes/etc/graphiql/graphiql.min.js.map')
-fs.copySync('node_modules/graphiql/graphiql.min.css', 'target/classes/etc/graphiql/graphiql.min.css')
+if(!fs.existsSync('target/classes/etc/graphiql')) {
+    fs.mkdirSync('target/classes/etc/graphiql')
+}
+fs.copyFileSync('node_modules/graphiql/graphiql.js', 'target/classes/etc/graphiql/graphiql.js')
+// fs.copyFileSync('node_modules/graphiql/graphiql.min.js', 'target/classes/etc/graphiql/graphiql.js')
+// fs.copyFileSync('node_modules/graphiql/graphiql.min.js.map', 'target/classes/etc/graphiql/graphiql.js.map')
+fs.copyFileSync('node_modules/graphiql/graphiql.css', 'target/classes/etc/graphiql/graphiql.css')
+// fs.copyFileSync('node_modules/graphiql/graphiql.min.css', 'target/classes/etc/graphiql/graphiql.css')
+// fs.copyFileSync('node_modules/graphiql/graphiql.min.css.map', 'target/classes/etc/graphiql/graphiql.css.map')
 
-fs.copySync('node_modules/react/cjs/react.development.js', 'target/classes/etc/graphiql/cjs/react.development.js')
-fs.copySync('node_modules/react/cjs/react.production.min.js', 'target/classes/etc/graphiql/cjs/react.min.js')
-fs.copySync('node_modules/react/umd/react.development.js', 'target/classes/etc/graphiql/umd/react.development.js')
-fs.copySync('node_modules/react/umd/react.production.min.js', 'target/classes/etc/graphiql/umd/react.min.js')
+if(!fs.existsSync('target/classes/etc/graphiql/cjs')) {
+    fs.mkdirSync('target/classes/etc/graphiql/cjs')
+}
+if(!fs.existsSync('target/classes/etc/graphiql/umd')) {
+    fs.mkdirSync('target/classes/etc/graphiql/umd')
+}
+// fs.copyFileSync('node_modules/react/cjs/react.development.js', 'target/classes/etc/graphiql/cjs/react.js')
+fs.copyFileSync('node_modules/react/cjs/react.production.min.js', 'target/classes/etc/graphiql/cjs/react.js')
+// fs.copyFileSync('node_modules/react/umd/react.development.js', 'target/classes/etc/graphiql/umd/react.js')
+fs.copyFileSync('node_modules/react/umd/react.production.min.js', 'target/classes/etc/graphiql/umd/react.js')
 
-fs.copySync('node_modules/react-dom/cjs/react-dom.development.js', 'target/classes/etc/graphiql/cjs/react-dom.development.js')
-fs.copySync('node_modules/react-dom/cjs/react-dom.production.min.js', 'target/classes/etc/graphiql/cjs/react-dom.min.js')
-fs.copySync('node_modules/react-dom/umd/react-dom.development.js', 'target/classes/etc/graphiql/umd/react-dom.development.js')
-fs.copySync('node_modules/react-dom/umd/react-dom.production.min.js', 'target/classes/etc/graphiql/umd/react-dom.min.js')
+// fs.copyFileSync('node_modules/react-dom/cjs/react-dom.development.js', 'target/classes/etc/graphiql/cjs/react-dom.js')
+fs.copyFileSync('node_modules/react-dom/cjs/react-dom.production.min.js', 'target/classes/etc/graphiql/cjs/react-dom.js')
+// fs.copyFileSync('node_modules/react-dom/umd/react-dom.development.js', 'target/classes/etc/graphiql/umd/react-dom.js')
+fs.copyFileSync('node_modules/react-dom/umd/react-dom.production.min.js', 'target/classes/etc/graphiql/umd/react-dom.js')
